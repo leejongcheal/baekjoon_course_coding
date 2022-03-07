07a0e42fd7511112d7bf59a20162149670183361
lee <aksndk123@naver.com>
2022-03-07 11:49:16 +0900

    조금더 머리쓴 풀이

--- 기초/브루트포스/브루트포스/2309 일곱난쟁이.py
+++ 기초/브루트포스/브루트포스/2309 일곱난쟁이.py
@@ -1,19 +1,26 @@
-def dfs(now, list = []):
-    if len(list) == 7:
-        if sum(list) == 100:
-            return list
-        else:
-            return None
-    for i in range(now+1, 9):
-        a = dfs(i, list + [L[i]])
-        if a:
-            return a
-    return None
-
 L = []
 for _ in range(9):
     L.append(int(input()))
 L.sort()
-L = dfs(-1)
+sum_L = sum(L[:7])
+flag = 0
+for i in range(7):
+    if sum_L - L[i] + L[7] == 100:
+        L[i],L[7] = L[7], L[i]
+        break
+    if sum_L - L[i] + L[8] == 100:
+        L[i], L[8] = L[8], L[i]
+        break
+if sum(L[:7]) != 100:
+    for i in range(6):
+        if flag:
+            break
+        for j in range(i+1,7):
+            if sum_L - L[i] - L[j] + L[7] + L[8] == 100:
+                L[i], L[j], L[7], L[8] = L[7], L[8], L[i], L[8]
+                flag = 1
+                break
+L = L[:7]
+L.sort()
 for l in L:
     print(l)
