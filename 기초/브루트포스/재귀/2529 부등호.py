3937dd62c5398b8cc06bb04c14cc75740f4b12a5
lee <aksndk123@naver.com>
2022-03-08 16:45:01 +0900

    * dfs강제가 되면서 효율적인 문제
    쫌 고급적인 풀이
    n = 4인경우
    max = 98765
    min = 01234 에 대해서 sign값에 대해서 버블정렬 하면서 만족하는 경우 바로 break하면 각각 최댓값 최솟값을 가지게 된다.

--- 기초/브루트포스/재귀/2529 부등호.py
+++ 기초/브루트포스/재귀/2529 부등호.py
@@ -1,23 +1,27 @@
-from itertools import permutations
-def check(L):
-    for i in range(n):
-        if sign[i] == "<":
-            if L[i] > L[i+1]:
-                return 0
-        else:
-            if L[i] < L[i+1]:
-                return 0
-    return 1
+def dfs(L):
+    global max_val, min_val, sign, n
+    cnt = len(L) - 1
+    if cnt == n:
+        s = "".join(map(str, L))
+        max_val = max(max_val, s)
+        min_val = min(min_val, s)
+        return
+    for i in range(10):
+        if visit[i] == 0:
+            if (sign[cnt] == "<" and L[-1] < i) or (sign[cnt] == ">" and L[-1] > i):
+                visit[i] = 1
+                dfs(L + [i])
+                visit[i] = 0
 
 
 n = int(input())
 sign = list(input().split())
 max_val = "0"
 min_val = "9999999999999999999999"
-for candidate in list(permutations(range(10), n + 1)):
-    if check(candidate):
-        s = "".join(map(str, candidate))
-        max_val = max(max_val, s)
-        min_val = min(min_val, s)
+visit = [0]*10
+for i in range(10):
+    visit[i] = 1
+    dfs([i])
+    visit[i] = 0
 print(max_val)
 print(min_val)
