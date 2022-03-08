bcebb2de0c4666e09b012bd21c5cb82816f90c21
lee <aksndk123@naver.com>
2022-03-08 16:18:30 +0900

    간단한 조합응용

---
+++ 기초/브루트포스/재귀/14889 스타트와 링크.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+
+def cal(A, B):
+    global L, n
+    sum_A = 0
+    for i in A:
+        for j in A:
+            sum_A += L[i][j]
+    sum_B = 0
+    for i in B:
+        for j in B:
+            sum_B += L[i][j]
+    return abs(sum_A - sum_B)
+n = int(input())
+ans = int(1e10)
+L = [list(map(int, input().split())) for _ in range(n)]
+for A in list(combinations(range(n), n // 2)):
+    B = []
+    for i in range(n):
+        if i not in A:
+            B.append(i)
+    ans = min(ans, cal(A, B))
+print(ans)
