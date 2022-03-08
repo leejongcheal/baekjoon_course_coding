d963b0da846e712b0ae2001945e5e20099e9457a
lee <aksndk123@naver.com>
2022-03-08 10:02:16 +0900

    * 숫자의 자릿수를 구하는 간단한 방법

--- 기초/브루트포스/브루트포스/1748 수 이어 쓰기1.py
+++ 기초/브루트포스/브루트포스/1748 수 이어 쓰기1.py
@@ -1,16 +1,7 @@
-n = int(input())
-temp = n
-cnt = 0
-while temp:
-    temp //= 10
-    cnt += 1
+n = input()
+digit = len(n)
 ans = 0
-if cnt == 1:
-    ans = n
-else:
-    a = 9
-    for i in range(cnt - 1):
-        ans += a * (i+1)
-        a *= 10
-    ans += (cnt) * (n - 10**(cnt - 1) + 1)
+for i in range(1, digit):
+    ans += i*9*(10**(i-1))
+ans += digit*(int(n) - (10**(digit-1)) + 1)
 print(ans)
