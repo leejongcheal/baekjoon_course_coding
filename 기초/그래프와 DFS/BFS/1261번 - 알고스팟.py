f55454e7ef96c696db345ff86a3955a27e6137aa
lee <aksndk123@naver.com>
2022-03-14 17:47:39 +0900

    *** 0-1 너비문제
    잘 풀었는데 메모리초과뜸 이유가 뭐지 ㅋㅋ
    문제의 핵심 ->기준은 벽을 부신 갯수
    
    벽의 갯수, 현재 이동한 갯수 ,x, y로 해서 풀려고 하니깐 시간초과
    visit 처리더 깔끔하게 할수있음
    
    -> 문제이해중 가장 중요한건 벽을 몇개 부시고 가냐지, 몇번만에 가냐가 아님..

--- 기초/그래프와 DFS/BFS/1261번 - 알고스팟.py
+++ 기초/그래프와 DFS/BFS/1261번 - 알고스팟.py
@@ -1,24 +1,24 @@
 from collections import deque
-import heapq
+INF = int(1e10)
 steps = [(1,0),(0,1),(-1,0),(0,-1)]
 M, N = map(int, input().split())
 graph = [list(map(int,list(input()))) for _ in range(N)]
-visit = set() # x, y, 벽부신 갯수
+visit = [[INF]*M for i in range(N)]
 # 00 -> N-1 M - 1로 벽최소
-q = []
-heapq.heappush(q,(0,0,0,0)) # 벽갯수, 이동갯수, x , y
-visit.add((0,0,0))
+q = deque()
+q.append((0,0,0)) # 벽갯수, x , y
+visit[0][0] = 0
 while q:
-    wall_cnt, move_cnt, x, y = heapq.heappop(q)
+    wall_cnt, x, y = q.popleft()
     if x == N - 1 and y == M - 1:
         print(wall_cnt)
         break
     for dx, dy in steps:
         nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < M and (wall_cnt, nx, ny) not in visit:
-            if graph[nx][ny] == 0:
-                visit.add((wall_cnt, nx, ny))
-                heapq.heappush(q,(wall_cnt, move_cnt + 1, nx, ny))
-            if graph[nx][ny] == 1:
-                visit.add((wall_cnt + 1, nx, ny))
-                heapq.heappush(q,(wall_cnt + 1, move_cnt + 1, nx, ny))
+        if 0 <= nx < N and 0 <= ny < M:
+            if graph[nx][ny] == 0 and visit[nx][ny] > wall_cnt:
+                visit[nx][ny] = visit[x][y]
+                q.appendleft((wall_cnt, nx, ny))
+            if graph[nx][ny] == 1 and visit[nx][ny] > wall_cnt:
+                visit[nx][ny] = visit[x][y] + 1
+                q.append((wall_cnt + 1, nx, ny))
