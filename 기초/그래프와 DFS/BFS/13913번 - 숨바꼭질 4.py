5bf966e33852efab1c795bbc36d5fe718f5b0d4f
lee <aksndk123@naver.com>
2022-03-14 14:22:56 +0900

    *** BFS에서 tracked를 찾는 방법 방법숙지
    하나하나 리스트를 q에 넣어서 풀었는데 이런식으로 path하나 선언해서 풀어주니 550 -> 200의 시간효율을 보여줌
    - 또다른 방법으로써 숙지해야함

--- 기초/그래프와 DFS/BFS/13913번 - 숨바꼭질 4.py
+++ 기초/그래프와 DFS/BFS/13913번 - 숨바꼭질 4.py
@@ -1,25 +1,35 @@
 from collections import deque, defaultdict
 DP = defaultdict(int)
-x, goal = map(int,input().split())
-if x > goal:
-    print(x - goal)
-    print(" ".join(map(str, [i for i in range(x, goal - 1, -1)])))
+path = defaultdict(int)
+start, goal = map(int,input().split())
+if start > goal:
+    print(start - goal)
+    print(" ".join(map(str, [i for i in range(start, goal - 1, -1)])))
 else:
     q = deque()
-    DP[x] = 0
-    q.append((x, [x]))
+    DP[start] = 0
+    q.append(start)
     while q:
-        x, tracked = q.popleft()
+        x = q.popleft()
         if x == goal:
             print(DP[x])
-            print(" ".join(map(str, tracked)))
+            now = x
+            ans = []
+            while now != start:
+                ans.append(now)
+                now = path[now]
+            ans.append(now)
+            print(" ".join(map(str, reversed(ans))))
             break
         if x - 1 >= 0 and DP[x-1] == 0:
             DP[x - 1] = DP[x] + 1
-            q.append((x - 1, tracked + [x - 1]))
+            path[x-1] = x
+            q.append(x - 1)
         if x + 1 <= 100000 and DP[x + 1] == 0:
             DP[x + 1] = DP[x] + 1
-            q.append((x + 1, tracked + [x + 1]))
+            path[x + 1] = x
+            q.append(x + 1)
         if x*2 <= 100000 and DP[x*2] == 0:
+            path[x * 2] = x
             DP[x * 2] = DP[x] + 1
-            q.append((x * 2, tracked + [x * 2]))
+            q.append(x * 2)
