bf1a69bc8a62ed6794fda729cbc9ea8a4effdfe8
lee <aksndk123@naver.com>
2022-03-14 17:20:53 +0900

    **** heap를 썼는데 답이 틀림 -> 알고리즘은 얼추 맞음 -> BFS풀이
    ** BFS풀이 -> 370에서 160으로
    곱셈은 어쩌지 지금값과 똑같음 따라서 큐의 맨앞에 넣어주어도 내가 원하는 순서가 유지가 됨, 따라서 heapq을 안쓰고 풀이 가능
    
    * 조건문도 넣어줄때 생각을 해야함, 곱셈이 우선적으로 들어가야함.
    대처
    1. 이미 조사한 경우인데 더 작은값이 있다면 업데이트 -> 현재수정
    2. 조건문에 곱셈을 위로 올려서 작은값이 먼저 들어가게
    
    0 27의 경우
    조건문 순서에서 1에서 1개의 카운트를 가지는데 q에 2,2가 먼저 들어가고 그다음에 2, 1을 넣어주는(곱셈)에서 이미 +로 2에 대해서 값이 들어가서 방문했다치고 안들어가게됨

--- 기초/그래프와 DFS/BFS/13549번 - 숨바꼭질 3.py
+++ 기초/그래프와 DFS/BFS/13549번 - 숨바꼭질 3.py
@@ -1,26 +1,24 @@
-from collections import deque, defaultdict
-import heapq
-# DP = defaultdict(int)
+from collections import deque
 DP = [0]*100001
 x, goal = map(int,input().split())
 if x > goal:
     print(x - goal)
 else:
-    q = []
+    q = deque()
     DP[x] = 1
-    heapq.heappush(q,(1, x))
+    q.append(x)
     while q:
         # print(q)
-        cnt, x = heapq.heappop(q)
+        x = q.popleft()
         if x == goal:
             print(DP[x] - 1)
             break
-        if x - 1 > 0 and (DP[x-1] == 0 or DP[x-1] > cnt + 1):
-            DP[x - 1] = cnt + 1
-            heapq.heappush(q,(cnt + 1, x - 1))
-        if x + 1 <= 100000 and (DP[x + 1] == 0 or DP[x+1] > cnt + 1):
-            DP[x + 1] = cnt + 1
-            heapq.heappush(q,(cnt + 1, x + 1))
-        if x > 0 and x*2 <= 100000 and (DP[x*2] == 0 or DP[x*2] > cnt):
-            DP[x * 2] = cnt
-            heapq.heappush(q,(cnt, x * 2))
+        if x > 0 and x*2 <= 100000 and DP[x*2] == 0:
+            DP[x * 2] = DP[x]
+            q.appendleft(x * 2)
+        if x - 1 > 0 and DP[x-1] == 0 :
+            DP[x - 1] = DP[x] + 1
+            q.append(x - 1)
+        if x + 1 <= 100000 and DP[x + 1] == 0 :
+            DP[x + 1] = DP[x] + 1
+            q.append(x + 1)
