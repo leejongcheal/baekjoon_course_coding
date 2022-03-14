3eaedcc7870d35243aa205620ea609ae326fa0d4
lee <aksndk123@naver.com>
2022-03-14 17:10:18 +0900

    *** heap를 썼는데 답이 틀림 -> 알고리즘은 얼추 맞음
    * 조건문도 넣어줄때 생각을 해야함, 곱셈이 우선적으로 들어가야함.
    
    0 27의 경우
    조건문 순서에서 1에서 1개의 카운트를 가지는데 q에 2,2가 먼저 들어가고 그다음에 2, 1을 넣어주는(곱셈)에서 이미 +로 2에 대해서 값이 들어가서 방문했다치고 안들어가게됨

---
+++ 기초/그래프와 DFS/BFS/13549번 - 숨바꼭질 3.py
@@ -0,0 +1,27 @@
+from collections import deque, defaultdict
+import heapq
+# DP = defaultdict(int)
+DP = [0]*100001
+x, goal = map(int,input().split())
+if x > goal:
+    print(x - goal)
+else:
+    q = []
+    DP[x] = 1
+    heapq.heappush(q,(1, x))
+    while q:
+        # print(q)
+        cnt, x = heapq.heappop(q)
+        if x == goal:
+            print(DP[x] - 1)
+            break
+        if x - 1 > 0 and (DP[x-1] == 0): # or DP[x-1] > cnt + 1):
+            DP[x - 1] = cnt + 1
+            heapq.heappush(q,(cnt + 1, x - 1))
+        if x + 1 <= 100000 and (DP[x + 1] == 0) :# or DP[x+1] > cnt + 1):
+            DP[x + 1] = cnt + 1
+            heapq.heappush(q,(cnt + 1, x + 1))
+        if x > 0 and x*2 <= 100000 and (DP[x*2] == 0):# or DP[x*2] > cnt):
+            DP[x * 2] = cnt
+            heapq.heappush(q,(cnt, x * 2))
+    # print(DP[:40])
