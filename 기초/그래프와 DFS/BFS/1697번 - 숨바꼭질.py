88ea5f5acc0546067630a5bc68fe4c3d9ae44307
lee <aksndk123@naver.com>
2022-03-14 13:59:31 +0900

    * 실수
    DP확인만하고 값을 안넣어줘서 q가 너무 커져서 메모리초과로 틀림

---
+++ 기초/그래프와 DFS/BFS/1697번 - 숨바꼭질.py
@@ -0,0 +1,23 @@
+from collections import deque, defaultdict
+DP = defaultdict(int)
+x, goal = map(int,input().split())
+if x > goal:
+    print(x - goal)
+else:
+    q = deque()
+    DP[x] = 0
+    q.append(x)
+    while q:
+        x = q.popleft()
+        if x == goal:
+            print(DP[x])
+            break
+        if x - 1 >= 0 and DP[x-1] == 0:
+            DP[x - 1] = DP[x] + 1
+            q.append(x - 1)
+        if x + 1 <= 100000 and DP[x + 1] == 0:
+            DP[x + 1] = DP[x] + 1
+            q.append(x + 1)
+        if x*2 <= 100000 and DP[x*2] == 0:
+            DP[x * 2] = DP[x] + 1
+            q.append(x*2)
