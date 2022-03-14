f1826c40fc4df13fd2cebfaa4a5ee2975bf5cd5e
lee <aksndk123@naver.com>
2022-03-14 16:46:05 +0900

    *** 문제이해 + BFS 방문 응용 2차원 대신 set
    최대크기가 1000 * 1000이라서 2차원 배열대신 set을 사용해봄
    
    메모리 차이는 7%정도에 시간차이는 거의 없음을 알수있음
    --> 굳이 set이 아니라 2차원배열 선언해서 풀어도된다는걸 배울수 있음

--- 기초/그래프와 DFS/BFS/14226번 - 이모티콘.py
+++ 기초/그래프와 DFS/BFS/14226번 - 이모티콘.py
@@ -1,23 +1,22 @@
 from collections import defaultdict
 import heapq
 S = int(input())
-DP = [[0]*1001 for _ in range(1001)]
+DP = set()
 q = [(0, 1, 0)]
 while q:
     time, view, temp = heapq.heappop(q)
     if view == S:
         print(time)
         break
-    if view - 1 > 0 and DP[view - 1][temp] == 0:
-        DP[view - 1][temp] = 1
+    if view - 1 > 0 and (view-1, temp) not in DP:
+        DP.add((view-1, temp))
         heapq.heappush(q, (time + 1, view - 1, temp))
-    if view + temp <= 1000 and DP[view][view] == 0 and temp == 0:
-        DP[view][view] = 1
+    if view + temp <= 1000 and temp == 0 and (view, view) not in DP:
+        DP.add((view, view))
         heapq.heappush(q, (time + 1, view, view))
-    if view + temp <= 1000 and DP[view + temp][temp] == 0 and temp != 0:
-        DP[view + temp][temp] = 1
+    if view + temp <= 1000 and temp != 0 and (view + temp, temp) not in DP:
+        DP.add((view + temp, temp))
         heapq.heappush(q, (time + 1, view + temp, temp))
-        if DP[view + temp][view] == 0:
-            DP[view + temp][view] = 1
-            temp = view
-            heapq.heappush(q, (time + 1, view, temp))
+        if (view + temp,view) not in DP:
+            DP.add((view + temp, view))
+            heapq.heappush(q, (time + 1, view, view))
