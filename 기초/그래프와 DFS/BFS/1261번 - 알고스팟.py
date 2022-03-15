7a4b0a5ad9c0bdf41e0ca47f211ddcb55db0bca3
lee <aksndk123@naver.com>
2022-03-15 09:44:34 +0900

    *** 0-1 너비문제 + 마지막수정
    잘 풀었는데 메모리초과뜸 이유가 뭐지 ㅋㅋ
    -> 마지막 조건문 실수함
    
    문제의 핵심 ->기준은 벽을 부신 갯수
    
    벽의 갯수, 현재 이동한 갯수 ,x, y로 해서 풀려고 하니깐 시간초과
    visit 처리더 깔끔하게 할수있음
    
    -> 문제이해중 가장 중요한건 벽을 몇개 부시고 가냐지, 몇번만에 가냐가 아님..

--- 기초/그래프와 DFS/BFS/1261번 - 알고스팟.py
+++ 기초/그래프와 DFS/BFS/1261번 - 알고스팟.py
@@ -6,20 +6,21 @@ graph = [list(map(int,list(input()))) for _ in range(N)]
 visit = [[INF]*M for i in range(N)]
 # 00 -> N-1 M - 1로 벽최소
 q = deque()
-q.append((0,0,0)) # 벽갯수, x , y
+# q.append((0,0,0)) # 벽갯수, x , y
+q.append((0,0)) # x , y
 visit[0][0] = 0
 while q:
-    wall_cnt, x, y = q.popleft()
+    x, y = q.popleft()
     if x == N - 1 and y == M - 1:
-        print(wall_cnt)
+        print(visit[x][y])
         break
     for dx, dy in steps:
         nx, ny = x + dx, y + dy
         if 0 <= nx < N and 0 <= ny < M:
-            if graph[nx][ny] == 0 and visit[nx][ny] > wall_cnt:
+            if graph[nx][ny] == 0 and visit[nx][ny] > visit[x][y]:
                 visit[nx][ny] = visit[x][y]
-                q.appendleft((wall_cnt, nx, ny))
+                q.appendleft((nx, ny))
             # 아 무슨 wall_cnt + 1 이 아닌 wall_cnt 넣었다고 메모리 초과뜨는 문제
-            elif graph[nx][ny] == 1 and visit[nx][ny] > wall_cnt + 1:
+            elif graph[nx][ny] == 1 and visit[nx][ny] > visit[x][y] + 1:
                 visit[nx][ny] = visit[x][y] + 1
-                q.append((wall_cnt + 1, nx, ny))
+                q.append((nx, ny))
