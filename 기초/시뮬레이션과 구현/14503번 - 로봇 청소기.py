1c3007bdbb995e55931d3a47e17454659d6497ef
lee <aksndk123@naver.com>
2022-03-15 20:50:44 +0900

    간단한 제한이동 구현

--- 기초/시뮬레이션과 구현/14503번 - 로봇 청소기.py
+++ 기초/시뮬레이션과 구현/14503번 - 로봇 청소기.py
@@ -1,12 +1,29 @@
 N, M = map(int, input().split())
 x, y, d = map(int, input().split())
-Map = [list(map(int, input().split()))]
+Map = [list(map(int, input().split())) for _ in range(N)]
 steps = [(-1, 0),(0,1),(1,0),(0,-1)]
 Map[x][y] = 2
 res = 1
 while 1:
+    flag = 0
     for i in range(1, 5):
         nd = (d - i) % 4
         dx, dy = steps[nd]
         nx, ny = x + dx, y + dy
-        if Map[nx][ny]
+        if Map[nx][ny] == 0:
+            x, y, d = nx, ny, nd
+            Map[x][y] = 2
+            res += 1
+            flag = 1
+            break
+    if flag:
+        continue
+    # 후진해야하는 경우
+    else:
+        dx, dy = steps[d]
+        px, py = x - dx, y - dy
+        if Map[px][py] == 1:
+            break
+        else:
+            x, y = px, py
+print(res)
