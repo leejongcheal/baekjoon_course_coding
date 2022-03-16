aafd4fbc192cd401cd48daa283a148ba583c19d2
lee <aksndk123@naver.com>
2022-03-16 15:30:08 +0900

    ** 멋진 범위나누기 구현문제
    생각했어야하는 부분 X < H 경우 H < X인 경우 등 여러가지 복합적으로 생각을 했어야함
    
    그림을 그려서 ㅁ  일때 중간의행의 범위는
                 ㅁ
     H <= i < X 임 -> H <X 인경우는 ok
     H > X인 경우는 저런 그림의 형태가 나오지않음 그러나 H<= i < X에 의해서 None이나와서 두가지 경우에 대해서 모두 만족한다.

--- 기초/시뮬레이션과 구현/16967번 - 배열 복원하기.py
+++ 기초/시뮬레이션과 구현/16967번 - 배열 복원하기.py
@@ -1,15 +1,13 @@
 H, W, X, Y = map(int, input().split())
 Map = [list(map(int,input().split())) for _ in range(H+X)]
 temp = [[-1]*W for _ in range(H)]
-for i in range(X):
-    for j in range(W):
-        temp[i][j] = Map[i][j]
-for di in range(X):
-    for j in range(W):
-        temp[-(di + 1)][j] = Map[-(di + 1)][j+Y]
 for i in range(H+X):
     for j in range(W+Y):
-        if X <= i < H and 0 <= j < Y:
+        if 0 <= i < X and 0 <= i < H and 0 <= j < W:
+            temp[i][j] = Map[i][j]
+        elif H <= i and 0 <= i - X < H and 0 <= j - Y < W:
+            temp[i-X][j - Y] = Map[i][j]
+        elif X <= i < H and 0 <= j < Y:
             temp[i][j] = Map[i][j]
         elif X <= i < H and W <= j < W + Y:
             temp[i - X][j - Y] = Map[i][j]
