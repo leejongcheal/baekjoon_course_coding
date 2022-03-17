efbbc7cf0ce4cb0fffe916cce3a6972909f18d00
lee <aksndk123@naver.com>
2022-03-17 23:25:57 +0900

    ** 무식하게 푸는데 기술필요함
    2가지의 방법이 있는데 전에 쓴거 응용해서 풀어봄 근데 주석부분이 더 빠르긴함

--- 연습/브루트 포스/기타/2143번 - 두 배열의 합.py
+++ 연습/브루트 포스/기타/2143번 - 두 배열의 합.py
@@ -4,16 +4,24 @@ N = int(input())
 A = list(map(int, input().split()))
 M = int(input())
 B = list(map(int, input().split()))
-sum_cnt = defaultdict(int)
+sum_cntA = defaultdict(int)
+sum_cntB = defaultdict(int)
 res = 0
 for left in range(N):
     now = 0
     for right in range(left, N):
         now += A[right]
-        sum_cnt[now] += 1
+        sum_cntA[now] += 1
 for left in range(M):
     now = 0
     for right in range(left, M):
         now += B[right]
-        res += sum_cnt[target - now]
+        sum_cntB[now] += 1
+for key in sum_cntA.keys():
+    res += sum_cntA[key] * sum_cntB[target - key]
+# for left in range(M):
+#     now = 0
+#     for right in range(left, M):
+#         now += B[right]
+#         res += sum_cnt[target - now]
 print(res)
