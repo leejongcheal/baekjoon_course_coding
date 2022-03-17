9da4cfb2f2e1114a5cfb6d1e41c9dbf0004a2d62
lee <aksndk123@naver.com>
2022-03-17 17:32:56 +0900

    *** 계산을 어떻게 할것인가??
    abc , ab
    a = 100 + 10
    b = 10 + 1
    c = 1
    a*9 + b*8 + c*7 이 최대값
    이러한 발상을 전혀 못하고 브루트포스 문제라 그냥 브루트포스로만 풀려고함
    결국에 저위의 과정이 필요함

--- 연습/브루트 포스/순열/1339번 - 단어 수학.py
+++ 연습/브루트 포스/순열/1339번 - 단어 수학.py
@@ -1,46 +1,27 @@
-def cal():
-    global keys, dic, L, dic_value
-    res = 0
+def cal_dic_value():
+    global dic_value, L
     for l in L:
         length = len(l) - 1
-        val = 0
         for e in l:
-            decode_val = dic_value[dic[e]]
-            if decode_val == -1:
-                decode_val = 9
-            val += decode_val*(10 ** length)
+            dic_value[e] += 10 ** length
             length -= 1
-        res += val
-    return res
+    return list(dic_value.values())
 
-def dfs(dic_idx):
-    global total, res
-    if dic_idx == total:
-        res = max(res, cal())
-        return
-    # 가지치기
-    else:
-        if res > cal():
-            return
-    for i in range(9,-1,-1):
-        if visit[i] == 0:
-            visit[i] = 1
-            dic_value[dic_idx] = i
-            dfs(dic_idx + 1)
-            dic_value[dic_idx] = -1
-            visit[i] = 0
+
 N = int(input())
 L = list(list(input()) for _ in range(N))
 keys = []
 for l in L:
     keys += l
 keys = set(keys)
-total = len(keys)
-dic = dict()
+dic_value = dict()# 자리수를 계산한값
+for key in keys:
+    dic_value[key] = 0
+value_list = cal_dic_value()
+value_list.sort(reverse=True)
 res = 0
-dic_value = [-1]*(total)
-for idx, key in enumerate(keys):
-    dic[key] = idx
-visit = [0]*10
-dfs(0)
+number = 9
+for v in value_list:
+    res += number * v
+    number -= 1
 print(res)
