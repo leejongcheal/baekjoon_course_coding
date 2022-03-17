40958dc017bea98ed2f53ce65ccf714fba8e25c7
lee <aksndk123@naver.com>
2022-03-17 21:57:07 +0900

    * 깔끔 조건문 2003번 먼저 다시풀기

--- 연습/브루트 포스/기타/1806번 - 부분합.py
+++ 연습/브루트 포스/기타/1806번 - 부분합.py
@@ -1,12 +1,17 @@
+INF = int(1e10)
 N, M = map(int, input().split())
 L = list(map(int, input().split()))
-res = 0
+res = INF
 left = 0
 now = 0
 for right in range(N):
     now += L[right]
     while now >= M:
-        res = min(res, right - left)
+        # print(left, right, now)
+        res = min(res, right - left + 1)
         now -= L[left]
         left += 1
-print(res)
+if res == INF:
+    print(0)
+else:
+    print(res)
