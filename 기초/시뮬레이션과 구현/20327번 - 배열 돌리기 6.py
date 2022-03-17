cbb88302d409ab6c1afb68d5026d775cb778f3ed
lee <aksndk123@naver.com>
2022-03-17 14:44:34 +0900

    ** 매우 복잡한 4차원배열
    나누는 부분 + 어떤식으로 저장할건지?
    복잡한 배열의 경우 거꾸로 코딩한다.

--- 기초/시뮬레이션과 구현/20327번 - 배열 돌리기 6.py
+++ 기초/시뮬레이션과 구현/20327번 - 배열 돌리기 6.py
@@ -3,14 +3,12 @@ def split(Map, l):
     M = 2**l
     cnt = N//M
     temp = [[[[0]*M for z in range(M)] for x in range(cnt)] for c in range(cnt)]
-    for index in range(0, cnt*cnt):
-        x = index // M
-        y = index % M
-        sub = temp[x][y]
-        for i in range(M):
-            for j in range(M):
-                # print(x, y, i, j)
-                sub[i][j] = Map[x*M+i][y*M+j]
+    for i in range(cnt):
+        for j in range(cnt):
+            sub = temp[i][j]
+            for x in range(M):
+                for y in range(M):
+                    sub[x][y] = Map[i*M + x][j*M + y]
     return temp
 
 def fill(Map, sub_list):
@@ -60,13 +58,6 @@ for _ in range(R):
 for k, l in R_list:
     # 나누기
     sub_list = split(Map,l)
-    for t in sub_list:
-        print(t)
-    # print(1212)
-    # for s in sub_list:
-    #     print(s)
-    # print(121222)
-    # 번호에 따른 연산
     if k < 5:
         cnt = N // 2**l
         if k == 1:
@@ -77,25 +68,16 @@ for k, l in R_list:
         elif k == 2:
             for i in range(cnt):
                 for j in range(cnt):
-                    print(i,j,sub_list[i][j])
                     sub_list[i][j] = reverse(sub_list[i][j])
-                    print(i,j,sub_list[i][j])
         elif k == 3:
             for i in range(cnt):
                 for j in range(cnt):
-                    print(i,j,sub_list[i][j])
                     sub_list[i][j] = rotate(sub_list[i][j])
-                    print(i,j,sub_list[i][j])
         elif k == 4:
             for i in range(cnt):
                 for j in range(cnt):
-                    print(i,j,sub_list[i][j])
                     sub_list[i][j] = reverse_rotate(sub_list[i][j])
-                    print(i, j, sub_list[i][j])
     elif k >= 5:
-        for t in sub_list:
-            print(t)
-        print(1212)
         if k == 5:
             sub_list = up_down(sub_list)
         elif k == 6:
@@ -104,11 +86,7 @@ for k, l in R_list:
             sub_list = rotate(sub_list)
         elif k == 8:
             sub_list = reverse_rotate(sub_list)
-    print("연산후")
-    for t in sub_list:
-        print(t)
     fill(Map, sub_list)
-    print("채운거확인")
-    for m in Map:
-        print(m)
     # 확인
+for m in Map:
+    print(" ".join(map(str, m)))
