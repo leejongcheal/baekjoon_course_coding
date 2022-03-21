883804ff6da45973633f80a249cf383c80a93206
lee <aksndk123@naver.com>
2022-03-21 14:04:47 +0900

    ** 기준이 복잡한 BFS + visit 처리 안해도되는 무식한 문

--- 연습/재귀/16197번 - 두 동전.py
+++ 연습/재귀/16197번 - 두 동전.py
@@ -1,30 +1,35 @@
+from collections import deque
+
 steps = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-def dfs(x1, y1, x2, y2, cnt):
-    if cnt > 9:
-        return -1
-    for dx, dy in steps:
-        nx1, ny1, nx2, ny2 = x1 + dx, y1 + dy, x2 + dx, y2 + dy
-        if (not (0 <= nx1 < N) or not(0 <= ny1 < M)) and (0 <= nx2 < N and 0 <= ny2 < M):
-            print(1)
-            return cnt + 1
-        elif (not (0 <= nx2 < N) or not(0 <= ny2 < M)) and (0 <= nx1 < N and 0 <= ny1 < M):
-            print(2, cnt, nx1, ny1, nx2, ny2)
-            return cnt + 1
-        elif 0 <= nx1 < N and 0 <= ny1 < M and 0 <= nx2 < N and 0 <= ny2 < M:
-            if L[nx1][ny1] == "#":
-                nx1, nx2 = x1, y1
-            if L[nx2][ny2] == "#":
-                nx2, ny2 = x2, y2
-            a = dfs(nx1, ny1, nx2, ny2, cnt + 1)
-            if a is not None:
-                print(3, a)
-                return a
-        else:
-            continue
-    return None
+
+
 # o: 동전
 # .: 빈 칸
 # #: 벽
+def bfs(x1, y1, x2, y2):
+    q = deque()
+    q.append((0, x1, y1, x2, y2))
+    while q:
+        cnt, x1, y1, x2, y2 = q.popleft()
+        if cnt > 9:
+            return -1
+        for dx, dy in steps:
+            nx1, ny1, nx2, ny2 = x1 + dx, y1 + dy, x2 + dx, y2 + dy
+            if (not (0 <= nx1 < N) or not (0 <= ny1 < M)) and (0 <= nx2 < N and 0 <= ny2 < M):
+                # print(1, cnt, nx1, ny1, nx2, ny2, x1, y1, x2, y2)
+                return cnt + 1
+            elif (not (0 <= nx2 < N) or not (0 <= ny2 < M)) and (0 <= nx1 < N and 0 <= ny1 < M):
+                # print(2, cnt, nx1, ny1, nx2, ny2)
+                return cnt + 1
+            elif 0 <= nx1 < N and 0 <= ny1 < M and 0 <= nx2 < N and 0 <= ny2 < M:
+                if L[nx1][ny1] == "#":
+                    nx1, ny1 = x1, y1
+                if L[nx2][ny2] == "#":
+                    nx2, ny2 = x2, y2
+                q.append((cnt+1, nx1, ny1, nx2, ny2))
+            else:
+                continue
+    return "error"
 N, M = map(int, input().split())
 res = 0
 x1 = y1 = x2 = y2 = -1
@@ -36,5 +41,5 @@ for i in range(N):
                 x1, y1 = i, j
             elif x2 == -1:
                 x2, y2 = i, j
-res = dfs(x1, y1, x2, y2, 0)
+res = bfs(x1, y1, x2, y2)
 print(res)
