2624aa0982f845c1ad8b4c68b148138d19ccea74
lee <aksndk123@naver.com>
2022-03-21 11:19:21 +0900

    *DP 풀이
    L에 대해서 정렬후 작은값부터의 합을 이용한 풀이
    전의합에대해서 1~prev까지 수를 만들수 있다고 치면, 지금값 now에 대해서 now가 prev + 1보다 작거나 같다면 1~prev+now까지 값을 만들수 있다.

--- 연습/재귀/14225번 - 부분수열의 합.py
+++ 연습/재귀/14225번 - 부분수열의 합.py
@@ -1,14 +1,9 @@
-from itertools import combinations
-from collections import defaultdict
 N = int(input())
 L = list(map(int, input().split()))
-num_dict = defaultdict(int)
-for i in range(1, N + 1):
-    for comb in combinations(L, i):
-        num_dict[sum(comb)] += 1
-index = 1
-while 1:
-    if num_dict[index] == 0:
+L.sort()
+max, ans = sum(L), 1
+for i in L:
+    if ans < i:
         break
-    index += 1
-print(index)
+    ans += i
+print(ans)
