4b8770f53197bf7e84d3f77b9bb7f3090e75869f
lee <aksndk123@naver.com>
2022-03-22 10:15:34 +0900

    ** 사이클 문제 + 가지치기
    - 익숙하지 않아서 그런지는 몰라도 자꾸 애매한 발상만함
    - 시작점을 기준으로 같은경우에 사이클 -> traced에 있는값을 또 방문하는 경우 traced[(x,y)]인덱스부터 지금까지 사이클로 생각을 못함
    - 일단 가지치기 안하니 150정도 나왔음
    - 가지치기 후 120ms NM이 쫌더 큰수였으면 열배정도 차이도 가능할듯

--- 연습/그래프와 BFS/그래프 알고리즘/16929번 - Two Dots.py
+++ 연습/그래프와 BFS/그래프 알고리즘/16929번 - Two Dots.py
@@ -1,9 +1,8 @@
 def dfs(x, y, val, traced):
     global visited, Map, steps, N, M
-    # if visited[i][j] == 1:
-    #     return 1
-    if (x,y) in traced:
-        # print(22, x, y, traced)
+    if visited[x][y] == 1:
+        return 1
+    if (x, y) in traced:
         index = traced.index((x, y))
         if len(traced[index:]) >= 4:
             return 0
@@ -16,21 +15,22 @@ def dfs(x, y, val, traced):
             if not dfs(nx, ny, val, traced):
                 return 0
     traced.pop()
-    # visited[i][j] = 1
+    visited[x][y] = 1
     return 1
+
 
 steps = [(1,0),(0,1),(-1,0),(0,-1)]
 N, M = map(int, input().split())
 Map = [list(input()) for _ in range(N)]
 visited = [[0]*M for _ in range(N)]
 flag = 0
-# print(dfs(0,0,Map[0][0], []))
 for i in range(N):
     for j in range(M):
         if visited[i][j] == 0:
             if not dfs(i,j,Map[i][j],[]):
                 flag = 1
                 break
+            visited[i][j] = 1
     if flag:
         break
 if flag:
