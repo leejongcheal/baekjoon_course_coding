6e1d04ab132d73a3f0d554ac25d4602c83301065
lee <aksndk123@naver.com>
2022-03-22 21:24:39 +0900

    *** 문제이해를 완벽하게 하지못함
    1. 색깔은 최대 3개임... 고로 답은 0123 중하나임
    
    X가 없는 경우 0개
    단일 X로만 주어지는 경우 1개
    X-X가 있는경우 최소 2개임
     -> dfs로 색 0,1만 두고 그리다가 겹치는 부분이 생긴다면 3개임
    
     이게 이문제의 핵심임
     나는 여기서 또 steps이 6개이고 X하나에 steps index로 01 12 23.. 에 그려야하는경우를 색이 3개필요하다 가정하고 풀었는데 틀림
     두개의 간선이 겹쳐져 있는경우가 아니여도 3개가 필요한 부분이 있다고 생각함
     따라서 색2개로 그리다가 now의 색과 next색이 같은경우가 생기면 3개가 필요하다고 두고풀었더니 통과함
    
     전에 그냥 무식한 dfs로 풀었는데 택도 없고, 그걸또 수정할려고 하니 말도 안되는 문제가 되어버림 아 개같네 진짜

--- 연습/그래프와 BFS/그래프 알고리즘/12946번 - 육각 보드.py
+++ 연습/그래프와 BFS/그래프 알고리즘/12946번 - 육각 보드.py
@@ -1,47 +1,17 @@
-# 아 시이발 좆같네 진짜 어쩌라고  그래서
-# 끝나는 기준도 애매하고 다채웠는지 확인하고싶은데도 못하고 돌다가 못채우는 경우는 없다고 가정을 치자
-# 그래서 끝날떄 까지 visit인 set에 계속 채워놓고 cnt를 계산하고 다음부터 min_index를 채우면됨
-# 그러면 전의 최소값은 어쩔껀데 ㅋㅋ min_index를  갯수를 세고
-
-def check(x, y, val):
-    global steps, N, Map
-    for dx, dy in steps:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0<= ny < N and Map[nx][ny] == val:
-            return 0
-    return 1
-
-def dfs(x, y, index, val, cnt):
-    # print(index, cnt)
-    # cnt는 그린거의 갯수, val 은 들어갈 값
-    global steps, min_index, M, Map, N, visit
-    if index >= min_index:
+def dfs(x, y, color=0):
+    global ans
+    if ans == 3:
         return
-    if cnt == M:
-        # print(1)
-        min_index = min(min_index, index)
-        return
-    Map[x][y] = val
+    ans = max(ans, 1)
+    Map[x][y] = color
     for dx, dy in steps:
         nx, ny = x + dx, y + dy
         if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == "X":
-            flag = 1
-            for i in range(index):
-                if check(nx, ny, i):
-                    flag = 0
-                    dfs(nx, ny, index, i, cnt + 1)
-            if flag:
-                dfs(nx, ny, index + 1, index, cnt + 1)
-    Map[x][y] = "X"
-
-def dfs_first(x, y):
-    visit.add((x, y))
-    for dx, dy in steps:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == "X":
-            if (nx, ny) not in visit:
-                dfs_first(nx, ny)
-
+            ans = 2
+            dfs(nx, ny, 1 - color)
+        elif 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == color:
+            ans = 3
+            return
 
 steps = [(-1, 0),(-1, 1),(0, 1),(1, 0),(1, -1),(0, -1)]
 N = int(input())
@@ -51,24 +21,14 @@ for i in range(N):
     for j in range(N):
         if Map[i][j] == "X":
             location.append((i, j))
-res = 10**10
-M = len(location)
-cnt = 0
-visit = set()
-visited = [[0]*N for _ in range(N)]
+res = 0
 for x, y in location:
-    if Map[x][y] == "X" and visited[x][y] == 0:
-        visit = set()
-        dfs_first(x, y)
-        M = len(visit)
-        for vx, vy in visit:
-            visited[vx][vy] = 1
-        # print(visit)
-        # print(M)
-        min_index = 10**10
-        cnt = 1
-        dfs(x, y, 1, 0, cnt)
-        res = min(min_index, res)
+    if Map[x][y] == "X":
+        ans = 1
+        dfs(x, y, 0)
+        res = max(res, ans)
+        if res == 3:
+            break
 if location:
     print(res)
 else:
