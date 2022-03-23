6599af63b20e4c1b74378d60f8d01faa5a09c5fb
lee <aksndk123@naver.com>
2022-03-23 11:42:33 +0900

    그냥 dfs써서 틀린 문제
    bfs로 푸니깐 a,b,c 든 a,b 풀이든 통과함 물론 시간은 450 -> 380정도의 효율은 있긴한데 아오 짜증나

--- 연습/그래프와 BFS/BFS 알고리즘/12886번 - 돌 그룹.py
+++ 연습/그래프와 BFS/BFS 알고리즘/12886번 - 돌 그룹.py
@@ -1,14 +1,12 @@
 from collections import defaultdict, deque
 
-
 def bfs(L):
     visit = defaultdict(int)
     q = deque()
-    visit[(L[0], L[2])] = 1
-    q.append((L[0], L[2]))
+    visit[(L[0], L[1], L[2])] = 1
+    q.append((L[0], L[1], L[2]))
     while q:
-        A, B = q.popleft()
-        C = Sum - (A + B)
+        A, B, C = q.popleft()
         if A == B == C:
             return 1
         for x, y in (A, B), (A, C), (B, C):
@@ -16,14 +14,15 @@ def bfs(L):
                 x, y = y, x
             y -= x
             x *= 2
-            X = min(x, y, Sum-(x + y))
-            Y = max(x, y, Sum-(x + y))
-            if visit[(X,Y)] == 0:
-                visit[(X, Y)] = 1
-                q.append((X, Y))
+            z = Sum - (x + y)
+            temp = tuple(sorted([x, y, z]))
+            if visit[temp] == 0:
+                visit[temp] = 1
+                q.append(temp)
     return 0
 
 
+steps = [(0, 1, 2),(0, 2, 1),(1, 2, 0)]
 L = list(map(int, input().split()))
 Sum = sum(L)
 if Sum%3 != 0:
