b7f4c3ae34a57d416d327522a710f0ca47536294
lee <aksndk123@naver.com>
2022-03-25 21:42:52 +0900

    ** visit방문을 얼마나 효율적으로 할것인가?
    일단처음에 리스트로 할려고 하니깐 (L[0], L[1], L[2])로는 visit할수 없음 L에 대해서 tuple화 해야하는 쓸때없는 시간이 필요함.
    그래서 리스트가 아닌 문자열을 원소로 하면 (s1,s2,s3)로 풀수 있게됨
    처음에 갠히 리스트로 풀려해서 어렵게 생각한듯
    
    visit에 넣는 방법으로 s1,s2,s3도 있고 또는 s1+s2+s3,1len,2len,3len 으로 쓸수도 있음

--- 연습/그래프와 BFS/BFS 알고리즘/12906번 - 새로운 하노이 탑.py
+++ 연습/그래프와 BFS/BFS 알고리즘/12906번 - 새로운 하노이 탑.py
@@ -4,16 +4,18 @@ L = []
 for i in range(3):
     s = list(input().split())
     if len(s) == 2:
-        a = list(s[1])
+        a = s[1]
         while a and a[0] == top[i]:
             a = a[1:]
         L.append(a)
     else:
-        L.append([])
+        L.append("")
+
+
 q = deque()
-visit = []
-visit.append(L)
-q.append((L, 0))
+visit = set()
+visit.add((L[0],L[1],L[2]))
+q.append((tuple(L), 0))
 res = -1
 while q:
     L, cnt = q.popleft()
@@ -26,22 +28,22 @@ while q:
                 continue
             if L[i]:
                 temp_i = L[i][:-1]
-                temp_j = L[j] + [L[i][-1]]
+                temp_j = L[j] + L[i][-1]
                 if temp_j[0] == top[j]:
                     temp_j = temp_j[1:]
                 if i == 0 and j == 1:
-                    Lij = [temp_i, temp_j, L[2]]
+                    Sij = (temp_i, temp_j, L[2])
                 elif i == 1 and j == 0:
-                    Lij = [temp_j, temp_i, L[2]]
+                    Sij = (temp_j, temp_i, L[2])
                 elif i == 0 and j == 2:
-                    Lij = [temp_i, L[1], temp_j]
+                    Sij = (temp_i, L[1], temp_j)
                 elif i == 2 and j == 0:
-                    Lij = [temp_j, L[1], temp_i]
+                    Sij = (temp_j, L[1], temp_i)
                 elif i == 1 and j == 2:
-                    Lij = [L[0], temp_i, temp_j]
+                    Sij = (L[0], temp_i, temp_j)
                 elif i == 2 and j == 1:
-                    Lij = [L[0], temp_j, temp_i]
-                if Lij not in visit:
-                    visit.append(Lij)
-                    q.append((Lij, cnt + 1))
+                    Sij = (L[0], temp_j, temp_i)
+                if Sij not in visit:
+                    visit.add(Sij)
+                    q.append((Sij, cnt + 1))
 print(res)
