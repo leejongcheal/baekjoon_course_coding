0c4a8c473d3fdaf6aa8f7f2907e20a7f33fb2617
lee <aksndk123@naver.com>
2022-03-29 12:53:27 +0900

    ** 실수지리게함 복잡한 구현
    flag를 global로 써야하는 이유
    - 값자체 참조는 가능함
    - 근데 값을 함수안에서 넣어주는경우 지역변수가 되어버림
    - 리스트처럼 주소값을 참조하는 변수와의 차이점을 기억할것
    
    red 부분에서 이해를 잘못함
    문제 이해하는 능력이 부족함 ㅜㅜ 다시풀어보자

--- 연습/시뮬레이션과 구현/17780번 - 새로운 게임.py
+++ 연습/시뮬레이션과 구현/17780번 - 새로운 게임.py
@@ -1,93 +1,67 @@
 from collections import defaultdict
-def change_d(d):
+# 이동및 이동한 체스들 값 업데이트
+def white(x, y, nx, ny, index):
+    global flag
+    temp = q_Map[x][y]
+    if q_Map[nx][ny] == 0:
+        q_Map[nx][ny] = q_Map[x][y]
+    else:
+        q_Map[nx][ny] += q_Map[x][y]
+    q_Map[x][y] = 0
+    if len(q_Map[nx][ny]) >= 4:
+        flag = 1
+    for i in temp:
+        chess[i][0], chess[i][1] = nx, ny
+
+def red(x, y, nx, ny, index):
+    global flag
+    temp = q_Map[x][y]
+    if q_Map[nx][ny] == 0:
+        q_Map[nx][ny] = q_Map[x][y][::-1]
+    else:
+        q_Map[nx][ny] += q_Map[x][y][::-1]
+    q_Map[x][y] = 0
+    if len(q_Map[nx][ny]) >= 4:
+        flag = 1
+    for i in temp:
+        chess[i][0], chess[i][1] = nx, ny
+
+def blue(index):
+    x, y, d = chess[index]
     d += 1
     if d == 2:
         d = 0
     elif d == 4:
         d = 2
-    return d
-
-# 그곳으로 이동하면서
-# 1, 비어있는곳으로 가는지
-# 2. 내가 리스트인지 확인
-# 3. 4개가 쌓아지는지 -> 수정된 nex 리스트들에 대해서 q_Map, chess 업데이트
-def white(x, y, nx, ny, index):
-    global flag, q_Map, chess
-    d = chess[index][2]
-    new = q_Map[x][y][::1]
-    if q_Map[nx][ny] == 0:
-        q_Map[nx][ny] = new
-    elif q_Map[nx][ny] != 0:
-        old = q_Map[nx][ny][::1]
-        q_Map[nx][ny] = old + new
-    q_Map[x][y] = 0
-    for i in new:
-        x, y, td = chess[i]
-        chess[i] = [nx, ny, td]
-
-    if len(q_Map[nx][ny]) >= 4:
-        # print("end check")
-        # for q in q_Map:
-        #     print(q)
-        flag = 1
-
-
-def red(x, y, nx, ny, index):
-    global flag, q_Map, chess
-    d = chess[index][2]
-    new = q_Map[x][y][::-1]
-    if q_Map[nx][ny] == 0:
-        q_Map[nx][ny] = new
-    elif q_Map[nx][ny] != 0:
-        old = q_Map[nx][ny][::1]
-        q_Map[nx][ny] = old + new
-    q_Map[x][y] = 0
-    for i in new:
-        x, y, td = chess[i]
-        chess[i] = [nx, ny, td]
-    if len(q_Map[nx][ny]) >= 4:
-        # print("end check")
-        # for q in q_Map:
-        #     print(q)
-        flag = 1
-
-
-def blue(x, y, index):
-    global flag, q_Map, chess
-    d = chess[index][2]
-    nd = change_d(d)
-    chess[index] = [x, y, nd]
-    dx, dy = steps[nd]
+    chess[index][2] = d
+    dx, dy = steps[d]
     nx, ny = x + dx, y + dy
     if 0 <= nx < N and 0 <= ny < N:
         if Map[nx][ny] == 0:
             white(x, y, nx, ny, index)
         elif Map[nx][ny] == 1:
             red(x, y, nx, ny, index)
-
-    # 파랑 또는 넘어가는경우 방향만 바꺼주기
-
+    # 범위밖 또는 파랑색을 만나는경우 반대방향 및 좌표 유지
 
 
-
-steps = [(0, 1),(0, -1),(-1, 0),(1, 0)]
+steps= [(0, 1),(0, -1),(-1, 0),(1, 0)] # 오 왼 위 아래
+flag = 0
 N, K = map(int, input().split())
 Map = [list(map(int, input().split())) for _ in range(N)]
-q_Map = [[0]*N for _ in range(N)] # 0 or [(i, d), ... 위]
+q_Map = [[0]*N for _ in range(N)]
 chess = defaultdict()
-for i in range(K):
+for i in range(1, K+1):
     x, y, d = map(int, input().split())
-    chess[i + 1] = [x - 1, y - 1, d - 1]
-    q_Map[x - 1][y - 1] = [i + 1]
+    chess[i] = [x-1, y-1, d-1]
+    q_Map[x-1][y-1] = [i]
+# 맵과 체스 완성
 time = 1
 res = -1
-flag = 0
 while time <= 1000:
-    # move 도중에 말이 4개 쌓이면 브레이크
     for index in range(1, K + 1):
         x, y, d = chess[index]
-        # 해당번호가 이동가능한 경우
-        if len(q_Map[x][y]) == 1 or (q_Map[x][y][0] == index):
+        # 맨아래에 있는경우만 이동
+        if q_Map[x][y][0] == index:
             dx, dy = steps[d]
             nx, ny = x + dx, y + dy
             if 0 <= nx < N and 0 <= ny < N:
@@ -96,20 +70,13 @@ while time <= 1000:
                 elif Map[nx][ny] == 1:
                     red(x, y, nx, ny, index)
                 elif Map[nx][ny] == 2:
-                    blue(x, y, index)
+                    blue(index)
             else:
-                blue(x, y, index)
-            if flag:
-                break
-    #     print(index, flag)
-    #     for q in q_Map:
-    #         print(q)
-    #     print()
-    # # print(time)
-    # print(index, flag)
+                blue(index)
+        if flag:
+            break
     if flag:
         res = time
         break
     time += 1
 print(res)
-
