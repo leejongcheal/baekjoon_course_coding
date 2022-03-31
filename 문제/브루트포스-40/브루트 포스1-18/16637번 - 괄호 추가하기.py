84eaca85c616dc62965da521ee41eedd1ef68797
lee <aksndk123@naver.com>
2022-03-31 10:47:28 +0900

    **** 무조건 연습 및 eval 함수, 실수암기
    1. res = 0로 초기화해서 틀림, 정답이 양수인지 음수인지 확인을 하고 0을 써야함 -> 그냥 왠만하면 INF = 1e10해서 ,
    최댓값을 구할땐 -INF로 초기화
    최솟값을 구할떈 INF로 초기화 하는걸로 암기
    
    2. 문자열 수식 계산 eval 함수 암기꼭하기

--- 문제/브루트포스-40/브루트 포스1-18/16637번 - 괄호 추가하기.py
+++ 문제/브루트포스-40/브루트 포스1-18/16637번 - 괄호 추가하기.py
@@ -1,36 +1,46 @@
 from copy import deepcopy
-def dfs(visit, index, visit_index):
-    global res, number, oper
-    if index >= M - 1:
-        temp = ""
-        i = 0
-        temp_n = []
-        temp_o = []
-        while i < M:
-            if i < M - 1 and visit[i] == visit[i + 1] != 0:
-                val = eval(number[i] + oper[i] + number[i+1])
-                temp_n.append(str(val))
-                if i + 1 < len(oper):
-                    temp_o.append(oper[i + 1])
-                i += 2
-            else:
-                if i < M - 1:
-                    temp_n.append(number[i])
-                    temp_o.append(oper[i])
+def dfs(visit, now_index):
+    global res
+    if now_index >= M - 1:
+        val = 0
+        index = 0
+        while index < M:
+            if visit[index] != 1:
+                if index == 0:
+                    val = int(number[index])
                 else:
-                    temp_n.append(number[i])
-                i += 1
-        val = int(temp_n[0])
-        for i in range(len(temp_n)-1):
-            val = int(eval(str(val) + temp_o[i] + temp_n[i+1]))
+                    val = eval(str(val) + oper[index - 1] + number[index])
+                index += 1
+            else:
+                if index == 0:
+                    val = eval(number[index] + oper[index] + number[index+1])
+                else:
+                    sum_n = eval(number[index] + oper[index] + number[index+1])
+                    val = eval(str(val) + oper[index - 1] + str(sum_n))
+                index += 2
         res = max(res, val)
-    for i in range(index, M - 1):
-        # 괄호 넣어주는 경우
-        visit[i] = visit[i+1] = visit_index
-        dfs(visit, i+2, visit_index + 1)
-        visit[i] = visit[i+1] = 0
-        # 괄호 안치는 경우
-        dfs(visit, i + 1, visit_index)
+        return
+    # 현재 선택
+    visit[now_index], visit[now_index + 1] = 1, 1
+    tv = tuple(visit)
+    if tv not in visit_set:
+        visit_set.add(tv)
+        dfs(visit, now_index + 2)
+    visit[now_index], visit[now_index + 1] = -1, -1
+    # 현재선택안하는 경우
+    visit[now_index], visit[now_index + 1] = 0, 0
+    tv = tuple(visit)
+    if tv not in visit_set:
+        visit_set.add(tv)
+        dfs(visit, now_index + 2)
+    visit[now_index], visit[now_index + 1] = -1, -1
+    # 다음값을 넘김
+    visit[now_index] = 0
+    tv = tuple(visit)
+    if tv not in visit_set:
+        visit_set.add(tv)
+        dfs(visit, now_index + 1)
+    visit[now_index] = -1
 
 
 N = int(input())
@@ -38,10 +48,10 @@ L = list(input())
 number = [L[i] for i in range(N) if i % 2 == 0]
 oper = [L[i] for i in range(N) if i % 2 == 1]
 M = len(number)
-visit = [0]*len(number)
+visit_set = set()
 res = -(2**32)
 if N == 1:
     res = int(number[0])
 else:
-    dfs(visit, 0, 1)
+    dfs([-1]*M, 0)
 print(res)
