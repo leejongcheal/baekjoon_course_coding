05a0dfb104fbcf45a09e822cfc30031cad5d74ad
lee <aksndk123@naver.com>
2022-03-31 16:05:33 +0900

    * 시간초과 뜸
    조합의 경우 조사하는것중 뻥 번호가 있어서 조합으로 풀면 시간초과 걸리는듯
    
    이를 위해 친구가 있는 번호에 대해서만 조합으로 바꿔도 시간초과 뜸 -> dfs풀이 강제하는듯?

--- 문제/브루트포스-40/브루트 포스1-18/17089번 - 세 친구.py
+++ 문제/브루트포스-40/브루트 포스1-18/17089번 - 세 친구.py
@@ -1,25 +1,5 @@
 from collections import defaultdict
-def dfs(now, tracked):
-    global res
-    if len(tracked) == 3:
-        val = 0
-        a, b, c = tracked
-        val += len(friend[a]) + len(friend[b]) + len(friend[c])
-        if b in friend[a]:
-            val -= 2
-        if c in friend[a]:
-            val -= 2
-        if c in friend[b]:
-            val -= 2
-        res = min(res, val)
-        return
-    for next in friend[now]:
-        if len(tracked) == 1 and len(friend[next]) > 1:
-            dfs(next, tracked + [next])
-        else:
-            a = tracked[0]
-            if next in friend[a]:
-                dfs(next, tracked + [next])
+from itertools import combinations
 
 
 INF = int(1e10)
@@ -30,9 +10,15 @@ for _ in range(M):
     a, b = map(int, input().split())
     friend[a].add(b)
     friend[b].add(a)
+L = []
 for i in range(1, N + 1):
     if len(friend[i]) != 0:
-        dfs(i,[i])
+        L.append(i)
+for comb in combinations(L, 3):
+    a, b, c = comb
+    if b in friend[a] and c in friend[a] and c in friend[b]:
+        val = len(friend[a]) + len(friend[b]) + len(friend[c]) - 6
+        res = min(res, val)
 if res == INF:
     print(-1)
 else:
