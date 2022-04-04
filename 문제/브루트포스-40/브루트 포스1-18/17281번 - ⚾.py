0e9495204e98b011f812ae429ce401fac887444a
lee <aksndk123@naver.com>
2022-04-04 13:56:32 +0900

    답지참고
    dfs로 풀었더니 920ms로 풀리는 답지 가져옴 흠...

--- 문제/브루트포스-40/브루트 포스1-18/17281번 - ⚾.py
+++ 문제/브루트포스-40/브루트 포스1-18/17281번 - ⚾.py
@@ -1,40 +1,50 @@
-from itertools import permutations
-from collections import defaultdict
+import sys
+
+input = sys.stdin.readline
+
+def dfs(cnt):
+    global ans
+    if cnt == 9:
+        start, score = 0, 0
+        for inning in a:
+            out, b1, b2, b3 = 0, 0, 0, 0
+            while out <= 2:
+                p = select[start]
+                if inning[p] == 0:
+                    out += 1
+                elif inning[p] == 1:
+                    score += b3
+                    b1, b2, b3 = 1, b1, b2
+                elif inning[p] == 2:
+                    score += b2 + b3
+                    b1, b2, b3 = 0, 1, b1
+                elif inning[p] == 3:
+                    score += b1 + b2 + b3
+                    b1, b2, b3 = 0, 0, 1
+                else:
+                    score += b1 + b2 + b3 + 1
+                    b1, b2, b3 = 0, 0, 0
+
+                start += 1
+                start %= 9
+
+        ans = max(ans, score)
+        return
+
+    for i in range(9):
+        if c[i]:
+            continue
+        c[i] = 1
+        select[i] = cnt
+        dfs(cnt + 1)
+        c[i] = 0
+        select[i] = 0
 
 
-res = 0
-N = int(input())
-L = [list(map(int, input().split())) for _ in range(N)]
-for order in permutations(range(2,10), 8):
-    order = list(order)
-    order.insert(3, 1)
-    # print(order)
-    inning = 0
-    index = 0
-    out = 0
-    runner = [0, 0, 0]
-    score = 0
-    while inning < N:
-        j = order[index % 9]
-        now = L[inning][j - 1]
-        if now == 1:
-            score += runner[2]
-            runner = [1, runner[0], runner[1]]
-        elif now == 2:
-            score += runner[1] + runner[2]
-            runner = [0, 1, runner[0]]
-        elif now == 3:
-            score += sum(runner)
-            runner = [0, 0, 1]
-        elif now == 4:
-            score += 1 + sum(runner)
-            runner = [0, 0, 0]
-        elif now == 0:
-            out += 1
-        if out == 3:
-            inning += 1
-            out = 0
-            runner = [0, 0, 0]
-        index += 1
-    res = max(res, score)
-print(res)
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+select, c = [0 for _ in range(9)], [0 for _ in range(9)]
+select[3], c[3] = 0, 1
+ans = 0
+dfs(1)
+print(ans)
