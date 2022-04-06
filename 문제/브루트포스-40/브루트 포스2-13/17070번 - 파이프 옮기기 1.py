9274ef49c5242ee7204584e463d51248ee0933ed
lee <aksndk123@naver.com>
2022-04-06 09:42:26 +0900

    ** 시간초과 하는 이유가 조건문이 길어서인듯
    풀긴하는데 이런식의 시간초과 는 솔직히 배울게 없어보임
    
    일단 1. x2, y2만의 조건을 가지고 풀이 왜냐면 x1,y1은 따라오는거기때문에 따질필요가 없음
    
    shape결정 또는 대각인지 결정하는걸 더 똑똑하게 생각해서 풀기
    
    x1의 값을 굳이 기억하지말고 x2 y2에 모양만으로 풀어보기

--- 문제/브루트포스-40/브루트 포스2-13/17070번 - 파이프 옮기기 1.py
+++ 문제/브루트포스-40/브루트 포스2-13/17070번 - 파이프 옮기기 1.py
@@ -1,23 +1,13 @@
-from collections import deque
 width_steps = [((0, 1, 0),(0, 1, 0), 0),((0,1, 0),(1, 1, 1), 2)]
 height_steps = [((1,0, 0),(1,0, 0), 1),((1,0, 0),(1,1,1), 2)]
 diagonal_steps = [((1,1 ,1),(0,1,0), 0),((1,1,1),(1,0,0), 1),((1,1,1),(1,1,1), 2)]
-
-N = int(input())
-Map = [list(map(int, input().split())) for _ in range(N)]
-pipe = [(0, 0), (0, 1)]
-res = 0
-q = deque()
-q.append((0, 0, 0, 1, 0))
-while q:
-    x1, y1, x2, y2, shape = q.popleft()
-    if res > 1000000:
-        break
-    if (x1 == N - 1 and y1 == N - 1) or (x2 == N - 1 and y2 == N - 1):
+def dfs(x1, y1, x2, y2, shape):
+    global res
+    if res >= 1000000:
+        return
+    if x2 == N - 1 and y2 == N - 1:
         res += 1
-        continue
-    # 가로
-    now_steps = []
+        return
     if shape == 0:
         now_steps = width_steps
     # 세로
@@ -26,18 +16,21 @@ while q:
     # 대각선
     else:
         now_steps = diagonal_steps
-    # 다음 n1 n2들이 벽이 아니거나 범위를 만족한다면 q에 넣어주기
     for step1, step2, nshape in now_steps:
         dx1, dy1, f1 = step1
         dx2, dy2, f2 = step2
         nx1, ny1 = x1 + dx1, y1 + dy1
         nx2, ny2 = x2 + dx2, y2 + dy2
-        if 0 <= nx1 < N and 0 <= ny1 < N and 0 <= nx2 < N and 0 <= ny2 < N\
-            and Map[nx1][ny1] != 1 and Map[nx2][ny2] != 1:
-            if f1 and (Map[x1 + 1][y1] == 1 or Map[x1][y1 + 1] == 1):
-                continue
+        if 0 <= nx2 < N and 0 <= ny2 < N\
+            and Map[nx2][ny2] != 1:
             if f2 and (Map[x2 + 1][y2] == 1 or Map[x2][y2 + 1] == 1):
                 continue
-            q.append((nx1, ny1, nx2, ny2, nshape))
+            dfs(nx1, ny1, nx2, ny2, nshape)
+
+
+N = int(input())
+Map = [list(map(int, input().split())) for _ in range(N)]
+res = 0
+dfs(0,0,0,1,0)
 print(res)
 
