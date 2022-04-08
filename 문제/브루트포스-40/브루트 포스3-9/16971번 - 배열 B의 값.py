955b6162fef162de99e8f9d6c3c7f2f5c4b5626e
lee <aksndk123@naver.com>
2022-04-08 17:28:26 +0900

    * 왜 틀렸나 했더니 처음 이동없이 합구하는 부분 틀렸나봄 그냥 브루트로 합구해서 알고리즘 돌리니 통과 ㅋㅋㅋㅋㅋㅋㅋ

--- 문제/브루트포스-40/브루트 포스3-9/16971번 - 배열 B의 값.py
+++ 문제/브루트포스-40/브루트 포스3-9/16971번 - 배열 B의 값.py
@@ -10,38 +10,24 @@ def slove_sum(Map):
     res += Map[0][0] + Map[0][M-1] + Map[N-1][M-1] + Map[N-1][0]
     return res
 
-res = 0
+
 N, M = map(int, input().split())
 Map = [list(map(int, input().split())) for _ in range(N)]
-first_sum = max(slove_sum(Map), res)
-diff = 0 # 이거의 최댓값 찾기
-#  행 조사
-n = 0
-for i in range(N):
-    diff = max(diff, 2*sum(Map[n]) - Map[n][0] - Map[n][M-1] - 2*sum(Map[i]) + Map[i][0] + Map[i][M-1])
-
-n = N - 1
-for i in range(N):
-    diff = max(diff, 2*sum(Map[n]) - Map[n][0] - Map[n][M-1] - 2*sum(Map[i]) + Map[i][0] + Map[i][M-1])
-
-# 열 조사
-m = 0
-sum_m = 0
-for i in range(N):
-    sum_m += Map[i][m]
-for i in range(M):
-    sum_i = 0
-    for j in range(N):
-        sum_i += Map[j][i]
-    diff = max(diff, 2*sum_m - 2*sum_i - Map[0][m] - Map[N-1][m] + Map[0][i] + Map[N-1][i])
-
-m = M-1
-sum_m = 0
-for i in range(N):
-    sum_m += Map[i][m]
-for i in range(M):
-    sum_i = 0
-    for j in range(N):
-        sum_i += Map[j][i]
-    diff = max(diff, 2*sum_m - 2*sum_i - Map[0][m] - Map[N-1][m] + Map[0][i] + Map[N-1][i])
+first_sum = slove_sum(Map)
+diff = 0# 이거의 최댓값 찾기
+for a in [0, N-1]:
+    for b in range(1, N-1):
+        new = sum(Map[a])*2 - sum(Map[b])*2 + Map[b][0] + Map[b][M-1] - Map[a][0] - Map[a][M-1]
+        diff = max(diff, new)
+# 열
+for a in [0, M-1]:
+    col_a = 0
+    for i in range(N):
+        col_a += Map[i][a]
+    for b in range(1, M-1):
+        col_b = 0
+        for i in range(N):
+            col_b += Map[i][b]
+        new = 2*col_a - 2*col_b + Map[0][b] + Map[N-1][b] - Map[0][a] - Map[N-1][a]
+        diff = max(diff, new)
 print(first_sum + diff)
