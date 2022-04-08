54881a11e4a5a380d51d93e150c3b5b72078d9fe
lee <aksndk123@naver.com>
2022-04-08 16:08:10 +0900

    * union-parent 응용 해서 풀어봄
    밑의 과정에서 생각해보니 1 -> 도착지로 올때, 통과해온 모든 점들은 같은 도착지를 공유함. 이를 통해 중복을 줄이는 개량된 union으로 풀이함
    
    아마 N*N조사하는 부분을 최대한 줄여야함.
    생각해보니깐 한칸씩 움직일 필요없음. 결국에 도착지는 정해져있는거임. 따라서 모든 칸에 대해서 도착지를 구하는 방식에서 효율적인 방법을 찾아야함

--- 문제/브루트포스-40/브루트 포스3-9/16957번 - 체스판 위의 공.py
+++ 문제/브루트포스-40/브루트 포스3-9/16957번 - 체스판 위의 공.py
@@ -1,37 +1,52 @@
 from collections import deque
+def find(L):
+    global parent
+    x, y = L
+    if parent[x][y] == L:
+        return [x, y]
+    parent[x][y] = find(parent[x][y])
+    return parent[x][y]
 
-
-def find_chess(Map):
-    chess = []
-    for i in range(N):
-        for j in range(M):
-            if Map[i][j] != 0:
-                chess.append((Map[i][j], i, j))
-    return chess
+def union(a, b): # 리스트형태로 넣어주기
+    global parent
+    A = find(a)
+    B = find(b)
+    ax, ay = A
+    bx, by = B
+    if Map[ax][ay] > Map[bx][by]:
+        parent[ax][ay] = B
+    else:
+        parent[bx][by] = A
 
 INF = 1e10
 steps = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
 N, M = map(int, input().split())
 Map = [list(map(int, input().split())) for _ in range(N)]
-res = [[0]*M for _ in range(N)]
-chess = []
+parent = [[0]*M for _ in range(N)]
 for i in range(N):
     for j in range(M):
-        chess.append((1, i, j))
-while chess:
-    temp = [[0]*M for _ in range(N)]
-    for cnt, x, y in chess:
-        min_val = Map[x][y]
-        mx, my = -1, -1
-        for dx, dy in steps:
-            nx, ny = x + dx,y + dy
-            if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] < min_val:
-                min_val = Map[nx][ny]
-                mx, my = nx, ny
-        if mx != -1:
-            temp[mx][my] += cnt
-        elif mx == -1:
-            res[x][y] += cnt
-    chess = find_chess(temp)
+        parent[i][j] = [i, j]
+for i in range(N):
+    for j in range(M):
+        if find([i,j]) == [i, j]:
+            x, y = i, j
+            while 1: # 이동이 없을떄까지 검사
+                min_val = Map[x][y]
+                mx, my = -1, -1
+                for dx, dy in steps:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] < min_val:
+                        min_val = Map[nx][ny]
+                        mx, my = nx, ny
+                if mx != -1:
+                    union([i, j], [mx, my])
+                    break
+                elif mx == -1:
+                    break
+res = [[0]*M for _ in range(N)]
+for i in range(N):
+    for j in range(M):
+        x, y = find([i, j])
+        res[x][y] += 1
 for r in res:
     print(*r)
