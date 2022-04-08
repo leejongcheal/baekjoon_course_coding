d348e425ac8a61e85e972dde55ce9843d4aefb60
lee <aksndk123@naver.com>
2022-04-08 10:39:31 +0900

    그냥 다익 벨만포드 시간복잡도 차이만 알고가기
    이문제는 이미 파이썬으로 풀지말라는 문제임. 그러나 다잌이 아닌 벨만으로 풀어야한다만 알자.
    
    같은 알고리즘쓰고 차이도 변수지정 차이밖에 없는데 하나는 맞고 내껀 틀림 ㅋㅋ

--- 문제/브루트포스-40/브루트 포스3-9/16958번 - 텔레포트.py
+++ 문제/브루트포스-40/브루트 포스3-9/16958번 - 텔레포트.py
@@ -1,31 +1,24 @@
-import heapq
-def solve(a, b):
-    DP = [INF]*N
-    DP[a] = 0
-    q = [(0, a)]
-    while q:
-        now_dist, now = heapq.heappop(q)
-        if now_dist > DP[now]:
-            continue
-        if now == b:
-            return now_dist
-        x, y, s = graph[now]
-        for next in range(N):
-            nx, ny, ns = graph[next]
-            dist = abs(x - nx) + abs(y - ny)
-            if s == ns == 1 and T < dist:
-                dist = T
-            if now_dist + dist < DP[next]:
-                DP[next] = now_dist + dist
-                heapq.heappush(q, (DP[next], next))
-
-INF = int(1e10)
-N, T = map(int, input().split())
-graph = []
-# 다익스트라로 푸는데 텔포의 경우도 생각 거리는 하나하나 다 계산해야하는듯 ㅅㅂ ㅋㅋ
+import sys
+N, T = map(int, sys.stdin.readline().rstrip().split())
+Map = [[0]*N for _ in range(N)]
+position = []
+teleport = []
 for _ in range(N):
-    s, x, y = map(int, input().split())
-    graph.append((x,y,s))
+    s, x, y = map(int, sys.stdin.readline().rstrip().split())
+    teleport.append(s)
+    position.append([x,y])
+for i in range(N):
+    for j in range(N):
+        xi, yi = position[i]
+        xj, yj = position[j]
+        diff = abs(xi - xj) + abs(yi - yj)
+        if teleport[i] == teleport[j] == 1:
+            diff = min(diff, T)
+        Map[i][j] = diff
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            Map[i][j] = min(Map[i][j], Map[i][k] + Map[k][j])
 for _ in range(int(input())):
-    a, b = map(int, input().split())
-    print(solve(a - 1, b - 1))
+    a, b = map(int, sys.stdin.readline().rstrip().split())
+    print(Map[a-1][b-1])
