9ebabc759c661e05672562e467df8a4e91cee181
lee <aksndk123@naver.com>
2022-04-11 13:38:37 +0900

    **** 아주 멋진 문제
    처음에 섬별로 갈수있는 최소거리를 미리 구한다음에 풀면 쉬운데 더럽게 풀었고 탐색처리하는게 복잡해서 틀림
    간단하게 만드니탐색자체도 쉬워짐.... 설계문제 + 처음에 문제를 잘못이해함. 다리를 무조건 그려서 구해야 쉬운문제로 착각함
    
    1. 길을 채우면서 가는 경우 지울때, 겹치는 간선도 지워버려서 좌표로 외워서 나중에 그리기로 바꿈
    
    2. 겹치는 부분은 1로 치는게 아닌 겹치는 만큼 + 해주는거임.. 좌표로만 판별하기로 바꿈
    
    3. 1의 번호에 여러개의 다리가 생기는 경우가 있는데 이를 탐색에 녹여내지못함
    
    4. 1->2->3  5->6->7->2 의 경우 탐색에 들어가짐 못함 아오 그냥 grapg거리별로 만들어서 dfs탐색해서 결과중 작은값을 답으로 하기 ㅋㅋ
    ----> 이건 내가 잘못생각함 어쨋든 모두 연결이 되면 어느 지점에서 시작하든 연결이 되어야함 탐색부분은 잘못된건 맞는데 저생각은 틀림

--- 문제/브루트포스-40/브루트 포스3-9/17472번 - 다리 만들기 2.py
+++ 문제/브루트포스-40/브루트 포스3-9/17472번 - 다리 만들기 2.py
@@ -9,63 +9,29 @@ def island_number(x, y):
             Map[nx][ny] = island_cnt
             group.append((nx, ny))
             island_number(nx, ny)
-def check(graph):
-    visit = [0]*island_cnt
-    q = deque()
-    q.append(1)
-    visit[0] = 1
-    while q:
-        now = q.popleft()
-        for next in graph[now]:
-            if visit[next - 1] == 0:
-                visit[next - 1] = 1
-                q.append(next)
-    if sum(visit) == island_cnt:
-        return 1
-    else:
-        return 0
-
-def connect_dfs(is_num, graph, connect):
-    global res
-    if check(graph):
-        cnt = 0
-        for x, y, nx, ny in connect:
-            dx, dy = abs(x - nx), abs(y - ny)
-            cnt += max(dx, dy) - 1
-        res = min(res, cnt)
-    visit = [0]*island_cnt
+def make_graph():
     for island in range(island_cnt):
         for x, y in island_group[island]:
             for dx, dy in steps:
                 nx, ny = x + dx, y + dy
-                # 다리 건설가능한 후보의 경우
                 if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] == 0:
-                    flag = 0
                     d = 1
+                    flag = 0
                     while 1:
                         nx += dx
                         ny += dy
                         if not (0 <= nx < N and 0 <= ny < M):
                             break
-                        # 다리 건설 가능한 경우
-                        if Map[nx][ny] != 0:
+                        elif Map[nx][ny] != 0:
                             flag = 1
                             break
                         d += 1
-                    # 다리 건설가능하고 거리 2이상 + 한번 연결된 적있는 섬이 아닌 경우
-                    if flag and d >= 2 and Map[nx][ny] not in graph[Map[x][y]]:
-                        temp = deepcopy(graph)
-                        temp[Map[x][y]].append(Map[nx][ny])
-                        temp[Map[nx][ny]].append(Map[x][y])
-                        connect_dfs(Map[nx][ny], temp, connect + [(x,y,nx,ny)])
-                        # 지금 섬에서 연결을 여러개 하는 경우 생각을 못함
+                    if flag and d >= 2:
+                        a, b = island, Map[nx][ny] - 1
+                        graph[a][b] = min(graph[a][b], d)
+                        graph[b][a] = min(graph[b][a], d)
 
-                        # 다리 연결 풀기
-                        tx, ty = x + dx, y + dy
-
-
-
-start_x, start_y = -1, -1
+INF = int(1e10)
 steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
 N, M = map(int, input().split())
 Map = [list(map(int, input().split())) for _ in range(N)]
@@ -75,8 +41,6 @@ visit = [[0]*M for _ in range(N)]
 for i in range(N):
     for j in range(M):
         if visit[i][j] == 0 and Map[i][j] != 0:
-            if start_x == -1:
-                start_x, start_y = i, j
             visit[i][j] = 1
             Map[i][j] = island_cnt
             group = [(i, j)]
@@ -84,9 +48,22 @@ for i in range(N):
             island_cnt += 1
             island_group.append(group)
 island_cnt -= 1
-res = int(1e10)
-connect_dfs(1, defaultdict(list), [])
-if res != int(1e10):
+graph = [[INF]*island_cnt for _ in range(island_cnt)]
+make_graph()
+res = INF
+q = deque()
+q.append(([0], 0))
+
+while q:
+    tracked, dist = q.popleft()
+    if len(tracked) == island_cnt:
+        res = min(res, dist)
+        continue
+    for now in tracked:
+        for next in range(island_cnt):
+            if graph[now][next] != INF and next not in tracked:
+                q.append((tracked+[next], dist + graph[now][next]))
+if res != INF:
     print(res)
 else:
     print(-1)
