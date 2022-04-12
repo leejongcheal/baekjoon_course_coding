0d131b60b6f2e458a42be689319d867e353f4372
lee <aksndk123@naver.com>
2022-04-12 17:02:57 +0900

    *복잡하네 진짜
    와 답이 12가 나와야되는데 13나와서 개같앗는데
    넣을때 dist+1 or dist+2값이 들어가서 값이 12 or 13의 값을 가질수 있음을 기억해야함 이거떄문에 왜틀렸나 고민했네 + 근데 어쩌피 시간초과 아오

---
+++ 문제/그래프와 BFS-15/BFS 알고리즘 - 12/16959번 - 체스판 여행 1.py
@@ -0,0 +1,74 @@
+from collections import deque, defaultdict
+knight = [(-2, -1),(-2, 1),(-1, 2),(1, 2),(2, 1),(2, -1),(1, -2),(-1, -2)]
+bishop = [(-1, -1),(-1, 1),(1, 1),(1, -1)]
+lock = [(1, 0),(-1, 0),(0, 1),(0, -1)]
+N = int(input())
+INF = int(1e10)
+Map = [list(map(int, input().split())) for _ in range(N)]
+steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
+# graph [나이트 비숍 룩 0  1 2] [해당값에서][해당값으로이동]
+for i in range(N):
+    for j in range(N):
+        if Map[i][j] == 1:
+            sx, sy = i, j
+# q = 현재번호(1시작 N^N이면 끝) x y 말타입(0,1,2 나이트 비숍 룩) 거리
+q = deque()
+q.append((1, sx, sy, 0, 0))
+q.append((1, sx, sy, 1, 0))
+q.append((1, sx, sy, 2, 0))
+res = -1
+visit = defaultdict(int) # 번호 좌표 타입 = dist
+visit[(1, sx, sy, 0)] = 0
+visit[(1, sx, sy, 1)] = 0
+visit[(1, sx, sy, 2)] = 0
+res = int(1e10)
+while q:
+    num, x, y, type, dist = q.popleft()
+    if Map[x][y] == num + 1:
+        num += 1
+        if num == N*N:
+            res = min(dist, res)
+            continue
+        visit[(num, x, y, type)] = dist
+    # 나이트 이동 0
+    if type == 0:
+        nd = dist
+    else:
+        nd = dist + 1
+    for dx, dy in knight:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < N:
+            if visit[(num, nx, ny, 0)] == 0 or visit[(num, nx, ny, 0)] > nd + 1:
+                visit[(num, nx, ny, 0)] = nd + 1
+                q.append((num, nx, ny, 0, nd + 1))
+    # 비숍 이동 1
+    if type == 1:
+        nd = dist
+    else:
+        nd = dist + 1
+    for dx, dy in bishop:
+        nx, ny = x + dx, y + dy
+        while 0 <= nx < N and 0 <= ny < N:
+            if visit[(num, nx, ny, 1)] == 0 or visit[(num, nx, ny, 1)] > nd + 1:
+                visit[(num, nx, ny, 1)] = nd + 1
+                q.append((num, nx, ny, 1, nd + 1))
+            nx += dx
+            ny += dy
+    # 룩이동  2
+    if type == 2:
+        nd = dist
+    else:
+        nd = dist + 1
+    for dx, dy in lock:
+        nx, ny = x + dx, y + dy
+        while 0 <= nx < N and 0 <= ny < N:
+            if visit[(num, nx, ny, 2)] == 0 or visit[(num, nx, ny, 2)] > nd + 1:
+                visit[(num, nx, ny, 2)] = nd + 1
+                q.append((num, nx, ny, 2, nd + 1))
+            nx += dx
+            ny += dy
+if res >= int(1e10):
+    print(-1)
+else:
+    print(res)
+
