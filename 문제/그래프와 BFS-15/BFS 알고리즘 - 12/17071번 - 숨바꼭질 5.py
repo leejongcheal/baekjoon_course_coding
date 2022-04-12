3b1a65a3017103c4f65de125f4ebc301c95447ca
lee <aksndk123@naver.com>
2022-04-12 12:19:26 +0900

    ***** 아주 까다롭고 훌륭한 문제
    1. 문제 풀이 과정부터 까다로움 BFS는 맞는데 답을 향해서 달려가는것이 아니라 모든 위치에 대해서 구한다음에(DP를 다구한다음에) 가능한 답의 후보들에 대해서 DP값이 있고 가능하다면 res 에 넣어주면서 제일 작은값을 출력하는 문제
    
    2. 발상자체 해당값에 도달하는데 홀짝에 나누어서 제일 적은시간에 도달하는걸 갈수있는 모든 좌표에 대해서 구함 -> 이과정에서 visit을 홀짝으로 나누었고, 최대 50만개이므로 최대 100만개의 값을 가질수 있음.
    
    3. 홀짝으로 나눈이유에서 5라는 값에 4초에 도달한다면 4초 이후의 짝수시간으로 5에 도달 할수있음. 앞뒤로 움직이면 됨으로 이를 활용해서 푼문제
    
    4. res구하는 과정에서
    odd_visit[K] != 0 and odd_visit[K] <= t and t%2 == 1:
    위의 조건문을 하나하나 보자. 일단 도달할수 있으면 0이 아닐꺼임. 또한 t초 내로 도달해야 해당 t초에 K값으로 갈수 있을꺼임.[또한 K에 t초 이하로 홀수인 시간으로 돌아 왔으니 t의 값도 홀수가 되어야 +2를 여러번해서 도달할수 있음.]
    
    아주 정확한 의미이해를 했어야 풀수있는 문제..

--- 문제/그래프와 BFS-15/BFS 알고리즘 - 12/17071번 - 숨바꼭질 5.py
+++ 문제/그래프와 BFS-15/BFS 알고리즘 - 12/17071번 - 숨바꼭질 5.py
@@ -1,33 +1,46 @@
+from collections import defaultdict
 N, K = map(int, input().split())
 q = []
 visit = set()# t%2, 위치
+odd_visit = defaultdict(int)
+even_visit = defaultdict(int)
 q.append((N))
 t = 1
-res = -1
+INF = int(1e10)
+res = INF
+temp_k = K
 if N == K:
     res = 0
-while q:
-    if res != -1:
-        break
-    temp = []
-    K += t
-    if K > 500000:
-        break
+else:
     while q:
-        if res != -1:
+        temp = []
+        temp_k += t
+        if K > 500000:
             break
-        x = q.pop()
-        if x > K:
-            oper = [x-1,x+1]
-        else:
-            oper = [x-1,x+1,x*2]
-        for nx in oper:
-            if nx == K:
-                res = t
-                break
-            elif 0 <= nx <= 500000 and (t%2, nx) not in visit:
-                visit.add((t%2, nx))
-                temp.append(nx)
+        while q:
+            x = q.pop()
+            for nx in [x-1,x+1,x*2]:
+                if 0 <= nx <= 500000:
+                    if t%2 == 0 and even_visit[nx] == 0:
+                        temp.append(nx)
+                        even_visit[nx] = t
+                    elif t%2 == 1 and odd_visit[nx] == 0:
+                        temp.append(nx)
+                        odd_visit[nx] = t
+        t += 1
+        q = temp
+t = 1
+# 홀수초내에 방문을 했다는건 예를들어서 최소 3초에 방문했다면 3,5,7,9초도 가능하다는 뜻
+while K <= 5000000:
+    K += t
+    if even_visit[K] != 0 and even_visit[K] <= t and t%2 == 0:
+        res = min(res, t)
+    if odd_visit[K] != 0 and odd_visit[K] <= t and t%2 == 1:
+        # t초 보다작다면 t초에 그값을 만들수 있다는 뜻이므로 t의값을 비교하면됨.
+        # 그러나 t초보다 큰값이면 t초에 그그값에 도달할수 없어서 답자체가 안됨.
+        res = min(res, t)
     t += 1
-    q = temp
-print(res)
+if res == INF:
+    print(-1)
+else:
+    print(res)
