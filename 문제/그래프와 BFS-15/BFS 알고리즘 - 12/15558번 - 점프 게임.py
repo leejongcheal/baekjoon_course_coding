1639c0c44be9620a1e3a71669b87ef7bb1d438e1
lee <aksndk123@naver.com>
2022-04-12 10:17:44 +0900

    간단한 BFS인데 메모리초과뜸 ㅋㅋ

--- 문제/그래프와 BFS-15/BFS 알고리즘 - 12/15558번 - 점프 게임.py
+++ 문제/그래프와 BFS-15/BFS 알고리즘 - 12/15558번 - 점프 게임.py
@@ -1,23 +1,26 @@
 from collections import deque
 N, K = map(int, input().split())
-L = [list(map(int, input().split())) for _ in range(2)]
+L = [list(input()) for _ in range(2)]
 q = deque()
-q.append((1, 0, 0))
+q.append((0, 0, 0))
 res = 0
 while q:
     t, x, y = q.popleft()
     if y >= N:
         res = 1
         break
-    if y - 1 >= 0 and y - 1 > t and L[x][y-1] == 1:
-        q.append((t+1,x, y-1))
+    if 0 <= y - 1 < N and y - 1 > t and L[x][y-1] == "1":
+        q.append((t+1, x, y-1))
     if y + 1 >= N:
         res = 1
         break
-    elif y + 1 >= 0 and y + 1 > t and L[x][y+1] == 1:
-        q.append((t+1,x,y+1))
-    if y + k >= N:
+    elif 0 <= y + 1 < N and y + 1 > t and L[x][y+1] == "1":
+        q.append((t+1, x ,y+1))
+    if y + K >= N:
         res = 1
-    elif 0 <= y + k < N and y + k > t and L[x^1][]
+        break
+    elif 0 <= y + K < N and y + K > t and L[x^1][y+K] == "1":
+        q.append((t+1, x^1, y + K))
+print(res)
 
 
