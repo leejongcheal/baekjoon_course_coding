2a369ecdee295dd3755c3b131955e3b67df435b4
lee <aksndk123@naver.com>
2022-04-14 13:42:29 +0900

    ** 체스판1과 비교 visit의 key를 늘리는게 아닌 value에 추가해서 시간초과를 피함
    비교횟수까지 알아야해서 visit의 key에 비교횟수까지 추가해서 푸니깐 -> 시간초과...
    
    차라리 visit에 [dist, 말바꾼횟수]로 해서 visit비교할때, dist가 더작으면 업데이트 또는 (dist가 같고 횟수가 더적은경우)에도 업데이트하면 체스판1이랑 거의 비슷한 경우의수를 가짐 이를 통해서 시간초과를 해결함

--- 문제/그래프와 BFS-15/BFS 알고리즘 - 12/16952번 - 체스판 여행 2.py
+++ 문제/그래프와 BFS-15/BFS 알고리즘 - 12/16952번 - 체스판 여행 2.py
@@ -1,11 +1,12 @@
 from collections import deque, defaultdict
-knight = [(-2, -1),(-2, 1),(-1, 2),(1, 2),(2, 1),(2, -1),(1, -2),(-1, -2)]
-bishop = [(-1, -1),(-1, 1),(1, 1),(1, -1)]
-lock = [(1, 0),(-1, 0),(0, 1),(0, -1)]
+
+knight = [(-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2)]
+bishop = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
+lock = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 N = int(input())
 INF = int(1e10)
 Map = [list(map(int, input().split())) for _ in range(N)]
-steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
+steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 # graph [나이트 비숍 룩 0  1 2] [해당값에서][해당값으로이동]
 for i in range(N):
     for j in range(N):
@@ -17,10 +18,10 @@ q.append((1, sx, sy, 0, 0, 0))
 q.append((1, sx, sy, 1, 0, 0))
 q.append((1, sx, sy, 2, 0, 0))
 res = []
-visit = defaultdict(int) # 번호/ 좌표 /타입 = dist/ 말바꾼 횟수
-visit[(1, sx, sy, 0, 0)] = 0
-visit[(1, sx, sy, 1, 0)] = 0
-visit[(1, sx, sy, 2, 0)] = 0
+visit = defaultdict(int)  # [번호,좌표,타입] 를 key로 가지고 원소를 [작은dist, 작은 말바꾼 횟수]
+visit[(1, sx, sy, 0)] = 0
+visit[(1, sx, sy, 1)] = 0
+visit[(1, sx, sy, 2)] = 0
 temp1 = []
 temp2 = []
 while q:
@@ -29,10 +30,10 @@ while q:
         num, x, y, type, dist, change = q.pop()
         if Map[x][y] == num + 1:
             num += 1
-            if num == N*N:
+            if num == N * N:
                 res.append((change, dist))
                 continue
-            visit[(num, x, y, type, change)] = dist
+            visit[(num, x, y, type)] = [dist, change]
         # 나이트 이동 0
         if type == 0:
             nd = dist
@@ -43,8 +44,9 @@ while q:
         for dx, dy in knight:
             nx, ny = x + dx, y + dy
             if 0 <= nx < N and 0 <= ny < N:
-                if visit[(num, nx, ny, 0, nc)] == 0 or visit[(num, nx, ny, 0, nc)] > nd + 1:
-                    visit[(num, nx, ny, 0, nc)] = nd + 1
+                if visit[(num, nx, ny, 0)] == 0 or visit[(num, nx, ny, 0)][0] > nd + 1 \
+                        or (visit[(num, nx, ny, 0)][0] == nd + 1 and visit[(num, nx, ny, 0)][1] > nc):
+                    visit[(num, nx, ny, 0)] = [nd + 1, nc]
                     if nd == dist:
                         temp1.append((num, nx, ny, 0, nd + 1, nc))
                     else:
@@ -59,8 +61,11 @@ while q:
         for dx, dy in bishop:
             nx, ny = x + dx, y + dy
             while 0 <= nx < N and 0 <= ny < N:
-                if visit[(num, nx, ny, 1, nc)] == 0 or visit[(num, nx, ny, 1, nc)] > nd + 1:
-                    visit[(num, nx, ny, 1, nc)] = nd + 1
+                # if visit[(num, nx, ny, 1, nc)] == 0 or visit[(num, nx, ny, 1, nc)] > nd + 1:
+                #    visit[(num, nx, ny, 1, nc)] = nd + 1
+                if visit[(num, nx, ny, 1)] == 0 or visit[(num, nx, ny, 1)][0] > nd + 1 \
+                        or (visit[(num, nx, ny, 1)][0] == nd + 1 and visit[(num, nx, ny, 1)][1] > nc):
+                    visit[(num, nx, ny, 1)] = [nd + 1, nc]
                     if nd == dist:
                         temp1.append((num, nx, ny, 1, nd + 1, nc))
                     else:
@@ -77,8 +82,11 @@ while q:
         for dx, dy in lock:
             nx, ny = x + dx, y + dy
             while 0 <= nx < N and 0 <= ny < N:
-                if visit[(num, nx, ny, 2, nc)] == 0 or visit[(num, nx, ny, 2, nc)] > nd + 1:
-                    visit[(num, nx, ny, 2, nc)] = nd + 1
+                # if visit[(num, nx, ny, 2, nc)] == 0 or visit[(num, nx, ny, 2, nc)] > nd + 1:
+                #     visit[(num, nx, ny, 2, nc)] = nd + 1
+                if visit[(num, nx, ny, 2)] == 0 or visit[(num, nx, ny, 2)][0] > nd + 1 \
+                        or (visit[(num, nx, ny, 2)][0] == nd + 1 and visit[(num, nx, ny, 2)][1] > nc):
+                    visit[(num, nx, ny, 2)] = [nd + 1, nc]
                     if nd == dist:
                         temp1.append((num, nx, ny, 2, nd + 1, nc))
                     else:
@@ -89,8 +97,5 @@ while q:
     temp1 = temp2
     if len(res) != 0:
         break
-if len(res) == 0:
-    print(-1)
-else:
-    res.sort()
-    print(res[0][1], res[0][0])
+res.sort()
+print(res[0][1], res[0][0])
