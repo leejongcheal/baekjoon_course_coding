3267c0e94c83aff33b63de1032e090ec16fba61a
lee <aksndk123@naver.com>
2022-04-14 17:40:43 +0900

    ㅇ

---
+++ 문제/시뮬레이션과 구현 - 11/2064번 - IP 주소.py
@@ -0,0 +1,48 @@
+def find(idx):
+    bin_list = []
+    for add in addrs:
+        val = bin(add[idx])[2:]
+        val = "0"*(8-len(val)) + val
+        bin_list.append(val)
+    flag = 0
+    for i in range(8):
+        pre = bin_list[0][i]
+        for b in bin_list:
+            if pre != b[i]:
+                flag = 1
+                start = i
+                break
+        if flag == 1:
+            break
+    res = 2^(8-start) - 1
+    return 255 - res
+bin
+N = int(input())
+networt = []
+bitmask = []
+addrs = [list(map(int, input().split("."))) for _ in range(N)]
+print(addrs)
+for i in range(4):
+    new = 255
+    for add in addrs:
+        new &= add[i]
+    networt.append(new)
+
+flag = 0
+for i in range(4):
+    if flag == 1:
+        bitmask.append(0)
+        continue
+    for add in addrs:
+        if add[i] != networt[i]:
+            flag = 1
+            val = find(i)
+            bitmask.append(val)
+            break
+    if flag:
+        continue
+    bitmask.append(255)
+bitmask = ".".join(map(str, bitmask))
+networt = ".".join(map(str, networt))
+print(networt)
+print(bitmask)
