80ea0cf1cdacf79e96f25a99925e84c89a3407c5
lee <aksndk123@naver.com>
2022-04-18 16:35:57 +0900

    * 중국인의 나머지 정리 공부 쫌함

--- 기초/브루트포스/브루트포스/1476 날짜 계산.py
+++ 기초/브루트포스/브루트포스/1476 날짜 계산.py
@@ -1,12 +1,23 @@
-e,s,m = 0,0,0
-re, rs, rm = map(int, input().split())
-re -= 1
-rs -= 1
-rm -= 1
-i = 1
-while not (e == re and s == rs and m == rm):
-    i += 1
-    e = (e+1) % 15
-    s = (s+1) % 28
-    m = (m+1) % 19
-print(i)
+e, s, m = map(int, input().split())
+e -= 1
+s -= 1
+m -= 1
+# 15 / 28/ 19
+p1, p2, p3 = 1,1,1
+flag = 1
+while flag:
+    flag = 0
+    val1 = 28*19*p1
+    val2 = 15*19*p2
+    val3 = 15*28*p3
+    if val1 % 15 != 1:
+        p1 += 1
+        flag = 1
+    if val2 % 28 != 1:
+        p2 += 1
+        flag = 1
+    if val3 % 19 != 1:
+        p3 += 1
+        flag = 1
+res = (val1*e + val2*s + val3*m) % (15*28*19)
+print(res + 1)
