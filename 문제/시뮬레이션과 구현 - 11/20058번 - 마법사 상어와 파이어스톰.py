38899a44b2c2d9bc705ef8ae0b6772923d9c9b95
lee <aksndk123@naver.com>
2022-04-18 12:05:01 +0900

    삼성기출 40분컷
    아 회전에서 실수해서 쫌걸린거랑 i,j로 비교해야하는 x, y로 비교해서 디버깅 잠시찾은거 빼고는 쉬웠음

---
+++ 문제/시뮬레이션과 구현 - 11/20058번 - 마법사 상어와 파이어스톰.py
@@ -0,0 +1,59 @@
+from copy import deepcopy
+from collections import deque
+steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
+def rotate(x, y, L):
+    for i in range(2**L):
+        for j in range(2**L):
+            temp[x+j][y-i+2**L-1] = Map[x+i][y+j]
+N, M = map(int, input().split())
+N = 2 ** N
+Map = [list(map(int, input().split())) for _ in range(N)]
+L_list = list(map(int, input().split()))
+for L in L_list:
+    temp = deepcopy(Map)
+    x, y = 0, 0
+    rotate(x, y, L)
+    while x < N:
+        y += 2**L
+        if y < N:
+            rotate(x, y, L)
+        else:
+            x += 2**L
+            y = 0
+            if x < N:
+                rotate(x, y, L)
+    # 확인 temp에 대해서
+    Map = deepcopy(temp)
+    for i in range(N):
+        for j in range(N):
+            if Map[i][j] == 0:
+                continue
+            cnt = 0
+            for dx, dy in steps:
+                nx, ny = i + dx, j + dy
+                if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] != 0:
+                    cnt += 1
+            if cnt < 3:
+                temp[i][j] -= 1
+    Map = temp
+total = sum([sum(x) for x in Map])
+max_chunk = 0
+visit = [[0]*N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if Map[i][j] != 0 and visit[i][j] == 0:
+            visit[i][j] = 1
+            now = 1
+            q = deque()
+            q.append((i, j))
+            while q:
+                x, y = q.popleft()
+                for dx, dy in steps:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] != 0 and visit[nx][ny] == 0:
+                        visit[nx][ny] = 1
+                        now += 1
+                        q.append((nx, ny))
+            max_chunk = max(max_chunk, now)
+print(total)
+print(max_chunk)
