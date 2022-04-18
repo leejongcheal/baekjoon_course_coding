ec3ee03fc2dc05946545d1d8a96979a9ed241611
lee <aksndk123@naver.com>
2022-04-18 10:01:43 +0900

    *** 발상의 문제
    높이를 구해서 저장해놓는다는 발상을 못함.
    다른 이상한식으로 풀려했는데 솔직히 말이 안되는 발상에 구현자체도 힘든 문제

--- 문제/시뮬레이션과 구현 - 11/2571번 - 색종이 - 3.py
+++ 문제/시뮬레이션과 구현 - 11/2571번 - 색종이 - 3.py
@@ -1,9 +1,16 @@
-from collections import defaultdict, deque
+def calculate(x, y):
+    height = []
+    now_res = 0
+    for width in range(y, N):
+        if Map[x][width] != 0:
+            height.append(Map[x][width])
+            now_res = max(now_res, (width - y + 1)*min(height))
+        else:
+            return now_res
+    return now_res
 
 
-
-
-N = 30
+N = 100
 Map = [[0]*N for _ in range(N)]
 cnt = int(input())
 color_paper = [list(map(int, input().split())) for _ in range(cnt)]
@@ -13,9 +20,13 @@ for y, x in color_paper:
     for i in range(x, ex):
         for j in range(y, ey):
             Map[i][j] = 1
+for i in range(1, N):
+    for j in range(N):
+        if Map[i][j] != 0 and Map[i-1][j] != 0:
+            Map[i][j] = Map[i - 1][j] + 1
 res = 100
 for i in range(N):
     for j in range(N):
-        if Map[i][j] == 1:
-            calculate(i, j)
+        if Map[i][j] != 0:
+            res = max(res, calculate(i, j))
 print(res)
