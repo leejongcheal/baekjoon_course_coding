706f308c13688c485128191379d2dd0ed2aa9b1d
lee <aksndk123@naver.com>
2022-04-19 16:05:44 +0900

    ** DP문제인데 BFS도 녹아들어간 문제.
    [x*2, x-1,x+1] 사용
    그리고 DP를 굳이 visit대신으로 쓸려면 처음값을 1로 두고 나중에 결과에서 -1뺴는 테크닉을 사용하자
    왜냐면, 처음값을 0으로 두면 또 나중에 0이 아닌 더큰값이 들어가는 경우가 생김, 또 그걸 이용해서 문제를 풀게되는 경우가 생기니 주의하자.
    -> 사실 이건 이문제가 수준 떨어지는걸 보여준다고 생각함 갠적으로...
    
    DP문제가 맞긴한데 DP로 풀려하면 조금더 고급스럽게 풀어야함
    DP는 시험에 잘안나온다를 생각하고 BFS발상할수 있냐? 의 문제
    
    DP확인만하고 값을 안넣어줘서 q가 너무 커져서 메모리초과로 틀림

--- 기초/그래프와 DFS/BFS/1697번 - 숨바꼭질.py
+++ 기초/그래프와 DFS/BFS/1697번 - 숨바꼭질.py
@@ -1,23 +1,15 @@
 from collections import deque, defaultdict
 DP = defaultdict(int)
 x, goal = map(int,input().split())
-if x > goal :
-    print(x - goal)
-else:
-    q = deque()
-    DP[x] = 0
-    q.append(x)
-    while q:
-        x = q.popleft()
-        if x == goal:
-            print(DP[x])
-            break
-        if x - 1 >= 0 and DP[x-1] == 0:
-            DP[x - 1] = DP[x] + 1
-            q.append(x - 1)
-        if x + 1 <= 100000 and DP[x + 1] == 0:
-            DP[x + 1] = DP[x] + 1
-            q.append(x + 1)
-        if x*2 <= 100000 and DP[x*2] == 0:
-            DP[x * 2] = DP[x] + 1
-            q.append(x*2)
+q = deque()
+DP[x] = 1
+q.append(x)
+while q:
+    x = q.popleft()
+    if x == goal:
+        print(DP[goal] - 1)
+        break
+    for next in [x*2, x + 1, x - 1]:
+        if 0 <= next <= 100000 and DP[next] == 0:
+            DP[next] = DP[x] + 1
+            q.append(next)
