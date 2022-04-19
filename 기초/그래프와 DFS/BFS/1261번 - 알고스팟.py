3739bb58bfbad9907c4f96e470397f9102a4f632
lee <aksndk123@naver.com>
2022-04-19 14:49:22 +0900

    *** 0-1 너비문제 중요유형 + 큐 관련해서 풀이 2가지 상기
    중요한 포인트
    1. 거리가 아닌 벽을 몇개 부시냐가 중요한 문제
    2. 위에 따른 visit 효율적인 응용필요함
    3. 큐 관련해서 들어가는 값이 전의값 or 전의값 + 1 이라 BFS를 유지하기위해 두값에 대해서 차이를 둬야함
     3-1 전의값인 경우 appendleft , +1인경우는 append를 해줘서 유지
     3-2 전의값과 +1한값에대해서 따로 큐 를 사용해서 푸는 방법
    
    잘 풀었는데 메모리초과뜸 이유가 뭐지 ㅋㅋ
    -> 마지막 조건문 실수함
    
    문제의 핵심 ->기준은 벽을 부신 갯수
    
    벽의 갯수, 현재 이동한 갯수 ,x, y로 해서 풀려고 하니깐 시간초과
    visit 처리더 깔끔하게 할수있음
    
    -> 문제이해중 가장 중요한건 벽을 몇개 부시고 가냐지, 몇번만에 가냐가 아님..

--- 기초/그래프와 DFS/BFS/1261번 - 알고스팟.py
+++ 기초/그래프와 DFS/BFS/1261번 - 알고스팟.py
@@ -1,26 +1,36 @@
 from collections import deque
-INF = int(1e10)
-steps = [(1,0),(0,1),(-1,0),(0,-1)]
+steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
 M, N = map(int, input().split())
-graph = [list(map(int,list(input()))) for _ in range(N)]
-visit = [[INF]*M for i in range(N)]
-# 00 -> N-1 M - 1로 벽최소
-q = deque()
-# q.append((0,0,0)) # 벽갯수, x , y
-q.append((0,0)) # x , y
+Map = [list(input()) for _ in range(N)]
+INF = int(1e10)
+visit = [[INF]*M for _ in range(N)]
 visit[0][0] = 0
+q = deque()
+q.append((0, 0))
+next = []
+flag = 0
 while q:
-    x, y = q.popleft()
-    if x == N - 1 and y == M - 1:
-        print(visit[x][y])
+    same = []
+    while q:
+        x, y = q.pop()
+        if x == N - 1 and y == M - 1:
+            flag = 1
+            break
+        dist = visit[x][y]
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M:
+                if Map[nx][ny] == '0' and visit[nx][ny] > dist:
+                    same.append((nx, ny))
+                    visit[nx][ny] = dist
+                elif Map[nx][ny] == '1' and visit[nx][ny] > dist + 1:
+                    next.append((nx, ny))
+                    visit[nx][ny] = dist + 1
+    if flag:
         break
-    for dx, dy in steps:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < M:
-            if graph[nx][ny] == 0 and visit[nx][ny] > visit[x][y]:
-                visit[nx][ny] = visit[x][y]
-                q.appendleft((nx, ny))
-            # 아 무슨 wall_cnt + 1 이 아닌 wall_cnt 넣었다고 메모리 초과뜨는 문제
-            elif graph[nx][ny] == 1 and visit[nx][ny] > visit[x][y] + 1:
-                visit[nx][ny] = visit[x][y] + 1
-                q.append((nx, ny))
+    elif same:
+        q = same
+    else:
+        q = next
+        next = []
+print(visit[N-1][M-1])
