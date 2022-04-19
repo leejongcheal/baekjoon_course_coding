536bf824d03ed2ea79f39070300c38fd7116128a
lee <aksndk123@naver.com>
2022-04-19 17:32:58 +0900

    * 복잡한 조건문 설정문제 + 코딩력 + 수식잘짜는 연습+ 디버깅연습 까지 훌륭함
    두번째는 조사할 행열을 리스트로 만들어서 검사하는식으로 코드를 줄임
    다시 푸니 쉽긴하네 확실히. 설계부분에서 시간이 덜하고 확실하니깐..
    처음보는 문제푸는 연습이 필요함

--- 기초/시뮬레이션과 구현/14890번 - 경사로.py
+++ 기초/시뮬레이션과 구현/14890번 - 경사로.py
@@ -1,93 +1,62 @@
+def check(l):
+    visit = [0]*N
+    pre = l[0]
+    for i in range(N):
+        if pre == l[i]:
+            continue
+        elif abs(l[i] - pre) == 1:
+            if pre < l[i]:
+                cnt = 0
+                now = i - 1
+                flag = 0
+                while cnt < L:
+                    if 0 <= now < N and l[now] == pre and visit[now] == 0:
+                        visit[now] = 1
+                        cnt += 1
+                        now -= 1
+                    else:
+                        break
+                if cnt != L:
+                    return 0
+                pre = l[i]
+            elif pre > l[i]:
+                cnt = 0
+                now = i
+                flag = 0
+                while cnt < L:
+                    if 0 <= now < N and l[now] == l[i] and visit[now] == 0:
+                        visit[now] = 1
+                        cnt += 1
+                        now += 1
+                    else:
+                        break
+                if cnt != L:
+                    return 0
+                pre = l[i]
+        else:
+            return 0
+    return 1
+
+
 N, L = map(int, input().split())
-Map = [list(map(int,input().split())) for _ in range(N)]
-# 행 검사
+Map = [list(map(int, input().split())) for _ in range(N)]
 res = 0
-for i in range(N):
-    check = set()
-    flag = 1
-    for j in range(N - 1):
-        if flag == 0:
-            break
-        diff = abs(Map[i][j] - Map[i][j+1])
-        # 크기가 2차이인경우
-        if diff > 1:
-            flag = 0
-            break
-        if diff == 1:
-            if Map[i][j] > Map[i][j+1]:
-                cnt = 0
-                for next in range(j+1, N):
-                    if next not in check and Map[i][next] == Map[i][j+1]:
-                        check.add(next)
-                        cnt += 1
-                        if cnt == L:
-                            break
-                    else:
-                        flag = 0
-                        break
-                if cnt < L:
-                    flag = 0
-                    break
-            elif Map[i][j] < Map[i][j+1]:
-                cnt = 0
-                for prev in range(j, -1, -1):
-                    if prev not in check and Map[i][prev] == Map[i][j]:
-                        check.add(prev)
-                        cnt += 1
-                        if cnt == L:
-                            break
-                    else:
-                        flag = 0
-                        break
-                if cnt < L:
-                    flag = 0
-                    break
-    if flag:
-        # print(i)
+for m in Map:
+    if check(m):
         res += 1
-# 열검사
-# print("행")
 for j in range(N):
-    check = set()
-    flag = 1
-    for i in range(N - 1):
-        if flag == 0:
-            break
-        diff = abs(Map[i][j] - Map[i + 1][j])
-        # 크기가 2차이인경우
-        if diff > 1:
-            flag = 0
-            break
-        if diff == 1:
-            if Map[i][j] > Map[i + 1][j]:
-                cnt = 0
-                for next in range(i + 1, N):
-                    if next not in check and Map[next][j] == Map[i + 1][j]:
-                        check.add(next)
-                        cnt += 1
-                        if cnt == L:
-                            break
-                    else:
-                        flag = 0
-                        break
-                if cnt < L:
-                    flag = 0
-                    break
-            elif Map[i][j] < Map[i + 1][j]:
-                cnt = 0
-                for prev in range(i, -1, -1):
-                    if prev not in check and Map[prev][j] == Map[i][j]:
-                        check.add(prev)
-                        cnt += 1
-                        if cnt == L:
-                            break
-                    else:
-                        flag = 0
-                        break
-                if cnt < L:
-                    flag = 0
-                    break
-    if flag:
-        # print(j)
+    temp = []
+    for i in range(N):
+        temp.append(Map[i][j])
+    if check(temp):
         res += 1
 print(res)
+
+
+
+
+
+
+
+
+
