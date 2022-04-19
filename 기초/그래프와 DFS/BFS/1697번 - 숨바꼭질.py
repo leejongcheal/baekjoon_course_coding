0d549e362b0eab8d9c1217fce5a6d921f27198ab
lee <aksndk123@naver.com>
2022-04-19 16:17:01 +0900

    *** 오류난 부분만 제대로 확인 DP문제인데 BFS도 녹아들어간 문제.
    [x*2, x-1,x+1] 사용
    그리고 DP를 굳이 visit대신으로 쓸려면 처음값을 1로 두고 나중에 결과에서 -1뺴는 테크닉을 사용하자
    왜냐면, 처음값을 0으로 두면 또 나중에 0이 아닌 더큰값이 들어가는 경우가 생김, 또 그걸 이용해서 문제를 풀게되는 경우가 생기니 주의하자.
    -> 사실 이건 이문제가 수준 떨어지는걸 보여준다고 생각함 갠적으로...
    *** 아 왜 오류냐면 next순서가 [x*2, x-1,x+1] 임
    따라서 0시작일때, DP[0*2] = DP[0] = 1
    그후에 DP[1] = 2 가 되버려서 문제가 되는거임.
    따라서 지금처럼 바뀐 코드는 ac를 받게됨
    
    DP문제가 맞긴한데 DP로 풀려하면 조금더 고급스럽게 풀어야함
    DP는 시험에 잘안나온다를 생각하고 BFS발상할수 있냐? 의 문제
    
    DP확인만하고 값을 안넣어줘서 q가 너무 커져서 메모리초과로 틀림

--- 기초/그래프와 DFS/BFS/1697번 - 숨바꼭질.py
+++ 기초/그래프와 DFS/BFS/1697번 - 숨바꼭질.py
@@ -2,14 +2,14 @@ from collections import deque, defaultdict
 DP = defaultdict(int)
 x, goal = map(int,input().split())
 q = deque()
-DP[x] = 1
+DP[x] = 0
 q.append(x)
 while q:
     x = q.popleft()
     if x == goal:
-        print(DP[goal] - 1)
+        print(DP[goal])
         break
-    for next in [x*2, x + 1, x - 1]:
+    for next in [x + 1, x - 1, x*2]:
         if 0 <= next <= 100000 and DP[next] == 0:
             DP[next] = DP[x] + 1
             q.append(next)
