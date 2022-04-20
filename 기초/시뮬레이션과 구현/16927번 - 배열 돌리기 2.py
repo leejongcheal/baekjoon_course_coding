d30a7a6f1258054560bf689025f58017feda4e4e
lee <aksndk123@naver.com>
2022-04-20 12:17:13 +0900

    * 시간 효율을 위해서 한번만 회전하는 방법을 생각
    핵심
    1. 회전은 몇번하면 자기자신으로 돌아롬 -> %연산
    2. ** 회전을 할때, 회전하는것들만 리스트로 따로구하는 방법
    
    처음엔 R번에 대해서 하나하나 회전한다음에 출력을함
    -> 당연히 오래걸림
    근데 회전을 몇번해야 돌아오는지 계산하기가 싫었는데,,,
    생각을 해보니 회전만 돌아가는 리스트를 따로 구하고 그 리스트별로 %len(line) 으로 몇번도는지 구하고 그 리스트에대해서 그만큼 뒤로 보낸다음에 채워주면 되는 문제.

--- 기초/시뮬레이션과 구현/16927번 - 배열 돌리기 2.py
+++ 기초/시뮬레이션과 구현/16927번 - 배열 돌리기 2.py
@@ -1,78 +1,71 @@
-def fill(Map, temp, N, M, x, y):
-    level = x
-    #밑으로
-    i, j = level, level
-    for i in range(level, N-1-level):
-        temp[i+1][j] = Map[i][j]
-    #오른쪽으로
-    i,j = N-1-level, level
-    for j in range(level, M-1-level):
-        temp[i][j+1] = Map[i][j]
-    #위로
-    i, j = N-1-level, M-1-level
-    for i in range(N-1-level,level, -1):
-        temp[i-1][j] = Map[i][j]
-    #왼쪽으로
-    i, j = level, M - 1 - level
-    for j in range(M-1-level,level, -1):
-        temp[i][j-1] = Map[i][j]
-    # print(str(i)+"시작한 결과")
-    # for t in temp:
-    #     print(t)
-def split(Map, level, n, m):
-    line = []
-    i,j = level, level
-    for j in range(level, level + m -1):
-        line.append(Map[i][j])
-    i,j=level,level+m-1
-    for i in range(level,level + n - 1):
-        line.append(Map[i][j])
-    i,j=level + n -1, level+m-1
-    for j in range(level+m-1,level,-1):
-        line.append(Map[i][j])
-    i,j = level+n-1, level
-    for i in range(level+n-1,level,-1):
-        line.append(Map[i][j])
-    return line
-
-
-def fill(res,temp, level, n, m):
-    i, j = level, level
-    index = 0
-    for j in range(level, level + m - 1):
-        res[i][j] = temp[index]
-        index += 1
-    i, j = level, level + m - 1
-    for i in range(level, level + n - 1):
-        res[i][j] = temp[index]
-        index += 1
-    i, j = level + n - 1, level + m - 1
-    for j in range(level + m - 1, level, -1):
-        res[i][j] = temp[index]
-        index += 1
-    i, j = level + n - 1, level
-    for i in range(level + n - 1, level, -1):
-        res[i][j] = temp[index]
-        index += 1
-
-
-def rotate(Map, N, M,R):
-    res = [[0]*M for _ in range(N)]
+def split():
     lines = []
-    for i in range(min(N, M) // 2):
-        lines.append(split(Map, i, N - 2*i, M - 2*i))
-    temps = []
+    cnt = min(N, M) // 2
+    for level in range(cnt):
+        line = []
+        x, y = level, level
+        line.append(Map[x][y])
+        # 밑으로
+        while x + 1 < N - level:
+            x += 1
+            line.append(Map[x][y])
+        # 오른쪽으로
+        while y + 1 < M - level:
+            y += 1
+            line.append(Map[x][y])
+        # 위로
+        while x - 1 >= level:
+            x -= 1
+            line.append(Map[x][y])
+        # 오른쪽으로
+        while y - 1 > level:
+            y -= 1
+            line.append(Map[x][y])
+        lines.append(line)
+    return lines
+
+
+def fill(lines):
+    cnt = min(N, M) // 2
+    for level in range(cnt):
+        line = lines[level]
+        index = 0
+        x, y = level, level
+        Map[x][y] = line[index]
+        index += 1
+        # 밑으로
+        while x + 1 < N - level:
+            x += 1
+            Map[x][y] = line[index]
+            index += 1
+        # 오른쪽으로
+        while y + 1 < M - level:
+            y += 1
+            Map[x][y] = line[index]
+            index += 1
+        # 위로
+        while x - 1 >= level:
+            x -= 1
+            Map[x][y] = line[index]
+            index += 1
+        # 오른쪽으로
+        while y - 1 > level:
+            y -= 1
+            Map[x][y] = line[index]
+            index += 1
+
+
+def rotate():
+    lines = split()
+    after_lines = []
     for line in lines:
         move_index = R % len(line)
-        temp = line[move_index:] + line[:move_index]
-        temps.append(temp)
-    for i in range(min(N, M) // 2):
-        fill(res,temps[i], i, N - 2 * i, M - 2 * i)
-    return res
+        after_lines.append(line[-move_index:] + line[:-move_index])
+    fill(after_lines)
 
 
-N, M, R = map(int,input().split())
-Map = [list(input().split()) for _ in range(N)]
-Map = rotate(Map, N, M, R)
+N, M, R = map(int, input().split())
+Map = [list(map(int, input().split())) for _ in range(N)]
+rotate()
 for m in Map:
-    print(" ".join(m))
+    print(*m)
