be44d3c3513f82d74c28e71b18b0446900d81980
lee <aksndk123@naver.com>
2022-04-20 13:28:07 +0900

    전에 어떻게 푼ㄴ거야..
    왜 그런식으로 풀었는지도 모르겠고, 답나오는 과정도 이상함
    그냥 한번에 정면뒤 구하는식으로 바꾸어서 품

--- 기초/시뮬레이션과 구현/16931번 - 겉넓이 구하기.py
+++ 기초/시뮬레이션과 구현/16931번 - 겉넓이 구하기.py
@@ -1,30 +1,20 @@
-N, M = map(int,input().split())
-Map = [list(map(int, input().split())) for _ in range(N)]
-res = 0
-front = 0
-rear = 0
-right = 0
-left = 0
-bottom = N*M
-for i in range(N):
-    front += Map[i][0]
-    for j in range(1, M):
-        if Map[i][j] > Map[i][j-1]:
-            front += Map[i][j] - Map[i][j-1]
-    # rear += Map[i][M-1]
-    # for j in range(M-2, -1, -1):
-    #     if Map[i][j] > Map[i][j + 1]:
-    #         rear = Map[i][j] - Map[i][j+1]
+def check(L):
+    length = len(L)
+    cnt = L[0]
+    for i in range(length - 1):
+        cnt += abs(L[i] - L[i+1])
+    cnt += L[-1]
+    return cnt
 
+
+N, M = map(int, input().split())
+res = 2*N*M
+Map = [list(map(int, input().split())) for _ in range(N)]
+for m in Map:
+    res += check(m)
 for j in range(M):
-    left += Map[0][j]
-    for i in range(1, N):
-        if Map[i][j] > Map[i-1][j]:
-            left += Map[i][j] - Map[i-1][j]
-    # right += Map[N-1][j]
-    # for i in range(N-2, -1,-1):
-    #     if Map[i][j] > Map[i+1][j]:
-    #         right += Map[i][j] - Map[i+1][j]
-# res = front + rear + left + right + bottom*2
-res = (front + left + bottom)*2
+    temp =[]
+    for i in range(N):
+        temp.append(Map[i][j])
+    res += check(temp)
 print(res)
