9c0236bc9741e802b0e75e00b27b0a1722e6ec6e
lee <aksndk123@naver.com>
2022-04-22 16:09:46 +0900

    * 최대색이 3개인걸 알수 있으면 풀고 아니면 못푸는 문제
    일단 전에는 dfs로 풀었는데 굳이 왜그랬지 bfs풀이가 맞는거 같은데..
    
    우린 색이 최대 3개인걸알고 있음
    X가 없으면 0개
    X가 있으면 1~3개
    X는 있는데 이어지는 X가 없으면 1개
    X는 있는데 이어지는 X가 있으면 2~3개
    0,1만 넣어주면서 진행할때, 주변을 검사했을때, 같은 색이 나온다면 색을 3개

--- 연습/그래프와 BFS/그래프 알고리즘/12946번 - 육각 보드.py
+++ 연습/그래프와 BFS/그래프 알고리즘/12946번 - 육각 보드.py
@@ -1,35 +1,31 @@
-def dfs(x, y, color=0):
-    global ans
-    if ans == 3:
-        return
-    ans = max(ans, 1)
-    Map[x][y] = color
-    for dx, dy in steps:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == "X":
-            ans = 2
-            dfs(nx, ny, 1 - color)
-        elif 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == color:
-            ans = 3
-            return
+from collections import deque
+def solve():
+    res = 1
+    for i in range(N):
+        for j in range(N):
+            if Map[i][j] == "X":
+                Map[i][j] = 0
+                q = deque()
+                q.append((i, j))
+                while q:
+                    x, y = q.popleft()
+                    for dx, dy in steps:
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == "X":
+                            if res == 1:
+                                res = 2
+                            Map[nx][ny] = Map[x][y] ^ 1
+                            q.append((nx, ny))
+                        elif 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == Map[x][y]:
+                            return 3
+    return res
 
-steps = [(-1, 0),(-1, 1),(0, 1),(1, 0),(1, -1),(0, -1)]
 N = int(input())
-location = []
-Map = [list(input()) for _ in range(N)]
-for i in range(N):
-    for j in range(N):
-        if Map[i][j] == "X":
-            location.append((i, j))
+steps = [(-1, 0),(-1, 1),(0,-1),(0,1),(1, -1),(1, 0)]
 res = 0
-for x, y in location:
-    if Map[x][y] == "X":
-        ans = 1
-        dfs(x, y, 0)
-        res = max(res, ans)
-        if res == 3:
-            break
-if location:
-    print(res)
+Map = [list(input()) for _ in range(N)]
+if sum([x.count("X") for x in Map]) == 0:
+    res = 0
 else:
-    print(0)
+    res = solve()
+print(res)
