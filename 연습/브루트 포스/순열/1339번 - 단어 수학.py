88952e5c58f367e9d452053ced4e9a2f29fe588d
lee <aksndk123@naver.com>
2022-04-22 13:13:03 +0900

    * 발상만 확인

--- 연습/브루트 포스/순열/1339번 - 단어 수학.py
+++ 연습/브루트 포스/순열/1339번 - 단어 수학.py
@@ -1,27 +1,17 @@
-def cal_dic_value():
-    global dic_value, L
-    for l in L:
-        length = len(l) - 1
-        for e in l:
-            dic_value[e] += 10 ** length
-            length -= 1
-    return list(dic_value.values())
-
-
+from collections import defaultdict
+num = defaultdict(int)
 N = int(input())
 L = list(list(input()) for _ in range(N))
-keys = []
 for l in L:
-    keys += l
-keys = set(keys)
-dic_value = dict()# 자리수를 계산한값
-for key in keys:
-    dic_value[key] = 0
-value_list = cal_dic_value()
-value_list.sort(reverse=True)
+    for idx, s in enumerate(l, 1):
+        num[s] += 10**(len(l) - idx)
+A = []
+for s in num.keys():
+    A.append(num[s])
+A.sort(reverse=True)
 res = 0
-number = 9
-for v in value_list:
-    res += number * v
-    number -= 1
+now = 9
+for val in A:
+    res += val*now
+    now -= 1
 print(res)
