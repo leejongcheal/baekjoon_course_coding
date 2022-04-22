e08e55e67af09b17a46e6c0a1ce05d0bdb1d8c9c
lee <aksndk123@naver.com>
2022-04-22 10:43:30 +0900

    ***** 배열이아닌 x,y좌표 사용하는 방법
    1. x,y좌표도 내맘대로 하는게 아닌 주어진대로 맞춰풀어야하는데 y축을거꾸로 해서 자꾸 틀림
    
    2. res을 셀때 방법도 잘못생각함
    
    3.애초에 발상도 못한 문제
    
    쉬운데 왜 오래걸리고 못풀었을까... 아오 전형적인 주어진대로 구현하는 문제

--- 기초/시뮬레이션과 구현/15685번 - 드래곤 커브.py
+++ 기초/시뮬레이션과 구현/15685번 - 드래곤 커브.py
@@ -1,30 +1,28 @@
-from collections import defaultdict
-steps = [(0,1),(-1,0),(0,-1),(1,0)]
+steps = [(1,0),(0,-1),(-1,0),(0,1)]
 N = int(input())
 curve_info = [list(map(int, input().split())) for _ in range(N)]
-total_sum = sum([i for i in range(1, N+1)])
-total_curve = defaultdict(set) # 해당좌표에 드래곤 커브 idx 넣어주기
-idx = 1
-for sy, sx, d, g in curve_info:
-    curve = defaultdict(int)
-    curve[(sx, sy)] = 1
+total_curve = set()
+for sx, sy, d, g in curve_info:
+    curve = []
+    curve.append((sx, sy))
     dx, dy = steps[d]
     nx, ny = sx + dx, sy + dy
-    curve[(nx, ny)] = 1
+    curve.append((nx, ny))
     ex, ey = nx, ny
     for i in range(g):
         L = []
-        for x, y in curve.keys():
-            nx, ny = y - ey + ex, -x + ex + ey
+        for x, y in curve[-2::-1]:
+            X, Y = x - ex, y - ey
+            nx, ny = -Y + ex, X + ey
             L.append((nx, ny))
-        for x, y in L:
-            curve[(x, y)] = 1
-        ex, ey = sy - ey + ex, -sx + ex + ey
-    for x, y in curve.keys():
-        total_curve[(x, y)].add(idx)
-    idx += 1
+        X, Y = sx - ex, sy - ey
+        ex, ey = -Y + ex, X + ey
+        curve += L
+    for now in range(len(curve)):
+        x, y = curve[now]
+        total_curve.add((x, y))
 res = 0
-for val in total_curve:
-    if sum(val) == total_sum:
+for x, y in total_curve:
+    if (x+1, y) in total_curve and (x, y+ 1) in total_curve and (x+1, y+1) in total_curve:
         res += 1
 print(res)
