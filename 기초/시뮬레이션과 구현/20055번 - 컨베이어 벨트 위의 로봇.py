af8caff536f16f4ac93b28e212fa4c6a48ea7fc1
lee <aksndk123@naver.com>
2022-04-22 10:53:33 +0900

    ** 문제 이해가 중요한 문제
    문제이해도 제대로 못했고, 로봇이동부분 이해를 제대로 못함
    
    문제이해와 설계만 했다면 구현은 쉬운 문제

--- 기초/시뮬레이션과 구현/20055번 - 컨베이어 벨트 위의 로봇.py
+++ 기초/시뮬레이션과 구현/20055번 - 컨베이어 벨트 위의 로봇.py
@@ -1,30 +1,21 @@
 N, K = map(int, input().split())
-robot = [0]*2*N
-durability = list(map(int, input().split()))
-k = durability.count(0)
-res = 0
-while k < K:
-    res += 1
-    # 벨트 회전
-    temp_robot = [0]*(2*N)
-    temp_dur = [0]*(2*N)
-    for i in range(2*N):
-        temp_robot[(i+1)%(2*N)] = robot[i]
-        temp_dur[(i+1)%(2*N)] = durability[i]
-    robot = temp_robot
-    durability = temp_dur
-    robot[N-1] = 0
-    # 로봇이동검사
-    i = N - 2
-    while 0 <= i:
-        if durability[i+1] > 0 and robot[i] == 1 and robot[i+1] == 0:
+inner = list(map(int, input().split()))
+robot = [0]*N
+time = 0
+while inner.count(0) < K:
+    time += 1
+    # 회전
+    inner = [inner[-1]] + inner[:-1]
+    robot = [0] + robot[:-2] + [0]
+    # 로봇이동
+    for i in range(N-2,-1,-1):
+        if robot[i] == 1 and robot[i+1] == 0 and inner[i+1] > 0:
             robot[i] = 0
             robot[i+1] = 1
-            durability[i+1] -= 1
-        i -= 1
-    robot[N - 1] = 0
-    if robot[0] == 0 and durability[0] > 0:
+            inner[i+1] -= 1
+    robot[-1] = 0
+    # 0위치에 로봇올리기
+    if inner[0] > 0:
+        inner[0] -= 1
         robot[0] = 1
-        durability[0] -= 1
-    k = durability.count(0)
-print(res)
+print(time)
