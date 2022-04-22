1d82af866c2f53d7be109c83357602a2d1ea6fe6
lee <aksndk123@naver.com>
2022-04-22 14:36:53 +0900

    *** 사이클 문제 4번째 풀이 가자
    아 정확히 설계가 잘안되고 잘못품
    사이클의 개념이 잘 안잡혀있음 연습필요함
    일단 갠히 cycle이 아니면 그걸 저장해서 풀려고 하는데 이게 문제임... 특히 이문제는 4개이상의 싸이클을 구성하는지 묻는 문제라 그걸 적용하기에 문제가 있음
    
    ** 그리고 dfs 자체도 잘 확인해보면 x,y에 대해서 탐색을 다하고 나서 방문체크를 해주는데 이런식의 먼저하고 방문체크하는 형식의 dfs써본적이 없어서 잘못품

--- 연습/그래프와 BFS/그래프 알고리즘/16929번 - Two Dots.py
+++ 연습/그래프와 BFS/그래프 알고리즘/16929번 - Two Dots.py
@@ -1,42 +1,38 @@
-def dfs(x, y, val, traced):
-    global visited, Map, steps, N, M
-    if visited[x][y] == 1:
+steps = [(-1 ,0),(1, 0),(0, -1),(0, 1)]
+def dfs(x, y,tracked):
+    if visit[x][y] == 1:
         return 1
-    if (x, y) in traced:
-        index = traced.index((x, y))
-        if len(traced[index:]) >= 4:
+    if (x, y) in tracked:
+        index = tracked.index((x, y))
+        if len(tracked[index:]) >= 4:
             return 0
         else:
             return 1
-    traced.append((x, y))
+    tracked.append((x, y))
     for dx, dy in steps:
         nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] == val:
-            if not dfs(nx, ny, val, traced):
-                return 0
-    traced.pop()
-    visited[x][y] = 1
+        if 0 <= nx < N and 0 <= ny < M:
+            if Map[nx][ny] == Map[x][y]:
+                if dfs(nx, ny, tracked) == 0:
+                    return 0
+    visit[x][y] = 1
+    tracked.pop()
     return 1
 
 
-steps = [(1,0),(0,1),(-1,0),(0,-1)]
 N, M = map(int, input().split())
 Map = [list(input()) for _ in range(N)]
-visited = [[0]*M for _ in range(N)]
+visit = [[0]*M for _ in range(N)]
 flag = 0
 for i in range(N):
     for j in range(M):
-        if visited[i][j] == 0:
-            if not dfs(i,j,Map[i][j],[]):
+        if visit[i][j] == 0:
+            if dfs(i, j,[]) == 0:
                 flag = 1
                 break
-            visited[i][j] = 1
     if flag:
         break
 if flag:
     print("Yes")
 else:
     print("No")
-
-
-
