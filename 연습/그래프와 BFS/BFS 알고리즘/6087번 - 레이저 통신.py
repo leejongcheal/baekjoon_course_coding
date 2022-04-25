9e02f0bdf4b282db32d171b9d91dcb333ce72aaf
lee <aksndk123@naver.com>
2022-04-25 16:03:03 +0900

    ** 조금더 정교한 풀이함 커밋만 확인
    1. 일단 방향은 내방향 + 90% 2개이니 이부분을 의식적으로 집어놈
    2. 같은 cnt라도 방향에 따른 방문을 체크하기위해서 작은경우와 같은경우를 따로 나눔 -> 여기선 시간손해긴한데 이게더 맞는거 같음
    3.이를위한 visit_d 변수를 따로 쓴거 굳이 visit에 다 집어놓을 필요없음
    
    질문검색해서 쉽게 풀었는데 그 반례없으면 솔직히 왜틀렸는지 알기 힘들었을꺼같음.
    cnt < visit[nx][ny] 풀이는 왠만하면 하지말고 cnt <= visit[nx][ny] 풀이를 하자
    
    - 이경우에는 같은 cnt에 다른 방향인 경우가 있기때문에 틀렸음 cnt뿐만 아니라 방향도 다음값에 영향을 주기 때문에

--- 연습/그래프와 BFS/BFS 알고리즘/6087번 - 레이저 통신.py
+++ 연습/그래프와 BFS/BFS 알고리즘/6087번 - 레이저 통신.py
@@ -1,41 +1,50 @@
 from collections import deque, defaultdict
+
+steps = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 INF = int(1e10)
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-# steps = [(0,1),(1,0),(0,-1),(-1,0)]
 M, N = map(int, input().split())
 Map = [list(input()) for _ in range(N)]
-visit = [[INF]*M for _ in range(N)]
-flag = 0
+visit = [[INF] * M for _ in range(N)]
+visit_d = defaultdict(list)
+start = end = -1
 for i in range(N):
     for j in range(M):
         if Map[i][j] == "C":
-            if flag == 0:
-                x, y = i, j
-                flag += 1
-            elif flag:
-                ex, ey = i, j
-visit[x][y] = 0
+            if start == -1:
+                start = (i, j)
+            else:
+                end = (i, j)
 q = deque()
+x, y = start
+visit[x][y] = 0
+visit_d[(x, y)] = [0, 1, 2, 3]
 for d in range(4):
     q.append((x, y, d, 0))
-# 거울갯수를 기준으로 오름차순으로 q 만들어야함
-res = -1
+
 while q:
     x, y, d, cnt = q.popleft()
-    if x == ex and y == ey:
+    if (x, y) == end:
         res = cnt
         break
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
+    for nd, (dx, dy) in enumerate(steps):
+        nx, ny = x + dx, y + dy
+        next_cnt = cnt + 1
+        if nd == d:
+            next_cnt = cnt
+        # 없어도 문제는 풀림 밑에서 조건이 걸리기 때문에
+        # -> 그래도 의도적으로 사용 + 시간조금이라도 빨라질테니
+        elif nd % 4 == (d + 2) % 4:
+            continue
         if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] != "*":
-            if i == d:
-                # cnt는 같은데 방향이 다른경우도 생각해줘야함
-                if cnt <= visit[nx][ny]:
-                    visit[nx][ny] = cnt
-                    q.appendleft((nx, ny, d, cnt))
+            if visit[nx][ny] > next_cnt:
+                visit[nx][ny] = next_cnt
+                visit_d[(nx, ny)] = [nd]
+            elif visit[nx][ny] == next_cnt and nd not in visit_d[(nx, ny)]:
+                visit_d[(nx, ny)].append(nd)
             else:
-                if cnt + 1 <= visit[nx][ny]:
-                    visit[nx][ny] = cnt + 1
-                    q.append((nx, ny, i, cnt + 1))
-print(res)
+                continue
+            if next_cnt == cnt:
+                q.appendleft((nx, ny, nd, cnt))
+            else:
+                q.append((nx, ny, nd, next_cnt))
+print(cnt)
