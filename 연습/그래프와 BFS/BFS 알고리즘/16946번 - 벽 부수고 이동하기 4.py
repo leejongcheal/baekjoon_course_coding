0fb2a7497dc4d5684672335626746d0fa42a89c9
lee <aksndk123@naver.com>
2022-04-25 12:40:37 +0900

    ** 시간초과 해결해야하는 문제 + 문제 잘읽기
    1. 최대 백만칸이되니 이걸 여러번 하면 무조건 시간초과
    이를 위해 0의 그룹들에 대해서 index, 갯수를 줌
    - 이때 visit그룹들의 번호를 주어 해당좌표의 번호와 갯수를 구하기 쉽게함
    
    2. 칸%10의 값을 넣어줘야함
    
    3. DFS풀이가 안되는 문제 -> 따라서 BFS로 풀었는데 그냥 언어차이

--- 연습/그래프와 BFS/BFS 알고리즘/16946번 - 벽 부수고 이동하기 4.py
+++ 연습/그래프와 BFS/BFS 알고리즘/16946번 - 벽 부수고 이동하기 4.py
@@ -1,29 +1,45 @@
-def dfs(x, y):
-    global cnt
-    for dx, dy in steps:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < M and visit[nx][ny] == 0 and Map[nx][ny] == "0":
-            visit[nx][ny] = 1
-            cnt += 1
-            dfs(nx, ny)
-
+from collections import defaultdict, deque
+def bfs(x, y):
+    global num
+    cnt = 1
+    visit[x][y] = num
+    q = deque()
+    q.append((x, y))
+    while q:
+        x, y = q.popleft()
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] == "0" and visit[nx][ny] == 0:
+                cnt += 1
+                visit[nx][ny] = num
+                q.append((nx, ny))
+    dist.append(cnt)
+    num += 1
 
 steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
 N, M = map(int, input().split())
 visit = [[0]*M for _ in range(N)]
 res = [["0"]*M for _ in range(N)]
 Map = [list(input()) for _ in range(N)]
+num = 1
+dist = [0]
+wall_list = []
 for i in range(N):
     for j in range(M):
-        if Map[i][j] == "1":
-            Map[i][j] = "0"
-            cnt = 1
-            visit = [[0]*M for _ in range(N)]
-            visit[i][j] = 1
-            dfs(i, j)
-            res[i][j] = str(cnt)
-            Map[i][j] = "1"
+        if Map[i][j] == "0" and visit[i][j] == 0:
+            bfs(i, j)
+        elif Map[i][j] == "1":
+            wall_list.append((i,j))
+for x, y in wall_list:
+    check = []
+    cnt = 1
+    for dx, dy in steps:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] == '0':
+            num = visit[nx][ny]
+            if num not in check:
+                check.append(num)
+                cnt += dist[num]
+    res[x][y] = str(cnt%10)
 for r in res:
     print("".join(r))
-
-
