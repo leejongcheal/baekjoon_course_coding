b486680fdc43e2bbcd70d1ceb45284e55dc5e6b5
lee <aksndk123@naver.com>
2022-04-25 13:27:11 +0900

    ** defaultdict는 메모리 초과, set은 시간이 3000ms 배열로 사용했어야하는 문제 ㅋㅋ
    visit을 3차원 배열로 풀었는데 2차원 배열도됨
    
    접근을 해당좌표에 같은거리로 벽을 안부시고, 부시고가 같은경우 안부신 경우에 대해서만 조사를 해도 안틀림

--- 연습/그래프와 BFS/BFS 알고리즘/2206번 - 벽 부수고 이동하기.py
+++ 연습/그래프와 BFS/BFS 알고리즘/2206번 - 벽 부수고 이동하기.py
@@ -1,31 +1,29 @@
 from collections import deque
-
+INF = int(1e10)
 steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 N, M = map(int, input().split())
-visit = [[[0] * M for __ in range(N)] for _ in range(2)]
+K = 1
+visit = [[INF] * M for _ in range(N)]
 graph = []
 for _ in range(N):
     graph.append(list(map(int, list(input()))))
 q = deque()
 # x y wall dis
 q.append((0, 0, 0, 1))
-visit[0][0][0] = 1
+visit[0][0] = 0
 res = -1
 while q:
     x, y, wall, dis = q.popleft()
     if x == N - 1 and y == M - 1:
         res = dis
         break
-
     for dx, dy in steps:
         nx, ny = x + dx, y + dy
         if 0 <= nx < N and 0 <= ny < M:
-            if graph[nx][ny] == 0 and visit[wall][nx][ny] == 0:
+            if graph[nx][ny] == 0 and visit[nx][ny] > wall:
                 q.append((nx, ny, wall, dis + 1))
-                visit[wall][nx][ny] = 1
-            elif graph[nx][ny] == 1 and wall == 0:
-                nnx, nny = nx + dx, ny + dy
-                if 0 <= nnx < N and 0 <= nny < M and visit[1][nnx][nny] == 0 and graph[nnx][nny] == 0:
-                    q.append((nnx, nny, wall + 1, dis + 2))
-                    visit[1][nnx][nny] = 1
+                visit[nx][ny] = wall
+            elif graph[nx][ny] == 1 and wall + 1 <= K and visit[nx][ny] > wall + 1:
+                q.append((nx, ny, wall + 1, dis + 1))
+                visit[nx][ny] = wall + 1
 print(res)
