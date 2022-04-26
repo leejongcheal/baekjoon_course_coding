78d147299f9d9f74904b039ebd3d7ebee65ba8e2
lee <aksndk123@naver.com>
2022-04-26 14:14:26 +0900

    풀다가 도저히 못풀겠음  돌다가 끝나는데 왜끝나는지 디버깅도 어렵고 모르겠음 답지보고 공부

--- 연습/브루트 포스/재귀/4574번 - 스도미노쿠.py
+++ 연습/브루트 포스/재귀/4574번 - 스도미노쿠.py
@@ -1,89 +1,82 @@
 from collections import defaultdict
-def find_possible_n(Map, x, y):
-    check = defaultdict(int)
+def next(x, y):
+    y += 1
+    if y == 9:
+        x += 1
+        y = 0
+    return x, y
+# 가능하면 1 아니면 0
+def check(x, y, val):
+    if val in Map[x]:
+        return 0
     for i in range(9):
-        check[Map[x][i]] += 1
-        check[Map[i][y]] += 1
-    si, sj = (x//3) * 3 ,(y // 3) * 3
-    for i in range(3):
-        for j in range(3):
-            check[Map[si+i][sj+j]] += 1
-    L = []
-    for i in range(1, 10):
-        if check[i] == 0:
-            L.append(i)
-    return L
-
-def dfs(Map, domino, N):
-    steps = [(0, 1), (1, 0)]
-    if N == 36:
-        return 1
-    for i in range(9):
-        for j in range(9):
-            if Map[i][j] == 0:
-                x, y = i, j
-                # 오른쪽, 아래로 가는거에서 안채워지는경우에 대한 처리가 안되어 있음
+        if (x, y) == (i, y):
+            continue
+        if Map[i][y] == val:
+            return 0
+    for i in range((x//3)*3, (x//3)*3+3):
+        for j in range((y//3)*3, (y//3)*3 + 3):
+            if (i,j) == (x, y):
+                continue
+            if Map[i][j] == val:
+                return 0
+    return 1
+def dfs(x, y): # 조사할 i, j가 주어짐
+    global res
+    print(x, y)
+    if res:
+        return
+    if x == 9:
+        res = Map
+        return
+    # 채워야 하는경우
+    xx, yy = next(x, y)
+    if Map[x][y] == 0:
+        for a, b in domino:
+            if visit[(a, b)] == 0:
+                visit[(a, b)] = 1
                 for dx, dy in steps:
                     nx, ny = x + dx, y + dy
                     if 0 <= nx < 9 and 0 <= ny < 9 and Map[nx][ny] == 0:
-                        for num1 in find_possible_n(Map, x, y):
-                            Map[x][y] = num1
-                            for num2 in find_possible_n(Map, nx, ny):
-                                Map[nx][ny] = num2
-                                a, b = num1, num2
-                                if a > b:
-                                    a, b = b, a
-                                if domino[a][b] == 1:
-                                    domino[a][b] = 0
-                                    print(N)
-                                    c = dfs(Map, domino, N+1)
-                                    print(1, N, c)
-                                    for m in Map:
-                                        print(m)
-                                    print("domini")
-                                    for d in domino:
-                                        print(d)
-                                    if c:
-                                        print(11, a)
-                                        return 1
-                                    domino[a][b] = 1
-                                Map[nx][ny] = 0
-                            Map[x][y] = 0
-    return 0
+                        if check(x,y,a) and check(nx, ny, b):
+                            Map[x][y], Map[nx][ny] = a, b
+                            dfs(xx, yy)
+                        if check(x, y, b) and check(nx, ny, a):
+                            Map[x][y], Map[nx][ny] = b, a
+                            dfs(xx, yy)
+                        Map[x][y], Map[nx][ny] = 0,0
+                visit[(a, b)] = 0
+    # 안채우고 가도 되는 경우
+    else:
+        dfs(xx, yy)
 
-tc = 1
+
+INF = int(1e10)
+steps = [(0, 1),(1 ,0),(0, -1),(-1, 0)]
 while 1:
+    domino = []
+    for i in range(1, 9):
+        for j in range(i+1, 10):
+            domino.append((i, j))
     N = int(input())
     if N == 0:
         break
-    print("Puzzle {}".format(tc))
-    tc += 1
-    # 도미노 사용 여부
-    domino = [[0]*10 for _ in range(10)]
-    for i in range(1, 9):
-        for j in range(i+1, 10):
-            domino[i][j] = 1
-    # 맵에 들어간 숫자 관리
+    res = 0
     Map = [[0]*9 for _ in range(9)]
-    # 도미노 위치 여부
-    in_domino = [[0]*9 for _ in range(9)]
     for _ in range(N):
-        a, X, b, Y = input().split()
-        a = int(a)
-        b = int(b)
-        x, y = ord(X[0]) - ord("A"), int(X[1]) - 1
-        Map[x][y] = a
-        x, y = ord(Y[0]) - ord("A"), int(Y[1]) - 1
-        Map[x][y] = b
-        if b < a:
-            a, b = b, a
-        domino[a][b] = 1
-    L = input().split()
+        a, pos_a, b, pos_b = input().split()
+        a, b = int(a), int(b)
+        pos_a = [ord(pos_a[0]) - 65, int(pos_a[1]) - 1]
+        pos_b = [ord(pos_b[0]) - 65, int(pos_b[1]) - 1]
+        Map[pos_a[0]][pos_a[1]] = a
+        Map[pos_b[0]][pos_b[1]] = b
+        a, b = min(a, b), max(a, b)
+        domino.remove((a, b))
+    num = list(input().split())
     for i in range(9):
-        x, y = ord(L[i][0]) - ord("A"), int(L[i][1]) - 1
-        Map[x][y] = i + 1
-    # for m in Map:
-    #     print(m)
-    dfs(Map, domino, N)
-    for m in Map:
-        print(m)
+        pos = [ord(num[i][0]) - 65, int(num[i][1]) - 1]
+        Map[pos[0]][pos[1]] = i+1
+    visit = defaultdict(int)
+    dfs(0, 0)
+    for m in res:
+        print(*m)
