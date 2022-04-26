86c307314979b96f3c00e44f2a9d9453b879e0f6
lee <aksndk123@naver.com>
2022-04-26 10:13:56 +0900

    ** 발상만 확인
    처음 800ms
    못가는경우 하나라도 있으면 -1처리 추가 700ms
    가지치기삭제 -> 2500ms
    간단한 가지치기 추가
    dist + min_val*(len(visit) - sum(visit)) > res
    에서 dist > res로 변경 -> 576ms  시간첫페이지 굿
    최종 350ms
    다익대신 BFS로 품 최대 N은 20이니 해봐야 400 이고 먼지도 해봐야 10개이니 맥시멈 4천번의 연산으로 각각의 거리들을 구할수 있음  굳이 다익쓸필요없음
    
    처음에 발상
    로봇 -> 가까운 먼지1
    먼지 -> 가까운 먼지2 ... 접근할려고 했는데 아닌거같아서 구글링해보니 아님
    더러운먼지는 10개이하, N은 40이하 이니 -> 브루트포스 비스무리하게 풀어도됨. 10! 해봐야 3만5천
    -** 결국엔 4000 + 3만5천으로 찍해봐야 십만단위 따라서 tc가 미치게 있는거 아닌이상 시간내 풀수있음을 알아야함
    
    그래서 각각의 로봇 ~ 먼지들 먼지1~로봇및나머지 거리를 다구하고 dfs돌리는걸로 발상함. 거리를 구할때 다익스트라 풀이함
    * 이부분을 BFS로 풀어도됨 사실상 플로이드같은 BFS지만
    
    그다름 구합값은 2차원 배열로 놓고 dfs풀이를 시작함
    가지치기도 넣어줌 - 효율적인지는 모르겠음 굳이란 느낌 - 그냥 뺴버림

--- 연습/그래프와 BFS/BFS 알고리즘/4991번 - 로봇 청소기.py
+++ 연습/그래프와 BFS/BFS 알고리즘/4991번 - 로봇 청소기.py
@@ -1,72 +1,77 @@
 from collections import deque
-import heapq
+steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
 INF = int(1e10)
-steps = [(1, 0),(0, 1),(-1, 0),(0, -1)]
-res = INF
 
 
-def dfs(L, now, dist, visit):
-    global res
-    # if visit.count(1) == len(visit):
-    if dist > res:
-        return
-    if sum(visit) == len(visit):
-        res = min(res, dist)
-        return
-    for i in range(len(visit)):
-        if visit[i] == 0:
-            visit[i] = 1
-            dfs(L, i,dist + L[now][i], visit)
-            visit[i] = 0
+def bfs(dist, Map, x, y, tr_pos):
+    visit = [[INF]*M for _ in range(N)]
+    q = deque()
+    q.append((x,y))
+    visit[x][y] = 0
+    while q:
+        x, y = q.popleft()
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M and visit[nx][ny] > visit[x][y] + 1 and Map[nx][ny] != "x":
+                visit[nx][ny] = visit[x][y] + 1
+                q.append((nx, ny))
+    for idx, (x, y) in enumerate(tr_pos):
+        dist[idx] = visit[x][y]
 
 
-def solve(Map, N, M):
-    global res
-    res = INF
-    min_val = INF
-    position_index = dict()
-    index = 1
-    L = []
+def find_dists():
+    tr_pos = []
     for i in range(N):
         for j in range(M):
-            if Map[i][j] == "o":
-                position_index[0] = (i,j)
             if Map[i][j] == "*":
-                position_index[index] = (i, j)
-                index += 1
-    # print(position_index)
-    length = len(position_index.keys())
-    for i in range(length):
-        sx, sy = position_index[i]
-        dist = [[INF]*M for _ in range(N)]
-        dist[sx][sy] = 0
-        q = [(0, sx, sy)]
-        while q:
-            now_dist, x, y = heapq.heappop(q)
-            if now_dist > dist[x][y]:
-                continue
-            for dx, dy in steps:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] != "x":
-                    if now_dist + 1 < dist[nx][ny]:
-                        dist[nx][ny] = now_dist + 1
-                        heapq.heappush(q,(dist[nx][ny], nx, ny))
-        temp = []
-        for i in range(length):
-            x, y = position_index[i]
-            if dist[x][y] == INF:
-                return -1
-            temp.append(dist[x][y])
-        L.append(temp)
-    visit = [0]*length
-    visit[0] = 1
-    dfs(L, 0, 0, visit)
-    return res
+                tr_pos.append((i,j))
+            elif Map[i][j] == "o":
+                tr_pos = [(i, j)] + tr_pos
+    dists = []
+    tr_cnt = len(tr_pos)
+    for idx, (x, y) in enumerate(tr_pos):
+        dist = [INF]*tr_cnt
+        bfs(dist, Map, x, y, tr_pos)
+        if INF in dist:
+            return -1
+        dists.append(dist)
+    return dists
+
+
+def dfs(now, cnt, visit, val): # now_idx, cnt, visit, val
+    global res
+    if cnt == dist_len:
+        res = min(res, val)
+        return
+    if val + min_dist*(dist_len-cnt) > res:
+        return
+    for next in range(dist_len):
+        if visit[next] == 0:
+            visit[next] = 1
+            dfs(next, cnt + 1, visit, val + dists[now][next])
+            visit[next] = 0
+
 
 
 while 1:
-    M, N = map(int,input().split())
-    if N == 0 and M == 0:
+    M, N = map(int, input().split())
+    if N == M == 0:
         break
     Map = [list(input()) for _ in range(N)]
-    print(solve(Map, N, M))
+    dists = find_dists()
+    if dists == -1:
+        print(dists)
+    else:
+        res = INF
+        # dists 구했으니 이걸토대로 가장 짧은 길이 구하기 dfs사용하자.
+        dist_len = len(dists)
+        visit = [0]*dist_len
+        min_dist = INF
+        for dist in dists:
+            for d in dist:
+                if d != 0:
+                    min_dist = min(d, min_dist)
+        visit[0] = 1
+        res = INF
+        dfs(0, 1, visit, 0) # now_idx, cnt, visit, val
+        print(res)
