3e2ad16a52c60b78851abb020cd820114ad88ca6
lee <aksndk123@naver.com>
2022-04-28 17:20:17 +0900

    ** DP풀이 비스무리 정답코드 전의껀 deque사용
    - why? 왜냐하면 특정 x, y좌표에대해서 같은 거리로 도달하지않는다는 점을 생각못함.
    대각선이동은 사실상 - | 이동을 한번에 하는거임.
    따라서 이러한 풀이를 하고 싶으면 해당 x, y도달할수 있는 경우의 수를 먼저 조사할수 있도록 해줘야함 -> heapq 발상
    
    힙으로 x,y 작은값이 앞으로 오도록 해서 푸니 visit값에 다넣은 다음 다음값에 대해서 조사를 할수 있게되어서 맞음.

--- 문제/브루트포스-40/브루트 포스2-13/17070번 - 파이프 옮기기 1.py
+++ 문제/브루트포스-40/브루트 포스2-13/17070번 - 파이프 옮기기 1.py
@@ -1,33 +1,34 @@
-from collections import deque, defaultdict
+from collections import defaultdict
+from heapq import heappush, heappop
 visit = defaultdict(int) # 도달한 갯수를 저장하자.
 N = int(input())
 Map = [list(map(int, input().split())) for _ in range(N)]
 shape = 0 # - 0 | 1 \ 2 로 두자
 x, y = 0, 1
 visit[(x, y, shape)] = 1
-q = deque()
+q = []
 q.append((x, y, shape))
 while q:
-    x, y, shape = q.popleft()
+    x, y, shape = heappop(q)
     # -> 이동
     if shape in [0, 2]:
         nx, ny = x , y + 1
         if 0 <= ny < N and Map[nx][ny] == 0:
             if visit[(nx, ny, 0)] == 0:
-                q.append((nx, ny, 0))
+                heappush(q, (nx, ny, 0))
             visit[(nx, ny, 0)] += visit[(x, y, shape)]
     # | 이동
     if shape in [1, 2]:
         nx, ny = x + 1, y
         if 0 <= nx < N and Map[nx][ny] == 0:
             if visit[(nx, ny, 1)] == 0:
-                q.append((nx, ny, 1))
+                heappush(q, (nx, ny, 1))
             visit[(nx, ny, 1)] += visit[(x, y, shape)]
     # \ 이동
     nx, ny = x + 1, y + 1
     if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == Map[x][y+1] == Map[x+1][y] == 0:
         if visit[(nx, ny, 2)] == 0:
-            q.append((nx, ny, 2))
+            heappush(q, (nx, ny, 2))
         visit[(nx, ny, 2)] += visit[(x, y, shape)]
 res = visit[(N-1, N-1, 0)] + visit[(N-1, N-1, 1)] + visit[(N-1, N-1, 2)]
 print(res)
