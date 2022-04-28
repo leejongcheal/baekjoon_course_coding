e8625bd58945dd83252c343c9890b6e95611a30a
lee <aksndk123@naver.com>
2022-04-28 15:55:27 +0900

    **** 삼성기출 운안좋으면 오래걸리는 문제 + 꼭 복습
    기술팁
    (prev_x, prev_y) < (x, y) 처럼 쓰거나
    prev_x *M + prev_y < M*x + y 처럼 사용
    ** 이때 10이 아닌 M값으로 차이를 줘야함.. 왜냐면 M이 10이상인경우에 x=0 y=11 x=1 y=0 이면 첫번째값이 11로 더커지기 때문에
    
    복습중 틀린부분 및 주의점 정리
    1. 사다리모양이 1-1 1-1 모양이 나올수 있으니 사다리마다 1이 아닌 다른값을 주어서 특정할수 있게 해야 어디로 내려갈지 정할수 있음
    2. 복잡한풀이 + 조합풀이했는데 틀림 -> 모르겠고, 그냥 삼성이니 다시풀기로함
    3.bfs 시간초과나서 dfs로 푸는데 답 찾으면 바로 return 했음
    -> 그값이 최솟값임을 보장하지 못하니 나가는 기준으로 여태 찾은값중 작은값으로 주고 cnt가 그 값보다 작은경우에만 조사하도록 수정
    4. dfs의 경우 전의값보다 큰값에 대해서만 조사하도록 했는데 0,0시작해버리면 0,0에 대해서 조사를 못하는 실수를함 -1,-1넣어주자.
    
    일단 사다리모양이 1-1 1-1 인 경우를 고려하지못했는데 찾음
    근데 틀렸다고 뜸... 이유를 몰라서 그냥 엎음..
    생각해보니 삼성 기출임 즉, 조합 모듈못씀 dfs풀이를 해보자.
    
    **** 아주 실수투성이 유발하는 멋친 문제 1,2,3,4 의 수정이 있었음. 정리 마지막에 특히 4번의 경우 외울정도로
    
    **  3번째 부분을 위해 수정하니 조금더 가는데 결국에 틀림
    
    **3. 왜 뜨나 생각해보니 최솟값이 아닐수도 있음 0123조사후 23 23 이런식으로 조사하기 때문에 res찾았다고 바로 return 때려버리면 안됨 -> 수정했는데 또틀림
    
    ***4 아... 0,0시작해버리니깐 0,0에 대해서 사다리를 놓을수가 없음... 아 진짜 ...
    
    **1.내가한 실수 x,y연결이면 x,y x,y+1에 대해서 1을 넣어줘서 연결선으로 둠
    근데 0,1 연결 2,3 연결인 경우 0 1 2 3이 1을 가짐 따라서 2에서 1로 갈지 3으로 갈지 알수없음..
    따라서 선에 따라 다른 번호를 두어서 구별을 두어서 풀기로함.
    
    2.시간초과를 줄이기위해 cnt을 바로 3으로 두고 now == cnt인 경우에 조사가 아닌 모든 경우에 조사를 해서 답이 나오면 현재 now값을 답으로 하기
    
    발상순서. 놓을 사다리 0, 1, 2, 3개로 나누어서 구함. 그리고 check해서 맞는 경우 그것을 답으로 하고 멈춤 -> BFS식 풀이했는데 시간초과가 뜸
    
    그냥 3개까지만  dfs 도는데 현재 몇개 구했는지 상관없이 체크해서 답을 구하고 구하면 return 때림 ->DFS식 풀이.
    시간초과는 안뜨는데 틀림 -> 방금 구한값이 최솟값이라는 보장이 없음
    
    따라서 DFS에서 답구한다음에 멈추는것이 아닌 더작은값이 있는지 조사하는식으로 변경. -> 모두구하면 당연히 시간초과가 뜨니 가지치기 해주면 상관없음 -> 쫌더 정확한데 틀림
    
    오류를 찾다보니 나는 dfs에서 0,0시작인데 0,0보다 큰 경우부터 찾는식으로 코드는 짬 ... 이부분이 진짜

--- 문제/브루트포스-40/브루트 포스2-13/15684번 - 사다리 조작.py
+++ 문제/브루트포스-40/브루트 포스2-13/15684번 - 사다리 조작.py
@@ -1,53 +1,47 @@
-def check(Map):
-    flag = 1
-    for j in range(M):
-        x, y = 0, j
-        while x < N:
-            if Map[x][y] != 0:
-                if y+1 < M and Map[x][y+1] == Map[x][y]:
-                    x, y = x + 1, y + 1
-                elif y - 1 >= 0 and Map[x][y-1] == Map[x][y]:
-                    x, y = x + 1, y - 1
-            elif Map[x][y] == 0:
-                x, y = x + 1, y
-        if y != j:
+def check1(y):
+    for x in range(0, N):
+        if Map[x][y] != 0:
+            if y - 1 >= 0 and Map[x][y - 1] == Map[x][y]:
+                y -= 1
+            elif y + 1 < M and Map[x][y + 1] == Map[x][y]:
+                y += 1
+    return y
+
+
+def check():
+    for i in range(M):
+        if i != check1(i):
             return 0
     return 1
 
 
-def dfs(Map, now, x, y):
-    global res, line_cnt
-    # 최솟값을 찾아야되는데 이미 2를 찾았으면 2,3조사가 아닌 1에 대해서만 조사하면됨.
-    if res <= now:
+def dfs(pre_x, pre_y, cnt):
+    global res
+    if check():
+        res = min(cnt, res)
         return
-    if check(Map):
-        res = min(res, now)
+    if cnt + 1 >= res:
         return
-    if now >= 3:
-        return
-    for i in range(N):
-        for j in range(M-1):
-            if i*10+j <= 10*x + y:
+    for x in range(N):
+        for y in range(M - 1):
+            if 10*x + y <= pre_x*10 + pre_y:
                 continue
-            # 전의 값보다 i,j 가 앞에 있는 경우
-            if Map[i][j] == Map[i][j+1] == 0:
-                Map[i][j] = Map[i][j+1] = 1 + line_cnt + now
-                dfs(Map, now + 1, i, j + 1)
-                Map[i][j] = Map[i][j+1] = 0
+            if Map[x][y] == 0 and Map[x][y+1] == 0 and cnt + 1 < res:
+                Map[x][y] = Map[x][y+1] = idx + cnt
+                dfs(x, y, cnt + 1)
+                Map[x][y] = Map[x][y + 1] = 0
 
 
-M, line_cnt, N = map(int, input().split())
+M, K, N = map(int, input().split())
 Map = [[0]*M for _ in range(N)]
-for i in range(line_cnt):
+idx = 1
+for _ in range(K):
     a, b = map(int, input().split())
-    a -= 1
-    b -= 1
-    Map[a][b] = Map[a][b+1] = i + 1
-INF = int(1e10)
-res = INF
-### 0,0 시작했는데 if i*10+j <= 10*x + y: 이부분 걸리는걸 생각못함..
-dfs(Map, 0, -1, -1)
-if res != INF:
-    print(res)
-else:
-    print(-1)
+    a, b = a - 1, b - 1
+    Map[a][b] = Map[a][b+1] = idx
+    idx += 1
+res = 4
+dfs(-1, -1, 0)
+if res == 4:
+    res = -1
+print(res)
