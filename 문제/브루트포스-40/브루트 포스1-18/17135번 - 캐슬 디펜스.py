33ab27b4ec09768d3c77cc7ed3754f78efeb021b
lee <aksndk123@naver.com>
2022-04-28 13:32:33 +0900

    *** 복잡한 구현 + 예외조건 잘 생각 + 디버깅 무조건 필요한 문제
    ㅋㅋ 디버깅으로 에러를 7개 정도 찾은듯
    1.temp 복사하는 부분 실수
    2. 궁수가 적을 찾으면 now_d 업데이트 해줘야하는데 안해줌
    3. attack에 대해서 pos만 삭제하고 Map은 그대로둠
    4. 적들이 한칸내려가는 경우 new_enmy를 구한다음에 Map을 업데이트해줘야하는데 도중에 업데이트해서 에러 생김 ㅋㅋㅋ 디버깅 없으면 어쩔뻔했어
    
    -전커밋
    연습용으로 딱좋음, 한번에 ac받긴했는데, 함수로 나누지 않고 풀거나 복잡하게 했으면 디버깅 하는부분에서 엄청오래 걸렸을꺼임.
    
    이런문제는 그냥 설계를 완벽하게 하고, 그설계를 보면서 코딩을 하자. 나같은 경우 enemy를 set으로 구하고 그 enemy에 대해서 cnt는 업데이트 해줬는데 Map을 업데이트안해줘서 틀림 -> 운좋게 빠른 디버깅했지만 운이 없다면...

--- 문제/브루트포스-40/브루트 포스1-18/17135번 - 캐슬 디펜스.py
+++ 문제/브루트포스-40/브루트 포스1-18/17135번 - 캐슬 디펜스.py
@@ -1,70 +1,62 @@
-from itertools import combinations
 from collections import deque
-from copy import deepcopy
-def bfs(x, y, Map, enemy):
-    visit = [[0]*M for _ in range(N)]
-    q = deque()
-    q.append((x-1, y, 1))
-    flag_d = 0
-    ds = []
-    while q:
-        x, y, d = q.popleft()
-        if d > D:
-            break
-        if flag_d and d > flag_d:
-            break
-        if Map[x][y] == 1:
-            flag_d = d
-            ds.append((y, x))
-        else:
-            for dx, dy in steps:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < N and 0 <= ny < M:
-                    q.append((nx, ny, d + 1))
-    if ds:
-        ds.sort()
-        y, x = ds[0]
-        enemy.add((x, y))
-
-
-def move_down(Map, enemy_cnt):
-    enemy_cnt -= Map[N-1].count(1)
-    for i in range(N-2, -1, -1):
-        for j in range(M):
-            Map[i+1][j] = Map[i][j]
-    for j in range(M):
-        Map[0][j] = 0
-    return Map, enemy_cnt
-
-
-
-def solve(A, Map, D, res):
-    enemy_cnt = total_enemy
-    re_cnt = 0
-    while enemy_cnt > 0:
-        # 궁수로 제거하기
-        enemy = set()
-        for aj in A:
-            bfs(N, aj, Map, enemy)
-
-        enemy_cnt -= len(enemy)
-        re_cnt += len(enemy)
-        for x, y in enemy:
+from itertools import combinations
+steps = [(0, 1),(0, -1),(-1, 0)]
+def solve(enemy_pos, cnt, Map):
+    global res
+    r = 0
+    while cnt:
+        attack = set()
+        # 궁수공격
+        for a in archer:
+            now_res = D
+            now = []
+            q = deque()
+            q.append((N-1, a, 1))
+            while q:
+                x, y, dist = q.popleft()
+                if dist > now_res:
+                    break
+                if Map[x][y] == 1:
+                    now_res = dist
+                    now.append([x, y])
+                for dx, dy in steps:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < M:
+                        q.append((nx, ny, dist + 1))
+            if now:
+                now.sort(key=lambda x:x[1])
+                attack.add(tuple(now[0]))
+        # 적 업데이트
+        r += len(attack)
+        for x, y in attack:
+            enemy_pos.remove([x, y])
             Map[x][y] = 0
-        # 적들 밑으로 이동하기
-        Map, enemy_cnt = move_down(Map, enemy_cnt)
-    return re_cnt
+        # 적 내려가기
+        new_enemy = []
+        for x, y in enemy_pos:
+            Map[x][y] = 0
+            x += 1
+            if x == N:
+                continue
+            new_enemy.append([x, y])
+        for x, y in new_enemy:
+            Map[x][y] = 1
+        enemy_pos = new_enemy
+        cnt = len(enemy_pos)
+    res = max(res, r)
 
 
 
-steps = [(1, 0),(0, 1),(-1, 0),(0, -1)]
 N, M, D = map(int, input().split())
-Map = [list(map(int, input().split())) for _ in range(N)]
-total_enemy = sum([x.count(1) for x in Map])
+origin = [list(map(int, input().split())) for _ in range(N)]
+enemy_pos = []
+for i in range(N):
+    for j in range(M):
+        if origin[i][j] == 1:
+            enemy_pos.append([i, j])
+enemy_total = len(enemy_pos)
 res = 0
-for archers_j in combinations(range(M), 3):
-    # print(archers_j)
-    temp = deepcopy(Map)
-    re_cnt = solve(archers_j, temp, D, res)
-    res = max(res, re_cnt)
+for archer in combinations(range(M), 3):
+    temp = [origin[i][:] for i in range(N)]
+    solve(enemy_pos[:], enemy_total, temp)
 print(res)
