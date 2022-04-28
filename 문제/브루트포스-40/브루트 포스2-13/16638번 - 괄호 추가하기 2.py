49ad691886bbaadd8e39bd9c912028b9fe53fa97
lee <aksndk123@naver.com>
2022-04-28 16:27:57 +0900

    * eval 함수 알면 너무 편안
    전의 괄호추가하기 수정된 코드를 이용해서 바로품
    이런문제의 핵심은
    0 + 나머지
    00 + 나머지 로 나누면
    앞자리가 다르기 떄문에! 나머지가 뭐가 나오든 유일한 값을 가지게 된다.
    
    괄호추가하기1은 val에 여태 계산된 값을 넣었음 -> eval이 연산자 우선순위에 따라 계산하기 때문에
     괄호2는 우선순위에 맞게 계산하니 괄호만 추가한 문자열을 완성시키고 eval로 계산하면됨
    
    1. eval 함수를 쓸줄 아냐?
    2. 괄호를 구하는 과정에서 그냥 dfs로 할려니깐 못풀겠음 -> 괄호 갯수에 따라서 뽑을려고 하니깐 문제가 쉬워짐
    조합인데 갯수에 따른 조합을 한번에 조합으로 풀려하니깐 복잡해짐.
    그냥 갯수에 따라서 조합다 돌리면 되는 문제임.

--- 문제/브루트포스-40/브루트 포스2-13/16638번 - 괄호 추가하기 2.py
+++ 문제/브루트포스-40/브루트 포스2-13/16638번 - 괄호 추가하기 2.py
@@ -1,45 +1,35 @@
-from copy import deepcopy
-def dfs(now, visit, depth):
-    if depth == C:
-        res.append((deepcopy(visit)))
-        return
-    if now >= M - 1:
-        return
-    for i in range(now, M - 1):
-        if visit[i] == 0:
-            visit[i] = visit[i+1] = 1
-            dfs(i + 2, visit, depth + 1)
-            visit[i] = visit[i+1] = 0
-def calculate(r):
-    s = ""
-    i = 0
-    while i < M:
-        if r[i] == 0:
-            if i != M-1:
-                s += number[i] + oper[i]
-            else:
-                s += number[i]
-            i += 1
-        elif r[i] == 1:
-            temp = number[i] + oper[i] + number[i+1]
-            s += str(eval(temp))
-            if i+1 != M - 1:
-                s += oper[i+1]
-            i += 2
-    return eval(s)
-
+def dfs(val, op_idx, num):
+    global res
+    if op_idx == oper_cnt:
+        res = max(res, eval(val))
+    # 한개 가능
+    if op_idx + 1 <= oper_cnt:
+        temp = val + oper[op_idx] + num[0]
+        dfs(temp, op_idx + 1, num[1:])
+    # 두개 가능
+    if op_idx + 2 <= oper_cnt:
+        temp = val + oper[op_idx] + '(' + num[0] + oper[op_idx + 1] + num[1] + ')'
+        dfs(temp, op_idx + 2, num[2:])
 
 
 N = int(input())
-L = list(input())
-oper = [L[i] for i in range(N) if i %2 == 1]
-number = [L[i] for i in range(N) if i %2 == 0]
-M = len(number)
-res = [[0]*M]
-cal_max = -1e10
-visit = [0]*M
-for C in range(1, M//2 + 1):
-    dfs(0, visit, 0)
-for r in res:
-    cal_max = max(cal_max, calculate(r))
-print(cal_max)
+idx = 0
+num = []
+oper = []
+for s in list(input()):
+    if idx % 2 == 0:
+        num.append(s)
+    else:
+        oper.append(s)
+    idx += 1
+num_cnt = len(num)
+oper_cnt = len(oper)
+res = -(1e10)
+if N == 1:
+    res = int(num[0])
+elif N == 3:
+    res = eval(num[0] + oper[0] + num[1])
+else:
+    dfs(num[0], 0, num[1:])
+    dfs("(" + num[0] + oper[0] + num[1] + ")", 1, num[2:])
+print(res)
