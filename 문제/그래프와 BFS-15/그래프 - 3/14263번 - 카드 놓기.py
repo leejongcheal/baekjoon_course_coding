699639b8514df108306838cbeb463e1c7f9e6b6c
lee <aksndk123@naver.com>
2022-04-29 17:37:12 +0900

    **** 위상정렬 어려운 응용문제
    확실히 힙이 이런구조에선 빠름 172 -> 164
    1. res를 구하는 과정에서 우선순위를 가지고 높는 방식에서
    위상이 0 에대해서 고정적으로 놓아야하는것과 우선수위에 맞게 놓아야하는(0끼리에 대해서) 로 나누어서 어렵게 풀려고 했으나 틀림.
    그냥 q에 위상이 0인것을 넣으면서 젤 작은값을 pop하는 방식으로 바꾸어서 맞음
    + 이부분을 힙으로 푸는 방법또한 해보자.
    
    일단 위치좌표 잡는걸 똑똑하게 했다고 생각하는데 틀렸다고 뜸 반례점...
    
    아 나는 하나하나 비교하면서 겹치면서 다른값을 가지거에 대해서 겹치는 좌표를 구한다음에 겹치는 부분에서 뭐가 우선값을 갖는지 구한다음에 위상정렬 준비를 했음.
    이 과정에서 겹치는 부분에 2가지말고 다른값이 들어가는 경우가 있는데 이를위해서 a, b로 나누어서 풀었는데 이부분에서 a에 2개외의 다른값이 들어가는 경우가 생긴듯?
    이 부분을 수정했는데도 틀림 ㅋㅋ
    
    그냥 하나하나의 좌표를 확인하면서 자신의 값이 아닌 다른값이 들어간 경우에 위상정렬을 위한 준비를 해보자
    
    -> 위의 방식이 편한데 내가 틀릴부분은 그쪽이아님
    불가능한 경우에 -1 출력 부분에서 틀렸음.
    예시와 같이
    12
    21 같은 경우만 불가능한게 아님.
    경우의 수로 나누는게 아니라 일단 위상정렬을 한다음에 모든 문자열이 답에 들어갔는지 또는 중복된것이 없는지 확인을 해서 -1출력해줬어야함.
    
    보통 불가능한 경우에는 큐가 비어서 모든탐색을 못하는 경우를 불가능하다고 표현함.
    큐에 여러개가 있는 경우는 만들수있는 경우의수가 많은것을 뜻함

--- 문제/그래프와 BFS-15/그래프 - 3/14263번 - 카드 놓기.py
+++ 문제/그래프와 BFS-15/그래프 - 3/14263번 - 카드 놓기.py
@@ -1,58 +1,48 @@
-from collections import deque, defaultdict
-import heapq
-
-
-def find(val):
-    L = []
-    for i in range(N):
-        for j in range(M):
-            if Map[i][j] == val:
-                L.append([i, j])
-    L.sort()
-    n_list = [i for i in range(L[0][0], L[-1][0] + 1)]
-    L.sort(key=lambda x: x[1])
-    m_list = [i for i in range(L[0][1], L[-1][1] + 1)]
-    group = set()
-    for i in n_list:
-        for j in m_list:
-            group.add((i, j))
-    return group
-
-
-steps = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+from collections import defaultdict
+from heapq import heappop, heappush
 N, M = map(int, input().split())
 Map = [list(input()) for _ in range(N)]
-groups = defaultdict(set)
+char = set()
 for i in range(N):
     for j in range(M):
-        if Map[i][j]!= "." and Map[i][j] not in groups.keys():
-            groups[Map[i][j]] = find(Map[i][j])
-indegree = defaultdict(int)
+        if Map[i][j] != ".":
+            char.add(Map[i][j])
+char = list(char)
+total = len(char)
+char_num = dict()
 graph = defaultdict(list)
-visit = []
-flag = 0
-for val in groups.keys():
-    for x, y in groups[val]:
-        if Map[x][y] != val and Map[x][y] not in graph[val]:
-            graph[val].append(Map[x][y])
-            indegree[Map[x][y]] += 1
-
-q = []
+indegree = defaultdict(int)
+for idx in range(total):
+    now = char[idx]
+    now_index = []
+    for i in range(N):
+        for j in range(M):
+            if Map[i][j] == now:
+                now_index.append([i, j])
+    now_index.sort()
+    sx, ex = now_index[0][0], now_index[-1][0]
+    now_index.sort(key=lambda x:x[1])
+    sy, ey = now_index[0][1], now_index[-1][1]
+    nexts = set()
+    for x in range(sx, ex + 1):
+        for y in range(sy, ey + 1):
+            if Map[x][y] != now:
+                nexts.add(Map[x][y])
+    for next in nexts:
+        graph[now].append(next)
+        indegree[next] += 1
 res = ""
-for key in groups.keys():
-    if indegree[key] == 0:
-        heapq.heappush(q,key)
+q = []
+for c in char:
+    if indegree[c] == 0:
+        heappush(q, c)
 while q:
-    now = heapq.heappop(q)
+    now = heappop(q)
     res += now
     for next in graph[now]:
         indegree[next] -= 1
         if indegree[next] == 0:
-            heapq.heappush(q, next)
-for key in groups.keys():
-    if key not in res:
-        res = - 1
-        break
-if res != -1 and len(res) != len(groups):
+            heappush(q, next)
+if len(res) != total:
     res = -1
 print(res)
