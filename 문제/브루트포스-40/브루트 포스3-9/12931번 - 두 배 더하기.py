40bf6cf49f400bd6e45be3a270fdd571d24c808c
lee <aksndk123@naver.com>
2022-04-29 13:07:41 +0900

    * 기술 A->B문제를 반대로 풀기
    A -> B 로 갈려하니깐 어려웠는데..
    B -> 0으로 간다고 생각하니깐 쉬워진 문제
    
    애초에 브루트포스로 어떻게 풀껀데.. 푼다고해도 설계자체가 너무 어려운데?
    할순있는데... 그생각하기전에 이렇게 풀듯

--- 문제/브루트포스-40/브루트 포스3-9/12931번 - 두 배 더하기.py
+++ 문제/브루트포스-40/브루트 포스3-9/12931번 - 두 배 더하기.py
@@ -1,22 +1,15 @@
 N = int(input())
-A = list(map(int, input().split()))
-B = []
-for i in A:
-    if i != 0:
-        B.append(i)
+B = list(map(int, input().split()))
 N = len(B)
 cnt = 0
 while sum(B) != 0:
-    flag = 1
-    # print(B, cnt)
     for i in range(N):
         if B[i]%2 != 0:
             B[i] -= 1
             cnt += 1
-            flag = 0
-            continue
-    if flag:
-        for i in range(N):
-            B[i] //= 2
-        cnt += 1
+    if sum(B) == 0:
+        break
+    for i in range(N):
+        B[i] //= 2
+    cnt += 1
 print(cnt)
