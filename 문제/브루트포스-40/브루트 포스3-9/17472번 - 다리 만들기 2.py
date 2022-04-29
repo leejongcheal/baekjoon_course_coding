dbdd16a0d5a2d1d8815b086c36663d67af848e74
lee <aksndk123@naver.com>
2022-04-29 15:55:10 +0900

    **** 아주 멋진 문제 또 풀어도 멋짐 근데 더럽긴함
    복습할때 풀이과정
    1. 섬별로 id주기
    2. 섬별로 갈수있는 섬 + 최소거리 구하기 - 자료구조가 더러움
    3. 다연결 가능한 방법 조사하면서 res 구하기
     - 끝점에대해서만 조사하는 식으로 하니 문제가 생김. 지나온곳에서 다리를 여러개 설치하는 경우도 고려되어야함
    
    전의 커밋
    
    처음에 섬별로 갈수있는 최소거리를 미리 구한다음에 풀면 쉬운데 더럽게 풀었고 탐색처리하는게 복잡해서 틀림
    간단하게 만드니탐색자체도 쉬워짐.... 설계문제 + 처음에 문제를 잘못이해함. 다리를 무조건 그려서 구해야 쉬운문제로 착각함
    
    1. 길을 채우면서 가는 경우 지울때, 겹치는 간선도 지워버려서 좌표로 외워서 나중에 그리기로 바꿈
    
    2. 겹치는 부분은 1로 치는게 아닌 겹치는 만큼 + 해주는거임.. 좌표로만 판별하기로 바꿈
    
    3. 1의 번호에 여러개의 다리가 생기는 경우가 있는데 이를 탐색에 녹여내지못함
    
    4. 1->2->3  5->6->7->2 의 경우 탐색에 들어가짐 못함 아오 그냥 grapg거리별로 만들어서 dfs탐색해서 결과중 작은값을 답으로 하기 ㅋㅋ
    ----> 이건 내가 잘못생각함 어쨋든 모두 연결이 되면 어느 지점에서 시작하든 연결이 되어야함 탐색부분은 잘못된건 맞는데 저생각은 틀림

--- 문제/브루트포스-40/브루트 포스3-9/17472번 - 다리 만들기 2.py
+++ 문제/브루트포스-40/브루트 포스3-9/17472번 - 다리 만들기 2.py
@@ -1,71 +1,69 @@
 from collections import deque, defaultdict
-from copy import deepcopy
-def island_number(x, y):
-    global island_cnt
-    for dx, dy in steps:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] == 1 and visit[nx][ny] == 0:
-            visit[nx][ny] = 1
-            Map[nx][ny] = island_cnt
-            group.append((nx, ny))
-            island_number(nx, ny)
-def make_graph():
-    for island in range(island_cnt):
-        for x, y in island_group[island]:
-            for dx, dy in steps:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] == 0:
-                    d = 1
-                    flag = 0
-                    while 1:
-                        nx += dx
-                        ny += dy
-                        if not (0 <= nx < N and 0 <= ny < M):
-                            break
-                        elif Map[nx][ny] != 0:
-                            flag = 1
-                            break
-                        d += 1
-                    if flag and d >= 2:
-                        a, b = island, Map[nx][ny] - 1
-                        graph[a][b] = min(graph[a][b], d)
-                        graph[b][a] = min(graph[b][a], d)
-
-INF = int(1e10)
 steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
 N, M = map(int, input().split())
 Map = [list(map(int, input().split())) for _ in range(N)]
-island_cnt = 1
-island_group = []
-visit = [[0]*M for _ in range(N)]
+idx = 2
+# 섬 별로 id 주기
 for i in range(N):
     for j in range(M):
-        if visit[i][j] == 0 and Map[i][j] != 0:
-            visit[i][j] = 1
-            Map[i][j] = island_cnt
-            group = [(i, j)]
-            island_number(i, j)
-            island_cnt += 1
-            island_group.append(group)
-island_cnt -= 1
-graph = [[INF]*island_cnt for _ in range(island_cnt)]
-make_graph()
-res = INF
-q = deque()
-q.append(([0], 0))
-
-while q:
-    tracked, dist = q.popleft()
-    if len(tracked) == island_cnt:
-        res = min(res, dist)
-        continue
-    for now in tracked:
-        for next in range(island_cnt):
-            if graph[now][next] != INF and next not in tracked:
-                q.append((tracked+[next], dist + graph[now][next]))
-if res != INF:
-    print(res)
-else:
-    print(-1)
-
-
+        if Map[i][j] == 1:
+            q = deque()
+            q.append((i, j))
+            Map[i][j] = idx
+            while q:
+                x, y = q.popleft()
+                for dx, dy in steps:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < M and Map[nx][ny] == 1:
+                        q.append((nx, ny))
+                        Map[nx][ny] = idx
+            idx += 1
+# 섬별로 가능한 다리 구하기
+check = defaultdict(int)
+graph = defaultdict(int)
+graph_list = defaultdict(set)
+for i in range(N):
+    for j in range(M):
+        if Map[i][j] != 0 and check[Map[i][j]] == 0:
+            a = Map[i][j]
+            q = [(i, j)]
+            while q:
+                x, y = q.pop()
+                for dx, dy in steps:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < N and 0 <= ny < M:
+                        if Map[nx][ny] == 0:
+                            dist = 0
+                            while 0 <= nx + dx < N and 0 <= ny + dy < M:
+                                dist += 1
+                                nx += dx
+                                ny += dy
+                                if Map[nx][ny] != 0:
+                                    if Map[nx][ny] != a and dist >= 2:
+                                        b = Map[nx][ny]
+                                        graph_list[a-2].add(b-2)
+                                        if graph[(a-2, b-2)] == 0 or graph[(a-2, b-2)] > dist:
+                                            graph[(a-2,b-2)] = dist
+                                    break
+                        elif Map[nx][ny] == a and check[(nx ,ny)] == 0:
+                            q.append((nx, ny))
+                            check[(nx, ny)] = 1
+# 연결가능한것중 최소 거리 구하기
+res = INF = int(1e10)
+idx -= 2
+visit = [0]*idx
+for start in range(idx):
+    q = deque()
+    q.append(([start], 0))
+    while q:
+        traced, dist = q.popleft()
+        if len(traced) == idx:
+            res = min(res, dist)
+            continue
+        for now in traced:
+            for next in graph_list[now]:
+                if next not in traced:
+                    q.append((traced + [next], dist + graph[(now, next)]))
+if res == INF:
+    res = -1
+print(res)
