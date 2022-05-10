837208b91c9b77da1b19b1b454f4f9dd00837f6f
lee <aksndk123@naver.com>
2022-05-10 14:42:13 +0900

    ** 체스판1과 비교 visit의 key를 늘리는게 아닌 value에 추가해서 시간초과를 피함 + 정확한 논리 더추가함
    * 답을 구할때 결과가 나오면 바로 break했는데, 이는 거리에 대해서는 만족하지만 바꾼횟수에 대해서는 만족한다고 볼수없음.
    따라서 해당 q에 대해서 답을 구했다면 돌면서 최솟값(dist, change 기준)을 구하고 q가 끝난다음에 break하는식으로 논리 수정함
    
    비교횟수까지 알아야해서 visit의 key에 비교횟수까지 추가해서 푸니깐 -> 시간초과...
    
    차라리 visit에 [dist, 말바꾼횟수]로 해서 visit비교할때, dist가 더작으면 업데이트 또는 (dist가 같고 횟수가 더적은경우)에도 업데이트하면 체스판1이랑 거의 비슷한 경우의수를 가짐 이를 통해서 시간초과를 해결함

--- 문제/그래프와 BFS-15/BFS 알고리즘 - 12/16959번 - 체스판 여행 1.py
+++ 문제/그래프와 BFS-15/BFS 알고리즘 - 12/16959번 - 체스판 여행 1.py
@@ -9,46 +9,47 @@ for i in range(N):
         if Map[i][j] == 1:
             sx, sy = i, j
 q = deque()
-q.append((1, sx, sy, 0, 1))
-q.append((1, sx, sy, 1, 1))
-q.append((1, sx, sy, 2, 1))
-res = -1
+q.append((1, sx, sy, 0, 1, 0)) # type, dist, change 순
+q.append((1, sx, sy, 1, 1, 0))
+q.append((1, sx, sy, 2, 1, 0))
+INF = int(1e10)
+res = [INF,INF]
 visit = defaultdict(int) # 번호 좌표 타입 = dist
-visit[(1, sx, sy, 0)] = 1
-visit[(1, sx, sy, 1)] = 1
-visit[(1, sx, sy, 2)] = 1
+visit[(1, sx, sy, 0)] = [1, 0]
+visit[(1, sx, sy, 1)] = [1, 0]
+visit[(1, sx, sy, 2)] = [1, 0]
 temp1 = []
 temp2 = []
 while q:
     while q:
-        num, x, y, type, dist = q.pop()
-        if visit[(num, x, y, type)] < dist:
+        num, x, y, type, dist, change = q.pop()
+        if visit[(num, x, y, type)] < [dist, change]:
             continue
         if Map[x][y] == num + 1:
             num += 1
             if num == N*N:
-                res = dist - 1
-                break
-            visit[(num,x ,y, type)] = dist
+                if res > [dist-1, change]:
+                    res = [dist-1, change]
+            visit[(num,x ,y, type)] = [dist, change]
         # 나이트 이동
         if type == 0:
-            nd = dist + 1
+            nd = [dist + 1, change]
         else:
-            nd = dist + 2
+            nd = [dist + 2, change+1]
         for dx, dy in knight:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N and visit[(num,nx,ny,0)] == 0 or visit[(num, nx, ny, 0)] > nd:
+            if 0 <= nx < N and 0 <= ny < N and (visit[(num,nx,ny,0)] == 0 or visit[(num, nx, ny, 0)] > nd):
                 visit[(num, nx, ny, 0)] = nd
                 if type == 0:
-                    temp1.append((num, nx, ny, 0, nd))
+                    temp1.append((num, nx, ny, 0, nd[0], nd[1]))
                 else:
-                    temp2.append((num, nx, ny, 0, nd))
+                    temp2.append((num, nx, ny, 0, nd[0], nd[1]))
         # 비숍 룩 이동
         for ntype, oper in [(1, bishop), (2, lock)]:
             if ntype == type:
-                nd = dist + 1
+                nd = [dist + 1, change]
             else:
-                nd = dist + 2
+                nd = [dist + 2, change + 1]
             for dx, dy in oper:
                 nx, ny = x, y
                 while 0 <= nx + dx < N and 0 <= ny + dy < N:
@@ -57,15 +58,15 @@ while q:
                     if visit[(num, nx,ny,ntype)] == 0 or visit[(num, nx,ny,ntype)] > nd:
                         visit[(num, nx,ny,ntype)] = nd
                         if type == ntype:
-                            temp1.append((num, nx, ny, ntype, nd))
+                            temp1.append((num, nx, ny, ntype, nd[0],nd[1]))
                         else:
-                            temp2.append((num, nx, ny, ntype, nd))
-    if res != -1:
+                            temp2.append((num, nx, ny, ntype, nd[0], nd[1]))
+    if res[0] != INF:
         break
     q = temp1
     temp1 = temp2
     temp2 = []
-print(res)
+print(res[0], res[1])
 
 
 
