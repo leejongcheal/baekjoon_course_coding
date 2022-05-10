bee1fde64ecee34f4cda64e0ee1baa494dca460c
lee <aksndk123@naver.com>
2022-05-10 14:30:04 +0900

    ****복습중 찾지못한 문제 생김 -> 새로 푸니깐 풀림
    복잡하네 + BFS인데 그냥 쓰면 최솟값 유지가 안되는 문제해결법 + 이중반복분 break위치 외우기
    + 깔끔코딩
    
    와 답이 12가 나와야되는데 13나와서 개같앗는데
    넣을때 dist+1 or dist+2값이 들어가서 값이 12 or 13의 값을 가질수 있음을 기억해야함 이거떄문에 왜틀렸나 고민했네 + 근데 어쩌피 시간초과 아오
    
    위의 문제를 해결하기 위해 모든 큐에대해서 돈다음에 작은 res값을 출력했는데 -> 결국엔 BFS풀이가 아닌 DFS풀이가 되버리니 시간초과가 당연함.
    
    따라서 q를 돌면서 +1인경우는 temp1에 +2인 경우는 temp2에 넣어줌
    q에 대해서 다돌고 temp1을 큐로 temp2는 temp1에 넣어주는식으로 거리별로 다른 리스트를 가지게해서 만약에 res가 구해진다면 그값이 최솟값임을 보장해주니 끝내도록 하는식으로 문제를 바꾸니 시간초과 안남

--- 문제/그래프와 BFS-15/BFS 알고리즘 - 12/16959번 - 체스판 여행 1.py
+++ 문제/그래프와 BFS-15/BFS 알고리즘 - 12/16959번 - 체스판 여행 1.py
@@ -3,85 +3,70 @@ knight = [(-2, -1),(-2, 1),(-1, 2),(1, 2),(2, 1),(2, -1),(1, -2),(-1, -2)]
 bishop = [(-1, -1),(-1, 1),(1, 1),(1, -1)]
 lock = [(1, 0),(-1, 0),(0, 1),(0, -1)]
 N = int(input())
-INF = int(1e10)
 Map = [list(map(int, input().split())) for _ in range(N)]
-steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
-# graph [나이트 비숍 룩 0  1 2] [해당값에서][해당값으로이동]
 for i in range(N):
     for j in range(N):
         if Map[i][j] == 1:
             sx, sy = i, j
-# q = 현재번호(1시작 N^N이면 끝) x y 말타입(0,1,2 나이트 비숍 룩) 거리
 q = deque()
-q.append((1, sx, sy, 0, 0))
-q.append((1, sx, sy, 1, 0))
-q.append((1, sx, sy, 2, 0))
+q.append((1, sx, sy, 0, 1))
+q.append((1, sx, sy, 1, 1))
+q.append((1, sx, sy, 2, 1))
 res = -1
 visit = defaultdict(int) # 번호 좌표 타입 = dist
-visit[(1, sx, sy, 0)] = 0
-visit[(1, sx, sy, 1)] = 0
-visit[(1, sx, sy, 2)] = 0
+visit[(1, sx, sy, 0)] = 1
+visit[(1, sx, sy, 1)] = 1
+visit[(1, sx, sy, 2)] = 1
 temp1 = []
 temp2 = []
 while q:
-    temp2 = []
     while q:
         num, x, y, type, dist = q.pop()
+        if visit[(num, x, y, type)] < dist:
+            continue
         if Map[x][y] == num + 1:
             num += 1
             if num == N*N:
-                res = dist
+                res = dist - 1
                 break
-            visit[(num, x, y, type)] = dist
-        # 나이트 이동 0
+            visit[(num,x ,y, type)] = dist
+        # 나이트 이동
         if type == 0:
-            nd = dist
-        else:
             nd = dist + 1
+        else:
+            nd = dist + 2
         for dx, dy in knight:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N:
-                if visit[(num, nx, ny, 0)] == 0 or visit[(num, nx, ny, 0)] > nd + 1:
-                    visit[(num, nx, ny, 0)] = nd + 1
-                    if nd == dist:
-                        temp1.append((num, nx, ny, 0, nd + 1))
-                    else:
-                        temp2.append((num, nx, ny, 0, nd + 1))
-        # 비숍 이동 1
-        if type == 1:
-            nd = dist
-        else:
-            nd = dist + 1
-        for dx, dy in bishop:
-            nx, ny = x + dx, y + dy
-            while 0 <= nx < N and 0 <= ny < N:
-                if visit[(num, nx, ny, 1)] == 0 or visit[(num, nx, ny, 1)] > nd + 1:
-                    visit[(num, nx, ny, 1)] = nd + 1
-                    if nd == dist:
-                        temp1.append((num, nx, ny, 1, nd + 1))
-                    else:
-                        temp2.append((num, nx, ny, 1, nd + 1))
-                nx += dx
-                ny += dy
-        # 룩이동  2
-        if type == 2:
-            nd = dist
-        else:
-            nd = dist + 1
-        for dx, dy in lock:
-            nx, ny = x + dx, y + dy
-            while 0 <= nx < N and 0 <= ny < N:
-                if visit[(num, nx, ny, 2)] == 0 or visit[(num, nx, ny, 2)] > nd + 1:
-                    visit[(num, nx, ny, 2)] = nd + 1
-                    if nd == dist:
-                        temp1.append((num, nx, ny, 2, nd + 1))
-                    else:
-                        temp2.append((num, nx, ny, 2, nd + 1))
-                nx += dx
-                ny += dy
-    q = temp1
-    temp1 = temp2
+            if 0 <= nx < N and 0 <= ny < N and visit[(num,nx,ny,0)] == 0 or visit[(num, nx, ny, 0)] > nd:
+                visit[(num, nx, ny, 0)] = nd
+                if type == 0:
+                    temp1.append((num, nx, ny, 0, nd))
+                else:
+                    temp2.append((num, nx, ny, 0, nd))
+        # 비숍 룩 이동
+        for ntype, oper in [(1, bishop), (2, lock)]:
+            if ntype == type:
+                nd = dist + 1
+            else:
+                nd = dist + 2
+            for dx, dy in oper:
+                nx, ny = x, y
+                while 0 <= nx + dx < N and 0 <= ny + dy < N:
+                    nx += dx
+                    ny += dy
+                    if visit[(num, nx,ny,ntype)] == 0 or visit[(num, nx,ny,ntype)] > nd:
+                        visit[(num, nx,ny,ntype)] = nd
+                        if type == ntype:
+                            temp1.append((num, nx, ny, ntype, nd))
+                        else:
+                            temp2.append((num, nx, ny, ntype, nd))
     if res != -1:
         break
+    q = temp1
+    temp1 = temp2
+    temp2 = []
 print(res)
+
+
+
 
