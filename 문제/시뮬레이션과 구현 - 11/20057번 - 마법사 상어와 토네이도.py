2461b346b87f1076c5aec8f95313b932dbc1e21a
lee <aksndk123@naver.com>
2022-05-13 14:42:58 +0900

    복습함  350 -> 220으로 더빨라짐
    다 0이고 하나만 100인걸 이용해서 내가 left down right up에서 인덱스 실수함을 찾아내고 수정해서 품

--- 문제/시뮬레이션과 구현 - 11/20057번 - 마법사 상어와 토네이도.py
+++ 문제/시뮬레이션과 구현 - 11/20057번 - 마법사 상어와 토네이도.py
@@ -1,61 +1,54 @@
-steps = [(0, -1),(1, 0),(0, 1),(-1, 0)]
-def sand_move(x, y, dx, dy):
-    # N범위 넘어가는건 Map넣지는 않지만 모래가 없어지는거긴함.
-    val = Map[x][y]
-    ex, ey = x + dx, y + dy
-    if dx != 0:
-        step10 = [(dx, 1),(dx, -1)]
-        step7 = [(0,1),(0,-1)]
-        step5 = [(2*dx, 0)]
-        step2 = [(0,2),(0,-2)]
-        step1 = [(dx*-1, 1), (dx*-1, -1)]
-    elif dy != 0:
-        step10 = [(1, dy), (-1, dy)]
-        step7 = [(1, 0), (-1, 0)]
-        step5 = [(0, 2*dy)]
-        step2 = [(2, 0), (-2, 0)]
-        step1 = [(1, -1*dy), (-1, -1*dy)]
-    step_num = [step10, step7,step5,step2,step1]
-    num = [10, 7, 5, 2, 1]
-    for i, step in enumerate(step_num):
-        n = num[i]
-        trash = int(val / 100 * n)
-        for dx, dy in step:
-            nx, ny =  x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N:
-                Map[nx][ny] += trash
-            Map[x][y] -= trash
-    if 0 <= ex < N and 0 <= ey < N:
-        Map[ex][ey] += Map[x][y]
-    Map[x][y] = 0
-
 N = int(input())
-Map = [list(map(int, input().split())) for _ in range(N)]
-first_sand = sum([sum(x) for x in Map])
-# 이동 구현
-x = y = (N-1) // 2
-idx = 1
-move = 1
-d = 0
-Map[x][y] = 0
-flag = 0
-while flag != 1:
-    dx, dy = steps[d]
-    for i in range(move):
+left = [ (-2, 0, 0.02), (-1, 0, 0.07), (+1, 0, 0.07), (+2, 0, 0.02),(-1, +1, 0.01), (+1, +1, 0.01), (-1, -1, 0.1),
+        (+1, -1, 0.1), (0, -2, 0.05)]
+down = [(0, -2, 0.02), (0, -1, 0.07), (0, 1, 0.07), (0, 2, 0.02), (-1, -1, 0.01), (-1, 1, 0.01), (1, -1, 0.1),
+        (1, 1, 0.1), (2, 0, 0.05)]
+right = [(-2, 0, 0.02), (-1, 0, 0.07), (1, 0, 0.07), (2, 0, 0.02), (-1, -1, 0.01), (1, -1, 0.01), (-1, 1, 0.1),
+         (1, 1, 0.1), (0, 2, 0.05)]
+up = [(0, -2, 0.02), (0, -1, 0.07), (0, 1, 0.07), (0, 2, 0.02), (1, -1, 0.01), (1, 1, 0.01), (-1, -1, 0.1),
+        (-1, 1, 0.1), (-2, 0, 0.05)]
+def move(step, dx, dy, cnt):
+    global x, y, flag
+    for i in range(cnt):
         x += dx
         y += dy
-        # 모래이동
-        if Map[x][y] != 0:
-            sand_move(x, y, dx, dy)
-        idx += 1
+        original = Map[x][y]
+        Map[x][y] = 0
+        temp = 0
+        for ddx, ddy, ratio in step:
+            nx, ny = x + ddx , y + ddy
+            temp += int(original*ratio)
+            if 0 <= nx < N and 0 <= ny < N:
+                Map[nx][ny] += int(original*ratio)
+        nx, ny = x +dx, y + dy
+        if 0 <= nx < N and 0 <= ny < N:
+            Map[nx][ny] += (original - temp)
         if x == 0 and y == 0:
             flag = 1
-            break
-    d += 1
-    if d == 2:
-        move += 1
-    elif d == 4:
-        move += 1
-        d = 0
-res = first_sand - sum([sum(x) for x in Map])
-print(res)
+            return
+
+Map = [list(map(int, input().split())) for _ in range(N)]
+total = sum([sum(x) for x in Map])
+x, y = N // 2, N // 2
+cnt = 1
+flag = 0
+while 1:
+    # 왼쪽이동
+    dx, dy = 0, -1
+    move(left,dx, dy,cnt)
+    if flag:
+        break
+    # 아래이동
+    dx, dy = 1, 0
+    move(down, dx, dy, cnt)
+    #
+    cnt += 1
+    # 오른쪽이동
+    dx, dy = 0, 1
+    move(right, dx, dy, cnt)
+    # 위이동
+    dx, dy = -1, 0
+    move(up, dx, dy, cnt)
+    #
+    cnt += 1
+print(total - sum([sum(x) for x in Map]))
