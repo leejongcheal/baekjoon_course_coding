4aaaa6f43a817fbc47faf692c7950b96cc060e24
lee <aksndk123@naver.com>
2022-05-13 15:40:55 +0900

    삼성 기출인데 30분도 안걸린듯 -> 복습은 쫌 걸림
    defaultdict이용이랑 now next나누어서 푸는 기술만 있으면 주어진대로 풀면되는 쉬운문제
    
    복습중 놓치고 n -> 1이거만 안놓쳤으면 됨
    + 0이되면 사라지게 해야하는데 안해서 틀림ㅋㅋ 아오 문제잘읽기

--- 문제/시뮬레이션과 구현 - 11/20056번 - 마법사 상어와 파이어볼.py
+++ 문제/시뮬레이션과 구현 - 11/20056번 - 마법사 상어와 파이어볼.py
@@ -1,43 +1,43 @@
 from collections import defaultdict
-steps = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
-N, M, K = map(int, input().split())
-fire = defaultdict(list) # m s d 기록
+steps = [(-1, 0),(-1, 1),(0, 1),(1, 1),(1, 0),(1, -1),(0, -1),(-1, -1)]
+N, M, k = map(int, input().split())
+fire = defaultdict(list)
 for _ in range(M):
-    x, y , m, s, d = map(int ,input().split())
-    x -= 1
-    y -= 1
-    fire[(x, y)].append((m,s,d))
-for t in range(K):
-    next = defaultdict(list)
+    r, c, m, s, d = map(int, input().split())
+    r -= 1
+    c -= 1
+    fire[(r, c)].append((m,s, d))
+for _ in range(k):
+    after = defaultdict(list)
     for x, y in fire.keys():
         for m, s, d in fire[(x, y)]:
             dx, dy = steps[d]
             nx, ny = (x + dx*s)%N, (y + dy*s)%N
-            next[(nx, ny)].append((m, s, d))
-    # 이동후 합치고 나누기
+            after[(nx, ny)].append((m, s, d))
     fire = defaultdict(list)
-    for x, y in next.keys():
-        if len(next[(x, y)]) == 1:
-            m, s, d = next[(x, y)][0]
-            fire[(x, y)].append((m, s, d))
+    for x, y in after.keys():
+        if len(after[(x, y)]) == 1:
+            fire[(x, y)].append(after[(x, y)][0])
         else:
-            sum_m = 0
-            sum_s = 0
-            m, s, d = next[(x, y)][0]
-            div_d = d%2
-            flag = 0
-            for m, s, d in next[(x, y)]:
+            sum_m , sum_s = 0, 0
+            list_d = []
+            cnt = len(after[(x, y)])
+            for m, s, d in after[(x, y)]:
                 sum_m += m
                 sum_s += s
-                if d%2 != div_d:
-                    flag = 1
-            D = [0,2,4,6]
-            if flag == 1:
-                D = [1, 3, 5, 7]
-            next_s = sum_s // len(next[(x, y)])
-            if sum_m // 5 != 0:
-                for i in range(4):
-                    fire[(x, y)].append((sum_m // 5, next_s, D[i]))
+                list_d.append(d)
+            avg_m = sum_m // 5
+            avg_s = sum_s // cnt
+            if avg_m == 0:
+                continue
+            now_d = [0, 2, 4, 6]
+            first = list_d[0]%2
+            for d in list_d:
+                if first != d%2:
+                    now_d = [1, 3, 5, 7]
+                    break
+            for d in now_d:
+                fire[(x, y)].append((avg_m, avg_s, d))
 res = 0
 for x, y in fire.keys():
     for m, s, d in fire[(x, y)]:
