0061749635afacaa4a3928e955ba2dc7a5ee1e08
lee <aksndk123@naver.com>
2022-05-13 16:21:11 +0900

    삼성기출 40분컷 + 복습중 좋은 방법 정리
    복습
    1. temp를 N사이즈로 만들어서 계속사용하기 M사이즈만들어서 Map업데이트하는식에서 수정
    2. deepcopy 대신 [x[::] for x in Map]
    3. start를 그냥 구해서 사용하는게 머리적으로 편하고 안정적임
    
    아 회전에서 실수해서 쫌걸린거랑 i,j로 비교해야하는 x, y로 비교해서 디버깅 잠시찾은거 빼고는 쉬웠음
    
    딥카피를 없애고 푸니깐 시간이 더 줄어들음
    1400->한개없앰 1000-> 아예 없앰 588
    딥카피가 무거운가 보네 ㅋㅋ
    복사기술
    temp = [x[::] for x in Map] 이건 몰랐네

--- 문제/시뮬레이션과 구현 - 11/20058번 - 마법사 상어와 파이어스톰.py
+++ 문제/시뮬레이션과 구현 - 11/20058번 - 마법사 상어와 파이어스톰.py
@@ -1,59 +1,65 @@
-from collections import deque
 steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
-def rotate(x, y, L):
-    for i in range(2**L):
-        for j in range(2**L):
-            temp[x+j][y-i+2**L-1] = Map[x+i][y+j]
-N, M = map(int, input().split())
-N = 2 ** N
-Map = [list(map(int, input().split())) for _ in range(N)]
-L_list = list(map(int, input().split()))
-for L in L_list:
-    temp = [[0]*N for _ in range(N)]
-    x, y = 0, 0
-    rotate(x, y, L)
-    while x < N:
-        y += 2**L
-        if y < N:
-            rotate(x, y, L)
-        else:
-            x += 2**L
-            y = 0
-            if x < N:
-                rotate(x, y, L)
+def rotate(x, y, M):
+    for i in range(M):
+        for j in range(M):
+            temp[x+j][y+M-i-1] = Map[x+i][y+j]
 
-    # 확인 temp에 대해서
-    Map = [x[::] for x in temp]
+
+def bfs(x, y):
+    global max_cnt
+    q = []
+    cnt = 1
+    q.append((x, y))
+    while q:
+        x, y = q.pop()
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] > 0 and visit[nx][ny] == 0:
+                visit[nx][ny] = 1
+                q.append((nx, ny))
+                cnt += 1
+    max_cnt = max(max_cnt, cnt)
+
+N, Q = map(int, input().split())
+N = 2**N
+Map = [list(map(int, input().split())) for _ in range(N)]
+L = list(map(int, input().split()))
+for l in L:
+    M = 2**l
+    start = [(0, 0)]
+    x, y = 0, 0
+    while 1:
+        y += M
+        if y >= N:
+            x += M
+            y = 0
+        if x >= N:
+            break
+        start.append((x, y))
+    # 회전
+    temp = [[0] * N for _ in range(N)]
+    for x, y in start:
+        rotate(x, y, M)
+    Map = temp
+    # 얼음 녹이기
+    original = [m[::] for m in Map]
     for i in range(N):
         for j in range(N):
-            if Map[i][j] == 0:
-                continue
-            cnt = 0
-            for dx, dy in steps:
-                nx, ny = i + dx, j + dy
-                if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] != 0:
-                    cnt += 1
-            if cnt < 3:
-                temp[i][j] -= 1
-    Map = temp
-total = sum([sum(x) for x in Map])
-max_chunk = 0
+            if original[i][j] != 0:
+                cnt = 0
+                for dx, dy in steps:
+                    nx, ny = i + dx, j + dy
+                    if 0 <= nx < N and 0 <= ny < N and original[nx][ny] > 0:
+                        cnt += 1
+                if cnt < 3:
+                    Map[i][j] -= 1
+total = sum([sum(m) for m in Map])
 visit = [[0]*N for _ in range(N)]
+max_cnt = 0
 for i in range(N):
     for j in range(N):
         if Map[i][j] != 0 and visit[i][j] == 0:
             visit[i][j] = 1
-            now = 1
-            q = deque()
-            q.append((i, j))
-            while q:
-                x, y = q.popleft()
-                for dx, dy in steps:
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] != 0 and visit[nx][ny] == 0:
-                        visit[nx][ny] = 1
-                        now += 1
-                        q.append((nx, ny))
-            max_chunk = max(max_chunk, now)
+            bfs(i,j)
 print(total)
-print(max_chunk)
+print(max_cnt)
