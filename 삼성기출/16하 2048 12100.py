f78ff3baa8f2c476ab244514b0e4aa2a39debc3a
lee <aksndk123@naver.com>
2022-05-17 10:10:55 +0900

    ***복습하기
    잘풀긴했는데 잡다한 실수
    1. for(N-1, 0, -1) 이 아님
    2. 빈부분을 "#"으로 썻는데, 0에대해서 자세히 생각을 안함
    이문제는 그냥 0 으로 쓰고 0에 대해서 연산을 안하는식으로 풀든가, 0을 "#"으로 변환후 쓰든가 했어야함
    
    솔직히 예제는 더줘라 하나가 뭐냐

--- 삼성기출/16하 2048 12100.py
+++ 삼성기출/16하 2048 12100.py
@@ -6,19 +6,62 @@ def get_max(Map):
             if Map[i][j] != "#":
                 Max = max(Max, Map[i][j])
     return Max
-def move(x, y, dx, dy, combine_idx):
-    pass
+def move(L, sx, sy, dx, dy, combine_idx):
+    x, y = sx, sy
+    if not (0 <= x + dx < N and 0 <= y + dy < N) or (L[x+dx][y+dy] != "#" and L[x+dx][y+dy] != L[sx][sy]):
+        return
+    while 0 <= x + dx < N and 0 <= y + dy < N:
+        if L[x+dx][y+dy] == L[sx][sy]:
+            if (x+dx, y + dy) not in combine_idx:
+                combine_idx.append((x+dx, y + dy))
+                L[x+dx][y+dy] *= 2
+            else:
+                L[x][y] = L[sx][sy]
+            L[sx][sy] = "#"
+            return
+        elif L[x+dx][y+dy] != "#" and L[x+dx][y+dy] != L[sx][sy]:
+            L[x][y] = L[sx][sy]
+            L[sx][sy] = "#"
+            return
+        x += dx
+        y += dy
+    L[x][y] = L[sx][sy]
+    L[sx][sy] = "#"
+    return
+
+
+
 def move_left(L):
-    pass
+    combine_idx = []
+    for j in range(N):
+        for i in range(N):
+            if L[i][j] != "#":
+                move(L, i, j, 0, -1, combine_idx)
 def move_right(L):
-    pass
+    combine_idx = []
+    for j in range(N-1, -1, -1):
+        for i in range(N):
+            if L[i][j] != "#":
+                move(L, i, j, 0, 1, combine_idx)
 def move_up(L):
-    pass
+    combine_idx = []
+    for i in range(N):
+        for j in range(N):
+            if L[i][j] != "#":
+                move(L, i, j, -1, 0, combine_idx)
 def move_down(L):
-    pass
+    combine_idx = []
+    for i in range(N - 1, -1, -1):
+        for j in range(N):
+            if L[i][j] != "#":
+                move(L, i, j, 1, 0, combine_idx)
 # 위 아래 왼 오 각각의 이동함수 만들기
 N = int(input())
 Map = [list(map(int, input().split())) for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if Map[i][j] == 0:
+            Map[i][j]= "#"
 q = deque()
 q.append((Map, 0))
 res = 0
@@ -28,22 +71,15 @@ while q:
        res = max(res, get_max(Map))
        continue
     temp = [m[::] for m in Map]
-    temp = move_left(temp)
+    move_left(temp)
     q.append((temp, cnt + 1))
     temp = [m[::] for m in Map]
-    temp = move_right(temp)
+    move_right(temp)
     q.append((temp, cnt + 1))
     temp = [m[::] for m in Map]
-    temp = move_up(temp)
+    move_up(temp)
     q.append((temp, cnt + 1))
     temp = [m[::] for m in Map]
-    temp = move_down(temp)
+    move_down(temp)
     q.append((temp, cnt + 1))
 print(res)
-
-
-
-
-
-
-
