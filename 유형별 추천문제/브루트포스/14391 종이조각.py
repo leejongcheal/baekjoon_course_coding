595fe6a3c43466390a27841ae1645db361d191c8
lee <aksndk123@naver.com>
2022-06-02 11:11:19 +0900

    ***** 이런식의 dfs문제에 약함
    연습필수...
    시작점보다 큰 다음값을 찾은다음에 그값보다큰 dfs이런식으로 하니
    *** 00 -> 10 dfs후에 1,1을 잡아서 0,1에대해서 검사를 못함 따라서 그냥 순서대로 찾으면서 visit이 0인경우에 dfs하도록 변경
    
    그다음 visit 지우는과정에서 시작점에 대해서 지우는걸 까먹음

--- 유형별 추천문제/브루트포스/14391 종이조각.py
+++ 유형별 추천문제/브루트포스/14391 종이조각.py
@@ -1,48 +1,47 @@
-from collections import deque
+def dfs(sx, sy, value):
+    global res
+    res = max(res, value)
+    flag = 0
+    for i in range(N):
+        for j in range(M):
+            if visit[i][j] == 0:
+                x, y = i, j
+                flag = 1
+                break
+        if flag:
+            break
+    if flag == 0:
+        return
+    now = Map[x][y]
+    visit[x][y] = 1
+    dfs(x, y, value + int(now))
+    mem = set()
+    for i in range(1, N):
+        if x + i < N and visit[x+i][y] == 0:
+            now += Map[x+i][y]
+            visit[x+i][y] = 1
+            mem.add((x+i, y))
+            dfs(x+i, y, value + int(now))
+        else:
+            break
+    now = Map[x][y]
+    for i, j in mem:
+        visit[i][j] = 0
+    mem = set()
+    for j in range(1, M):
+        if y + j < M and visit[x][y+j] == 0:
+            now += Map[x][y+j]
+            visit[x][y+j] = 1
+            mem.add((x, y+j))
+            dfs(x, y+j, value + int(now))
+        else:
+            break
+    for i, j in mem:
+        visit[i][j] = 0
+    visit[x][y] = 0
 N, M = map(int, input().split())
 Map = [list(input()) for _ in range(N)]
 visit = [[0]*M for _ in range(N)]
 res = 0
-q = deque()
-q.append((0, visit))
-while q:
-    flag = 0
-    val, visit = q.popleft()
-    # print(visit)
-    for i in range(N):
-        for j in range(M):
-            if visit[i][j] == 0:
-                flag = 1
-                temp = [v[::] for v in visit]
-                temp[i][j] = 1
-                q.append((val + int(Map[i][j]), temp))
-                x, y = i, j
-                now = Map[i][j]
-                for h in range(N):
-                    if 0 <= x+1 < N and temp[x+1][y] == 0:
-                        x += 1
-                        temp = [t[::] for t in temp]
-                        temp[x][y] = 1
-                        now += Map[x][y]
-                        q.append((val + int(now), temp))
-                    else:
-                        break
-                x, y = i, j
-                now = Map[i][j]
-                temp = [v[::] for v in visit]
-                temp[i][j] = 1
-                for w in range(M):
-                    if 0 <= y + 1 < M and temp[x][y+1] == 0:
-                        y += 1
-                        temp = [t[::] for t in temp]
-                        temp[x][y] = 1
-                        now += Map[x][y]
-                        q.append((val + int(now), temp))
-                    else:
-                        break
-            break
-        if flag:
-            break
-    if flag == 0:
-        res = max(res, val)
+dfs(-1, -1, 0)
 print(res)
