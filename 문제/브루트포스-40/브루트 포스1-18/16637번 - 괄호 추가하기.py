b47c7efc1528f6e895d3cef0cebaff2937c1e2d1
lee <aksndk123@naver.com>
2022-07-19 10:19:56 +0900

    *** 새로운 풀이
    시간은 전의 풀이랑 똑같고 더 접근하기 쉬움,
    
    1. dfs를 할때 지금 현재 계산을 한다 or 안한다로 나눔
     - 계산을 하는경우 1!+2+3 -> 3+3! 으로 준다.
     인덱스를 앞으로하는 이유는 ((1+2)+3) 같이못하고 (1+2)+(3+4)처럼 밖에 사용을 못함
     - 계산을 안하는 경우 1!+2+3 -> 1+2!+3으로 준다.
     나중에 인덱스의 범위에 따라서 계산을 하는식을 추가해줌
    2. 실수한 부분
     - 계산을 하고 new_s을 만들때, 인덱스전의 리스트들에 대해서 신경을못씀
    
     디버깅만 잘되면 바로 풀텐데...후 시벌

--- 문제/브루트포스-40/브루트 포스1-18/16637번 - 괄호 추가하기.py
+++ 문제/브루트포스-40/브루트 포스1-18/16637번 - 괄호 추가하기.py
@@ -1,35 +1,32 @@
-def dfs(val, op_idx, num):
-    global res
-    if op_idx == oper_cnt:
-        res = max(res, val)
-    # 한개 가능
-    if op_idx + 1 <= oper_cnt:
-        temp = eval(str(val) + oper[op_idx] + num[0])
-        dfs(temp, op_idx + 1, num[1:])
-    # 두개 가능
-    if op_idx + 2 <= oper_cnt:
-        temp = eval(str(val) + oper[op_idx] + '(' + num[0] + oper[op_idx + 1] + num[1] + ')')
-        dfs(temp, op_idx + 2, num[2:])
+def cal(a, oper, b):
+    if oper == "+":
+        res = int(a) + int(b)
+    elif oper == "-":
+        res = int(a) - int(b)
+    elif oper == "*":
+        res = int(a) * int(b)
+    return str(res)
+
+
+def total_cal(s):
+    for i in range(0,len(s)-1, 2):
+        s[i+2] = cal(s[i], s[i+1],s[i+2])
+    return int(s[-1])
+
+def dfs(s, index):
+    global max_res
+    if len(s) <= index + 1:
+        res = total_cal(s)
+        max_res = max(res, max_res)
+        return
+    now = cal(s[index], s[index + 1], s[index + 2])
+    new_s = s[:index] +[now] + s[index + 3:]
+    dfs(new_s, index+2)
+    dfs(s, index+2)
 
 
 N = int(input())
-idx = 0
-num = []
-oper = []
-for s in list(input()):
-    if idx % 2 == 0:
-        num.append(s)
-    else:
-        oper.append(s)
-    idx += 1
-num_cnt = len(num)
-oper_cnt = len(oper)
-res = -(1e10)
-if N == 1:
-    res = int(num[0])
-elif N == 3:
-    res = eval(num[0] + oper[0] + num[1])
-else:
-    dfs(int(num[0]), 0, num[1:])
-    dfs(eval(num[0] + oper[0] + num[1]), 1, num[2:])
-print(res)
+s = list(input())
+max_res = -int(1e10)
+dfs(s, 0)
+print(max_res)
