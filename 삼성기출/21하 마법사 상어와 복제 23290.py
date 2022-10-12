fe46e372f9531c6aab80ddb54acbdcc913486d4b
leejongcheal <aksndk123@naver.com>
2022-10-12 17:48:33 +0900

    **** 실수랑 자료구조 이용하는 방법만 보기
    최소값을 0으로 두어서 틀린 경우 ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
    최소값이 0인 경우 도 생각을 하자 ㅋㅋ

--- 삼성기출/21하 마법사 상어와 복제 23290.py
+++ 삼성기출/21하 마법사 상어와 복제 23290.py
@@ -1,103 +1,107 @@
+from collections import defaultdict
 from copy import deepcopy
-fish_steps = [(0, -1),(-1, -1),(-1, 0),(-1, 1),(0, 1),(1, 1),(1, 0),(1, -1)]
-shark_steps = [(-1, 0),(0, -1),(1, 0),(0, 1)]
-def fish_move_check(x, y):
-    global fish
-    if not (0 <= x < 4 and 0 <= y < 4):
-        return 0
-    if x == sx and y == sy:
-        return 0
-    if taste[x][y] > 0:
-        return 0
-    return 1
-def move_shark(d, x, y):
-    global cnt, fishs
-    dx, dy = shark_steps[d]
-    x, y = x + dx, y + dy
-    if 0 <= x < 4 and 0 <= y < 4:
-        # 위에 묶어서 풀면 안되지 갈순있어야함 범위를 벗어나는 경우를 -1, -1 리턴하도록 변경
-        if (x, y) not in fishs:
-            cnt += fish_cnt[x][y]
-            fishs.add((x, y))
-        return x, y
-    else:
-        return -1, -1
-M, S = map(int, input().split())
-fish = [[[0, 0, 0, 0, 0, 0, 0, 0] for _ in range(4)] for _ in range(4)]
-taste = [[0]*4 for _ in range(4)]
-for _ in range(M):
-    x, y, d = map(int, input().split())
-    fish[x-1][y-1][d-1] += 1
-sx, sy = map(int, input().split())
-sx -= 1
-sy -= 1
-for _ in range(S):
-    # 물고기 복사
-    copy = deepcopy(fish)
-    # 물고기 이동
-    New = [[[0, 0, 0, 0, 0, 0, 0, 0] for _ in range(4)] for _ in range(4)]
-    for x in range(4):
-        for y in range(4):
-            for d in range(8):
-                if fish[x][y][d] != 0:
-                    flag = 0
-                    for dd in range(8):
-                        nd = (d-dd)%8
-                        dx, dy = fish_steps[nd]
-                        nx, ny = x + dx, y + dy
-                        if fish_move_check(nx, ny):
-                            flag = 1
-                            New[nx][ny][nd] += fish[x][y][d]
-                            break
-                    # 이동하지 않는경우를 고려하지 않아서 틀림
-                    if flag == 0:
-                        New[x][y][d] += fish[x][y][d]
-    fish = New
-    fish_cnt = [[0]*4 for _ in range(4)]
+def fish_move(x, y, d):
+    global Map, taste, sx, sy, steps
+    for i in range(8):
+        nd = (d - i)%8
+        dx, dy = steps[nd]
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < 4 and 0 <= ny < 4 and taste[nx][ny] == 0:
+            if nx != sx or ny != sy:
+                return nx, ny, nd
+    return x, y, d
+
+def fishs_move():
+    global Map, taste, sx, sy, steps
+    New = [[[0] * 8 for _ in range(4)] for _ in range(4)]
     for i in range(4):
         for j in range(4):
-            fish_cnt[i][j] = sum(fish[i][j])
-    # 상어 이동
-    max_cnt = -1 # 0 으로 두고 시작함
-    res_d = [] # 만약에 위를 0으로 두었다면 []으로 되서 문제가 생김
+            for d in range(8):
+                if Map[i][j][d]:
+                    x, y, nd = fish_move(i, j, d)
+                    New[x][y][nd] += Map[i][j][d]
+    Map = New
+def shark_moves():
+    global step_shark, Map, sx, sy, taste
+    """
+    이부분을 1로두면 상어가 먹지않더라도 움직이는 경우 제어 못함
+    따라서 -1로 두어서 먹이0 제일우선순위 자리 구하는 경우 생각해야함
+    """
+    Max_cnt = -1
+    traked = set()
     for a in range(4):
         for b in range(4):
             for c in range(4):
-                fishs = set()
                 cnt = 0
-                now = [a, b, c]
+                visit = set()
                 x, y = sx, sy
-                x, y = move_shark(a, x, y)
-                if x == -1:
+                dx, dy = step_shark[a]
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < 4 and 0 <= ny < 4:
+                    visit.add((nx, ny))
+                    x, y = nx, ny
+                else:
                     continue
-                x, y = move_shark(b, x, y)
-                if x == -1:
+                dx, dy = step_shark[b]
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < 4 and 0 <= ny < 4:
+                    visit.add((nx, ny))
+                    x, y = nx, ny
+                else:
                     continue
-                x, y = move_shark(c, x, y)
-                if x == -1:
+                dx, dy = step_shark[c]
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < 4 and 0 <= ny < 4:
+                    visit.add((nx, ny))
+                    x, y = nx, ny
+                else:
                     continue
-                if cnt > max_cnt:
-                    max_cnt = cnt
-                    res_d = now
+                for x, y in visit:
+                    cnt += sum(Map[x][y])
+                if cnt > Max_cnt:
+                    Max_cnt = cnt
+                    traked = [a, b, c]
     x, y = sx, sy
-    for d in res_d:
-        dx, dy = shark_steps[d]
+    for d in traked:
+        dx, dy = step_shark[d]
         x, y = x + dx, y + dy
-        if sum(fish[x][y]) != 0:
+        if sum(Map[x][y]):
             taste[x][y] = 3
-            fish[x][y] = [0,0,0,0,0,0,0,0]
-    sx, sy = x, y
-    # 물고기 냄새 -1해주기
+        Map[x][y] = [0]*8
+    return x, y
+
+
+steps = [(0,-1),(-1, -1),(-1, 0),(-1, 1),(0, 1),(1, 1),(1, 0),(1,-1)]
+step_shark = [(-1, 0),(0, -1),(1, 0),(0, 1)]
+M, S = map(int, input().split())
+Map = [[[0]*8 for _ in range(4)] for _ in range(4)]
+taste = [[0]*4 for _ in range(4)]
+for fish_cnt in range(M):
+    x, y, d = map(int, input().split())
+    Map[x-1][y-1][d-1] += 1
+sx, sy = map(int, input().split())
+sx -= 1
+sy -= 1
+cnt = 0
+while cnt < S:
+    cnt += 1
+    initail = deepcopy(Map)
+    # 물고기 이동
+    fishs_move()
+    # 상어 이동
+    sx, sy = shark_moves()
+    # 냄새 -1
     for i in range(4):
         for j in range(4):
-            taste[i][j] -= 1
+            if taste[i][j] != 0:
+                taste[i][j] -= 1
     # 물고기 복사
     for i in range(4):
         for j in range(4):
             for d in range(8):
-                fish[i][j][d] += copy[i][j][d]
-res = 0
+                Map[i][j][d] += initail[i][j][d]
+cnt = 0
 for i in range(4):
     for j in range(4):
-        res += sum(fish[i][j])
-print(res)
+        cnt += sum(Map[i][j])
+print(cnt)
