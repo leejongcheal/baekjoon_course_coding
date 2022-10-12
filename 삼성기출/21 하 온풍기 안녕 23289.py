2fa5a50dfa945a387f104d0b850a28fe9c8df49b
leejongcheal <aksndk123@naver.com>
2022-10-12 19:03:38 +0900

    중간풀이 많이 더럽네 진짜 이거 2문제는 절대 못품 시간내로

---
+++ 삼성기출/21 하 온풍기 안녕 23289.py
@@ -0,0 +1,68 @@
+from copy import deepcopy
+def hot_wind():
+    global Map, wall, steps, next_step, hot, R, C
+    for hx, hy, d in hot[::-1]:
+        d1, d2 = steps[d]
+        x, y = hx + d1, hy + d2
+        new = set()
+        now = 5
+        new.add((x, y))
+        while now > 0 and new:
+            temp = set()
+            for x, y in new:
+                Map[x][y] += now
+                for dx, dy in next_step[d]:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < R and 0 <= ny < C:
+                        ddx, ddy = dx - d1, dy - d2
+                        dd = 10
+                        if ddx != 0 or ddy != 0:
+                            dd = steps.index((ddx, ddy))
+                        nnx, nny = x + ddx, y + ddy
+                        if d not in wall[nnx][nny] and dd not in wall[x][y]:
+                            temp.add((nx, ny))
+            now -= 1
+            new = temp
+
+
+
+R, C, K = map(int, input().split())
+check = set()
+steps = [(0, 1),(0, -1),(-1, 0),(1, 0)]
+next_step = [[(0, 1),(-1, 1),(1, 1)],[(1, -1),(0, -1),(-1, -1)],[(-1, 1),(-1 ,0),(-1, -1)],[(1, 1),(1, 0),(1, -1)]]
+hot = [] # x y d
+wall = [[[] for _ in range(C)] for _ in range(R)]
+Map = []
+cnt = 0
+for _ in range(R):
+    Map.append(list(map(int, input().split())))
+for _ in range(int(input())):
+    x, y, t = map(int, input().split())
+    if t == 0:
+        x1, y1 = x-1, y
+        d = steps.index((x1 - x, y1 - y))
+        wall[x-1][y-1].append(d)
+        d = steps.index((x - x1, y - y1))
+        wall[x1-1][y1-1].append(d)
+    else:
+        x1, y1 = x, y + 1
+        d = steps.index((x1 - x, y1 - y))
+        wall[x-1][y-1].append(d)
+        d = steps.index((x - x1, y - y1))
+        wall[x1-1][y1-1].append(d)
+for i in range(R):
+    for j in range(C):
+        if Map[i][j] == 5:
+            check.add((i, j))
+            Map[i][j] = 0
+        elif 1 <= Map[i][j] <= 4:
+            hot.append((i, j, Map[i][j]-1))
+            Map[i][j] = 0
+while 1:
+    if cnt > 100:
+        cnt = 101
+        break
+    # 온풍기 바람 발생
+    hot_wind()
+
+    print(1)
