082be0c1faa8aae6cc219b424f92752f365864d6
leejongcheal <aksndk123@naver.com>
2022-10-13 00:49:54 +0900

    ***아주 드러운 문제 2시간넘게 걸림
    + 계속 틀리네 아 ㅋㅋ 63% 틀림

--- 삼성기출/21하 마법사 상어와 블리자드 21611.py
+++ 삼성기출/21하 마법사 상어와 블리자드 21611.py
@@ -1,106 +1,132 @@
-from collections import defaultdict
-def find_L(x, y, t, dx, dy, check):
-    for _ in range(t):
+def make_remove_index(cnt):
+    global steps,shark_move, shark_steps, N, remove_index
+    x, y = N//2, N//2
+    d, s = shark_move[cnt]
+    dx, dy = shark_steps[d]
+    remove_index = []
+    for i in range(1, s+1):
         x += dx
         y += dy
-        L.append(Map[x][y])
-        index[(x, y)] = len(L) - 1
-        index[len(L) - 1] = (x, y)
-        if check and x == 0 and y == 0:
-            return 0, 0
-    return x, y
-def fill_L(L):
-    new_L = [0]
-    for l in L[1:]:
-        if l != 0:
-            new_L.append(l)
-    new_L += [0] * (len(L) - len(new_L))
-    return new_L
-steps = [(-1, 0),(1, 0),(0, -1),(0, 1)]
-N, M = map(int, input().split())
-Map = [list(map(int, input().split())) for _ in range(N)]
-# d, s
-blzard = [list(map(int, input().split())) for _ in range(M)]
-L = []
-index = defaultdict(int)
-# L과 index 만들기
-x = y = (N-1)//2
-t = 0
-L.append(Map[x][y])
-index[(x, y)] = len(L)-1
-index[len(L)-1] = (x, y)
-while 1:
-    t += 1
-    # <-
-    x, y = find_L(x, y, t, 0, -1, 1)
-    if x == 0 and y == 0:
-        break
-    # 밑
-    x, y = find_L(x, y, t, 1, 0, 0)
-    t += 1
-    # ->
-    x, y = find_L(x, y, t, 0, 1, 0)
-    # 위
-    x, y = find_L(x, y, t, -1, 0, 0)
-res = 0
-for idx in range(M):
-    # L값에 대해서 Map값 업데이트
-    for i in range(len(L)):
-        x, y = index[i]
-        Map[x][y] = L[i]
-    # 블리자드
-    d, s = blzard[idx]
-    d -= 1
-    dx, dy = steps[d]
-    x = y = (N-1)//2
-    for i in range(s):
-        x += dx
-        y += dy
-        idx = index[(x, y)]
-        L[idx] = 0
-    # L 빈칸 채우기
-    L = fill_L(L)
-    # 충돌검사 while 문
-    flag = 1 # 충돌있는지 검사용
-    while flag:
-        flag = 0
-        i = 1
-        while i < len(L)-1:
+        remove_index.append((x, y))
+def make_index():
+    global Map, steps, N, L_value, L_index, steps
+    x, y = N//2, N//2
+    now = 0
+    while 1:
+        now +=1
+        for i in range(4):
+            if i == 2:
+                now += 1
+            dx, dy = steps[i]
+            for _ in range(now):
+                x += dx
+                y += dy
+                if i == 0:
+                    if not (0 <= x < N and 0 <= y < N):
+                        return
+                L_index.append((x, y))
+def make_value():
+    global Map, steps, N, L_value, L_index, steps
+    L_value = [0]*(N*N - 1)
+    for i, (x, y) in enumerate(L_index):
+        if Map[x][y] == 0:
+            return
+        L_value[i] = Map[x][y]
+def remove():
+    global Map, steps, N, L_value, L_index, remove_index
+    L_new = []
+    for i, (x, y) in enumerate(L_index):
+        if (x, y) in remove_index:
+            continue
+        L_new.append(L_value[i])
+    L_new += [0]*(N*N-1-len(L_new))
+    L_value = L_new
+def bomb():
+    global Map, steps, N, L_value, L_index, remove_index, res
+    flag = 0
+    i = 1
+    L_new = []
+    if L_value[0] == 0:
+        L_value = [0]*(N*N-1)
+    pre = L_value[0]
+    cnt = 1
+    while i < len(L_value) and L_value[i] != 0:
+        if pre != L_value[i] and cnt < 4:
+            L_new += [pre]*cnt
+            pre = L_value[i]
             cnt = 1
-            if L[i] == L[i+1] and L[i] != 0:
-                cnt = 2
-                while i+cnt < len(L) and L[i] == L[i+cnt]:
-                    cnt += 1
-                if cnt >= 4:
-                    res += L[i]*cnt
-                    flag = 1
-                    for j in range(cnt):
-                        L[i+j] = 0
-            i += cnt
-        L = fill_L(L)
-    # 1. 충돌 삭제후 빈칸 채우기
-    # 2. 충돌있는지 검사
-    # 각각의 그룹에 대해서 연산후 L 채우기
-    group = [0]
-    i = 0
-    while i < len(L) - 1:
-        cnt = 1
-        if L[i] != 0:
-            while i + cnt < len(L) and L[i] == L[i + cnt]:
-                cnt += 1
-            group.append(cnt)
-            group.append(L[i])
-        i += cnt
-    if len(group) > len(L):
-        L = group[:len(L)]
+        elif pre != L_value[i] and cnt >= 4:
+            res[pre] += cnt
+            flag = 1
+            pre = L_value[i]
+            cnt = 1
+        elif pre == L_value[i]:
+            cnt += 1
+        i += 1
+    if cnt < 4:
+        L_new += [pre] * cnt
     else:
-        L = group + [0]*(len(L) - len(group))
-print(res)
-
-
-
-
-
-
-
-
+        res[pre] += cnt
+        flag = 1
+    L_new += [0] * (N * N - 1 - len(L_new))
+    L_value = L_new
+    return flag
+def change():
+    global Map, steps, N, L_value, L_index, remove_index, res
+    L_new = []
+    '''
+    아예 값이 없어서 안채워야 하는 경우 고려를 못함
+    L_value = [0,0,0,0...]인경우에도 
+    cnt = 1 고정이 되버림...시발
+    '''
+    if L_value[0] == 0:
+        L_value = [0]*(N*N-1)
+    pre = L_value[0]
+    cnt = 1
+    i = 1
+    while i < len(L_value) and L_value[i] != 0:
+        if pre != L_value[i]:
+            L_new.append(cnt)
+            L_new.append(pre)
+            pre = L_value[i]
+            cnt = 1
+        elif pre == L_value[i]:
+            cnt += 1
+        i += 1
+    L_new.append(cnt)
+    L_new.append(pre)
+    if len(L_new) >= N*N:
+        L_new = L_new[:N*N-1]
+    else:
+        L_new += [0] * (N * N - 1 - len(L_new))
+    L_value = L_new
+steps = [(0, -1),(1, 0),(0, 1),(-1,0)]
+shark_steps = [(-1, 0),(1, 0),(0, -1),(0, 1)]
+N, M = map(int, input().split())
+Map = []
+for _ in range(N):
+    Map.append(list(map(int, input().split())))
+shark_move = []
+for _ in range(M):
+    d, s = map(int, input().split())
+    shark_move.append((d-1, s))
+res = [0,0,0,0]
+L_value = []
+L_index = []
+make_index()
+make_value()
+remove_index = []
+for cnt in range(M):
+    # 삭제할 인덱스 만들기
+    make_remove_index(cnt)
+    # 구슬삭제
+    remove()
+    # 구슬 폭팔
+    while 1:
+        flag = bomb()
+        if flag == 0:
+            break
+    # 구슬 변환
+    change()
+total = sum([i*res[i] for i in range(4)])
+print(total)
