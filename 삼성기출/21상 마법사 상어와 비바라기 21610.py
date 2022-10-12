08f5bd4e36dcfbd7cac88a3644fa5df3f4d270c6
leejongcheal <aksndk123@naver.com>
2022-10-12 22:09:14 +0900

    30분컷 쉽긴하네 ㅋㅋ

--- 삼성기출/21상 마법사 상어와 비바라기 21610.py
+++ 삼성기출/21상 마법사 상어와 비바라기 21610.py
@@ -1,33 +1,50 @@
-steps = [(0, -1),(-1, -1),(-1, 0),(-1 ,1),(0, 1),(1, 1),(1, 0),(1, -1)]
-check_steps = [(-1, -1),(-1, 1),(1, 1),(1, -1)]
-N, M = map(int, input().split())
-Map = [list(map(int, input().split())) for _ in range(N)]
-move = [list(map(int, input().split())) for _ in range(M)]
-rain = [(N-1, 0),(N-1,1),(N-2,0),(N-2, 1)]
-for i in range(M):
-    d, s = move[i]
-    d -= 1
-    after_rain = []
+steps = [(0,-1),(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1)]
+steps_cloud = [(-1,-1),(-1,1),(1,1),(1,-1)]
+
+def move_cloud():
+    global N, M, Map, cnt, move, cloud, steps
+    new = []
+    d, s = move[cnt]
     dx, dy = steps[d]
-    for rx, ry in rain:
-        nrx, nry = (rx+dx*s)%N, (ry + dy*s)%N
-        after_rain.append((nrx, nry))
-    # 구름 +1 해주고 대각선 확인해야함 순서대로하자
-    for rx, ry in after_rain:
-        Map[rx][ry] += 1
-    # 증가한칸 대각선확인
-    for rx, ry in after_rain:
-        cnt = 0
-        for ddx, ddy in check_steps:
-            nrx, nry = rx + ddx, ry + ddy
-            if 0 <= nrx < N and 0 <= nry < N and Map[nrx][nry] > 0:
-                cnt += 1
-        Map[rx][ry] += cnt
-    # 다음 구름 찾기
-    rain = []
+    for x, y in cloud:
+        nx, ny = x + dx*s, y + dy*s
+        nx = nx % N
+        ny = ny % N
+        new.append((nx, ny))
+        Map[nx][ny] += 1
+    cloud = new
+
+def plus_cloud():
+    global N, M, Map, cnt, move, cloud, steps_cloud
+    for x, y in cloud:
+        for dx, dy in steps_cloud:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0<= ny < N and Map[nx][ny] > 0:
+                Map[x][y] += 1
+def new_cloud():
+    global N, M, Map, cnt, move, cloud, steps_cloud
+    new = []
     for i in range(N):
         for j in range(N):
-            if Map[i][j] >= 2 and (i, j) not in after_rain:
-                rain.append((i, j))
+            if Map[i][j] >= 2 and (i, j) not in cloud:
                 Map[i][j] -= 2
-print(sum([sum(m) for m in Map]))
+                new.append((i,j))
+    cloud = new
+N, M = map(int, input().split())
+Map= []
+for _ in range(N):
+    Map.append(list(map(int, input().split())))
+move = []
+for _ in range(M):
+    d, s = map(int, input().split())
+    move.append((d-1,s))
+cloud = [(N-1,0),(N-1,1),(N-2,0),(N-2,1)]
+for cnt in range(M):
+    # 이동후 비
+    move_cloud()
+    # 대각선 복사
+    plus_cloud()
+    # 새로운 구름 생성
+    new_cloud()
+res = sum([sum(x) for x in Map])
+print(res)
