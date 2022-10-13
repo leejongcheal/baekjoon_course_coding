0a0883a0a4bdecb6b0ec1b9b7f8f48a36536bd54
leejongcheal <aksndk123@naver.com>
2022-10-13 17:44:48 +0900

    30분컷 깔끔 풀이
    int(10**(love-1)) or [0,1,10,100,100][love] 사용가능

--- 삼성기출/21 상 상어초등학교 21608.py
+++ 삼성기출/21 상 상어초등학교 21608.py
@@ -1,43 +1,46 @@
 from collections import defaultdict
-steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
-N = int(input())
-Map = [[0]*N for _ in range(N)]
-friend = [list(map(int, input().split())) for _ in range(N*N)]
-friend_dict = defaultdict(int)
-for fr in friend:
-    now = fr[0]
-    friend_dict[now] = fr[1:]
-    res = (-1, -1)
+def find(s):
+    global N, Map, student, res
     for i in range(N):
         for j in range(N):
-            if Map[i][j] == 0:
-                f = b = 0
-                for dx, dy in steps:
-                    nx, ny = i + dx, j + dy
-                    if 0 <= nx < N and 0 <= ny < N:
-                        if Map[nx][ny] == 0:
-                            b += 1
-                        elif Map[nx][ny] != 0 and Map[nx][ny] in fr:
-                            f += 1
-                if (f, b) > res:
-                    res = (f, b)
-                    can_res = [(i, j)]
-                elif (f, b) == res:
-                    can_res.append((i, j))
-    can_res.sort()
-    x, y = can_res[0]
-    Map[x][y] = fr[0]
-# 완성
-comf_res = 0
-comf = [0, 1, 10, 100, 1000]
-for i in range(N):
-    for j in range(N):
-        cnt = 0
-        now = Map[i][j]
-        for dx, dy in steps:
-            nx, ny = i + dx, j + dy
-            if 0 <= nx < N and 0 <= ny < N:
-                if Map[nx][ny] in friend_dict[now]:
-                    cnt += 1
-        comf_res += comf[cnt]
-print(comf_res)
+            if Map[i][j] != 0:
+                continue
+            love, blank = 0,0
+            for dx, dy in steps:
+                nx, ny = i +dx, j + dy
+                if 0 <= nx < N and 0 <= ny < N:
+                    if Map[nx][ny] in student[s]:
+                        love += 1
+                    elif Map[nx][ny] == 0:
+                        blank += 1
+                if (love, blank, -1*i, -1*j) > res:
+                    res = (love, blank, -1*i, -1*j)
+    return res
+
+def cal_res():
+    global N, Map, student, res
+    for i in range(N):
+        for j in range(N):
+            love = 0
+            s = Map[i][j]
+            for dx, dy in steps:
+                nx, ny = i +dx, j + dy
+                if 0 <= nx < N and 0 <= ny < N:
+                    if Map[nx][ny] in student[s]:
+                        love += 1
+            res += int(10**(love-1))
+N = int(input())
+Map = [[0]*N for _ in range(N)]
+student = defaultdict(set)
+steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
+for _ in range(N*N):
+    a, *b = map(int, input().split())
+    student[a] = set(b)
+for s in student.keys():
+    res = (-1, -1, -1*N, -1*N)
+    find(s)
+    x, y = -1*res[2], -1*res[3]
+    Map[x][y] = s
+res = 0
+cal_res()
+print(res)
