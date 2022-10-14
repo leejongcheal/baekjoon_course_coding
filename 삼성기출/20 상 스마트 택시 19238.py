8f34d363e65aa8992583119288661328f26f4166
lee <aksndk123@naver.com>
2022-10-14 16:49:44 +0900

    ** 배열을 적절하게 조절
    전의 풀이보다 빨라짐
    승객은 2보다 크게 한번왔다면 0으로 해버림
    dfs에 거리까지 녹여내서 풀면됨
    이때 갈수없는 경우 + 연료 부족한 경우는 -1임을 주의해야함
    갈수 없는 경우를 고려안할뻔

--- 삼성기출/20 상 스마트 택시 19238.py
+++ 삼성기출/20 상 스마트 택시 19238.py
@@ -1,60 +1,88 @@
 from collections import deque, defaultdict
-steps = [(1, 0),(-1, 0),(0, -1),(0, 1)]
-def bfs(x, y, target):
-    res_d = INF
-    res = [(INF, INF)]
-    visit = defaultdict(int)
+
+# 못 가는 경우도 생각해야함
+def find(x, y):
+    global N, fuel, Map
+    cand = []
+    cand_dist = 10**7
+    visit = [[0]*N for _ in range(N)]
+    visit[x][y] = 1
     q = deque()
-    visit[(x, y)] = 1
     q.append((x, y, 0))
     while q:
-        x, y, d = q.popleft()
-        if d > res_d:
+        x, y, dist = q.popleft()
+        if dist > cand_dist:
             break
-        if (x, y) in target:
-            res_d = d
-            res.append((x, y))
-            continue
+        # 못 가는 경우
+        if fuel < dist:
+            return -1, -1
+        if Map[x][y] >= 2:
+            if cand_dist > dist:
+                cand.append((x, y))
+                cand_dist = dist
+                continue
+            elif cand_dist == dist:
+                cand.append((x, y))
+                continue
         for dx, dy in steps:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < N and Map[nx][ny] == 0 and visit[(nx, ny)] == 0:
-                visit[(nx, ny)] = 1
-                q.append((nx, ny, d+1))
-    res.sort()
-    x, y = res[0]
-    return x, y, res_d
+            if 0 <= nx < N and 0 <= ny < N and visit[nx][ny] == 0 and Map[nx][ny] != 1:
+                visit[nx][ny] = 1
+                q.append((nx, ny, dist + 1))
+    # 갈수 없는 경우
+    if len(cand) == 0:
+        return -1, -1
+    fuel -= cand_dist
+    cand.sort()
+    return cand[0][0], cand[0][1]
 
-
-
-INF = int(1e10)
+def go_target(x, y):
+    global N, fuel, Map, now_dist, target
+    tx, ty = target[Map[x][y]]
+    Map[x][y] = 0
+    visit = [[0] * N for _ in range(N)]
+    visit[x][y] = 1
+    q = deque()
+    q.append((x, y, 0))
+    while q:
+        x, y, dist = q.popleft()
+        # 연로 부족으로 못 가는 경우
+        if fuel < dist:
+            return -1, -1
+        if x == tx and y == ty:
+            fuel += dist
+            return x, y
+        for dx, dy in steps:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < N and visit[nx][ny] == 0 and Map[nx][ny] != 1:
+                visit[nx][ny] = 1
+                q.append((nx, ny, dist + 1))
+    # 다 돌았는데 못가는 경우
+    return -1, -1
+steps = [(1, 0),(-1, 0),(0, 1),(0, -1)]
 N, M, fuel = map(int, input().split())
-Map = [list(map(int,input().split())) for _ in range(N)]
-tx, ty = map(int,input().split())
-tx -= 1
-ty -= 1
-passenger = []
-destination = dict()
-for _ in range(M):
-    px, py, dx, dy = map(int, input().split())
-    passenger.append((px-1, py-1))
-    destination[(px-1, py-1)] = (dx-1, dy-1)
+Map = [list(map(int, input().split())) for _ in range(N)]
+# 0, 1, 2~ 승객번호
+sx, sy = map(int, input().split())
+sx -= 1
+sy -= 1
+target = defaultdict(int)
+for i in range(M):
+    x, y, tx, ty = map(int, input().split())
+    Map[x-1][y-1] = i + 2
+    target[i+2] = (tx-1, ty-1)
 cnt = 0
+x, y = sx, sy
 while cnt < M:
-    # 승객 위치 찾기
-    tx, ty, d1 = bfs(tx, ty, passenger)
-    if d1 == INF or fuel < d1:
-        break
-    passenger.remove((tx, ty))
-    fuel -= d1
-    tx, ty, d2 = bfs(tx, ty, [destination[(tx, ty)]])
-    if d2 == INF or fuel < d2:
-        break
-    fuel -= d2
-    fuel += d2*2
     cnt += 1
-if cnt == M:
-    print(fuel)
-else:
-    print(-1)
-
-
+    # 가까운 승객 찾기
+    x, y = find(x, y)
+    if x == -1 and y == -1:
+        fuel = -1
+        break
+    # target으로 가기
+    x, y = go_target(x, y)
+    if x == -1 and y == -1:
+        fuel = -1
+        break
+print(fuel)
